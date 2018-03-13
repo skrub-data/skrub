7aa5a783304861e915d5d36bc35d4a65737f158c
pcerda <patricio.cerda@inria.fr>
2018-03-13 11:39:37 +0100

    new file configuration

---
+++ dirty_cat/similarity_encoder.py
@@ -0,0 +1,371 @@
+import collections
+import numpy as np
+from scipy import sparse
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils import check_array
+
+import jellyfish
+import Levenshtein as lev
+import distance as dist
+
+from .ngrams_vectorizer import ngram_similarity
+
+
+def ngram_similarity(X, cats, n, sim_type=None, dtype=np.float64):
+    """ Similarity encoding:
+    Given to arrays of strings, returns the
+    similarity encoding matrix of size
+    len(X) x len(cats)
+    sim1(s_i, s_j) = ||min(ci, cj)||_1 /
+                    (||ci||_1 + ||cj||_1 - ||min(ci, cj)||_1)
+
+    sim2(s_i, s_j) = 2||min(ci, cj)||_1/ (||ci||_1 + ||cj||_1)
+
+    sim2_1(s_i, s_j) = 2 dot(c1, c2) / (dot(c1, c1) + dot(c2, c2))
+
+    sim2_2(s_i, s_j) = 2 dot(p1, p2) / (dot(p1, p1) + dot(p2, p2))
+
+    sim3(s_i, s_j) = dot(ci, cj) / (dot(ci, ci) + dot(cj, cj) - dot(ci, cj))
+
+    sim3_2(s_i, s_j) = dot(p1, p2) / (dot(p1, p1) + dot(p2, p2) - dot(p1, p2))
+
+    sim4(s_i, s_j) = dot(c1, c2) / (dot(c1, c1)^.5 * dot(c2, c2)^.5)
+
+    sim5(s_i, s_j) = dot(p1, p2) / (dot(p1, p1)^.5 * dot(p2, p2)^.5)
+
+    sim6(s_i, s_j) = dot(c1, c2)
+
+    sim7(s_i, s_j) = dot(p1, p2)
+
+    fisher_kernel
+    """
+    if sim_type == 'sim1':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        count2 = vectorizer.fit_transform(cats)
+        count1 = vectorizer.transform(unq_X)
+        sum2 = count2.sum(axis=1)
+        SE_dict = {}
+        for i, x in enumerate(count1):
+            aux = sparse.csr_matrix(np.ones((count2.shape[0], 1))).dot(x)
+            samegrams = count2.minimum(aux).sum(axis=1)
+            allgrams = x.sum() + sum2 - samegrams
+            similarity = np.divide(samegrams, allgrams)
+            SE_dict[unq_X[i]] = np.array(similarity).reshape(-1)
+        SE = []
+        for x in X:
+            SE.append(SE_dict[x])
+        return np.nan_to_num(np.vstack(SE))
+
+    if sim_type == 'sim2':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        count2 = vectorizer.fit_transform(cats)
+        count1 = vectorizer.transform(unq_X)
+        sum_matrix2 = count2.sum(axis=1)
+        SE_dict = {}
+        for i, x in enumerate(count1):
+            aux = sparse.csr_matrix(np.ones((count2.shape[0], 1))).dot(x)
+            samegrams = count2.minimum(aux).sum(axis=1)
+            allgrams = x.sum() + sum_matrix2
+            similarity = 2 * np.divide(samegrams, allgrams)
+            SE_dict[unq_X[i]] = np.array(similarity).reshape(-1)
+        SE = []
+        for x in X:
+            SE.append(SE_dict[x])
+        return np.nan_to_num(np.vstack(SE))
+
+    if sim_type == 'sim2_1':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = vectorizer.fit_transform(cats).transpose()
+        Ci = vectorizer.transform(unq_X)
+        SE_dict = {}
+        cij = Ci.dot(Cj).toarray()
+        cii = np.tile(Ci.multiply(Ci).sum(axis=1),
+                      (1, Cj.shape[1]))
+        cjj = np.tile(Cj.multiply(Cj).sum(axis=0),
+                      (Ci.shape[0], 1))
+        similarity = np.divide(2*cij, cii + cjj)
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[s] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'sim2_2':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = (vectorizer.fit_transform(cats) > 0
+              ).astype(dtype).transpose()
+        Ci = (vectorizer.transform(unq_X) > 0).astype(dtype)
+        SE_dict = {}
+        cij = Ci.dot(Cj).toarray()
+        cii = np.tile(Ci.multiply(Ci).sum(axis=1),
+                      (1, Cj.shape[1]))
+        cjj = np.tile(Cj.multiply(Cj).sum(axis=0),
+                      (Ci.shape[0], 1))
+        similarity = np.divide(2*cij, cii + cjj)
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[x] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'sim3':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = vectorizer.fit_transform(cats).transpose()
+        Ci = vectorizer.transform(unq_X)
+        SE_dict = {}
+        cij = Ci.dot(Cj).toarray()
+        cii = np.tile(Ci.multiply(Ci).sum(axis=1),
+                      (1, Cj.shape[1]))
+        cjj = np.tile(Cj.multiply(Cj).sum(axis=0),
+                      (Ci.shape[0], 1))
+        similarity = np.divide(cij, cii + cjj - cij)
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[x] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'sim3_2':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = (vectorizer.fit_transform(cats) > 0
+              ).astype(dtype).transpose()
+        Ci = (vectorizer.transform(unq_X) > 0).astype(dtype)
+        SE_dict = {}
+        cij = Ci.dot(Cj).toarray()
+        cii = np.tile(Ci.multiply(Ci).sum(axis=1),
+                      (1, Cj.shape[1]))
+        cjj = np.tile(Cj.multiply(Cj).sum(axis=0),
+                      (Ci.shape[0], 1))
+        similarity = np.divide(cij, cii + cjj - cij)
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[x] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'sim4':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = vectorizer.fit_transform(cats).transpose()
+        Ci = vectorizer.transform(unq_X)
+        SE_dict = {}
+        cij = Ci.dot(Cj).toarray()
+        cii = np.tile(np.power(Ci.multiply(Ci).sum(axis=1), .5),
+                      (1, Cj.shape[1]))
+        cjj = np.tile(np.power(Cj.multiply(Cj).sum(axis=0), .5),
+                      (Ci.shape[0], 1))
+        similarity = np.divide(cij, np.multiply(cii, cjj))
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[x] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'sim5':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = (vectorizer.fit_transform(cats) > 0
+              ).astype(dtype).transpose()
+        Ci = (vectorizer.transform(unq_X) > 0).astype(dtype)
+        SE_dict = {}
+        cij = Ci.dot(Cj).toarray()
+        cii = np.tile(np.power(Ci.multiply(Ci).sum(axis=1), .5),
+                      (1, Cj.shape[1]))
+        cjj = np.tile(np.power(Cj.multiply(Cj).sum(axis=0), .5),
+                      (Ci.shape[0], 1))
+        similarity = np.divide(cij, np.multiply(cii, cjj))
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[x] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'sim6':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = vectorizer.fit_transform(cats).transpose()
+        Ci = vectorizer.transform(unq_X)
+        SE_dict = {}
+        similarity = Ci.dot(Cj).toarray()
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[x] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'sim7':
+        unq_X = np.unique(X)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = (vectorizer.fit_transform(cats) > 0
+              ).astype(dtype).transpose()
+        Ci = (vectorizer.transform(unq_X) > 0).astype(dtype)
+        SE_dict = {}
+        similarity = Ci.dot(Cj).toarray()
+        X_dict = {s: i for i, s in enumerate(unq_X)}
+        index = [X_dict[x] for x in X]
+        similarity = similarity[index]
+        return np.nan_to_num(similarity)
+
+    if sim_type == 'fisher_kernel':
+        unq_X = np.unique(X)
+        unq_cats, count_j = np.unique(cats, return_counts=True)
+        theta = count_j/sum(count_j)
+        vectorizer = CountVectorizer(analyzer='char', ngram_range=(n, n))
+        Cj = (vectorizer.fit_transform(unq_cats) > 0
+              ).astype(dtype).toarray()
+        Ci = (vectorizer.transform(unq_X) > 0).astype(dtype).toarray()
+        m = Cj.shape[1]
+        SE_dict = {}
+        for i, p_i in enumerate(Ci):
+            gamma = np.zeros(m)
+            for j, p_j in enumerate(Cj):
+                gamma += (p_j == p_i).astype(dtype)*theta[j]
+            similarity = []
+            for j, p_j in enumerate(Cj):
+                sim_j = (p_j == p_i).astype(dtype) / gamma
+                similarity.append(sim_j.sum())
+            SE_dict[unq_X[i]] = np.array(similarity)
+        SE = []
+        for x in X:
+            SE.append(SE_dict[x])
+        return np.nan_to_num(np.vstack(SE))
+
+
+class SimilarityEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, similarity='ngram',
+                 ngram_similarity_type='sim2',
+                 n=3, categories='auto',
+                 dtype=np.float64, handle_unknown='error',
+                 clf_type='binary_clf'):
+        self.categories = categories
+        self.dtype = dtype
+        self.handle_unknown = handle_unknown
+        self.similarity = similarity
+        self.ngram_type = ngram_type
+        self.n = n
+
+    def fit(self, X, y=None):
+        if self.handle_unknown not in ['error', 'ignore']:
+            template = ("handle_unknown should be either 'error' or "
+                        "'ignore', got %s")
+            raise ValueError(template % self.handle_unknown)
+
+        if self.categories != 'auto':
+            for cats in self.categories:
+                if not np.all(np.sort(cats) == np.array(cats)):
+                    raise ValueError("Unsorted categories are not yet "
+                                     "supported")
+
+        X_temp = check_array(X, dtype=None)
+        if not hasattr(X, 'dtype') and np.issubdtype(X_temp.dtype, np.str_):
+            X = check_array(X, dtype=np.object)
+        else:
+            X = X_temp
+
+        n_samples, n_features = X.shape
+
+        self._label_encoders_ = [LabelEncoder() for _ in range(n_features)]
+
+        for i in range(n_features):
+            le = self._label_encoders_[i]
+            Xi = X[:, i]
+            if self.categories == 'auto':
+                le.fit(Xi)
+            else:
+                if self.handle_unknown == 'error':
+                    valid_mask = np.in1d(Xi, self.categories[i])
+                    if not np.all(valid_mask):
+                        diff = np.unique(Xi[~valid_mask])
+                        msg = ("Found unknown categories {0} in column {1}"
+                               " during fit".format(diff, i))
+                        raise ValueError(msg)
+                le.classes_ = np.array(self.categories[i])
+
+        self.categories_ = [le.classes_ for le in self._label_encoders_]
+        return self
+
+    def transform(self, X):
+        """Transform X using specified encoding scheme.
+        Parameters
+        ----------
+        X : array-like, shape [n_samples, n_features]
+            The data to encode.
+        Returns
+        -------
+        X_out : sparse matrix or a 2-d array
+            Transformed input.
+        """
+        X_temp = check_array(X, dtype=None)
+        if not hasattr(X, 'dtype') and np.issubdtype(X_temp.dtype, np.str_):
+            X = check_array(X, dtype=np.object)
+        else:
+            X = X_temp
+
+        n_samples, n_features = X.shape
+        X_int = np.zeros_like(X, dtype=np.int)
+        X_mask = np.ones_like(X, dtype=np.bool)
+
+        for i in range(n_features):
+            Xi = X[:, i]
+            valid_mask = np.in1d(Xi, self.categories_[i])
+
+            if not np.all(valid_mask):
+                if self.handle_unknown == 'error':
+                    diff = np.unique(X[~valid_mask, i])
+                    msg = ("Found unknown categories {0} in column {1}"
+                           " during transform".format(diff, i))
+                    raise ValueError(msg)
+                else:
+                    # Set the problematic rows to an acceptable value and
+                    # continue `The rows are marked `X_mask` and will be
+                    # removed later.
+                    X_mask[:, i] = valid_mask
+                    Xi = Xi.copy()
+                    Xi[~valid_mask] = self.categories_[i][0]
+            X_int[:, i] = self._label_encoders_[i].transform(Xi)
+
+        if self.similarity == 'levenshtein-ratio':
+            out = []
+            for j, cats in enumerate(self.categories_):
+                unqX = np.unique(X[:, j])
+                vect = np.vectorize(lev.ratio)
+                encoder_dict = {x: vect(x, cats.reshape(1, -1))
+                                for x in unqX}
+                encoder = [encoder_dict[x] for x in X[:, j]]
+                encoder = np.vstack(encoder)
+                out.append(encoder)
+            return np.hstack(out)
+
+        if self.similarity == 'sorensen':
+            out = []
+            for j, cats in enumerate(self.categories_):
+                unqX = np.unique(X[:, j])
+                vect = np.vectorize(dist.sorensen)
+                encoder_dict = {x: vect(x, cats.reshape(1, -1))
+                                for x in unqX}
+                encoder = [encoder_dict[x] for x in X[:, j]]
+                encoder = 1 - np.vstack(encoder)
+                out.append(encoder)
+            return np.hstack(out)
+
+        if self.similarity == 'jaro-winkler':
+            out = []
+            for j, cats in enumerate(self.categories_):
+                unqX = np.unique(X[:, j])
+                vect = np.vectorize(jellyfish.jaro_distance)
+                encoder_dict = {x: vect(x, cats.reshape(1, -1))
+                                for x in unqX}
+                encoder = [encoder_dict[x] for x in X[:, j]]
+                encoder = np.vstack(encoder)
+                out.append(encoder)
+            return np.hstack(out)
+
+        if self.similarity == 'ngram':
+            out = []
+            for j, cats in enumerate(self.categories_):
+                encoder = ngram_similarity(X[:, j], cats,
+                                           self.n, self.ngram_type,
+                                           dtype=self.dtype)
+                out.append(encoder)
+            return np.hstack(out)
---
+++ dirty_cat/target_encoder.py
@@ -0,0 +1,153 @@
+import collections
+import numpy as np
+from scipy import sparse
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils import check_array
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def lambda_(x, n):
+    out = x / (x + n)
+    return out
+
+
+class TargetEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self,
+                 categories='auto',
+                 clf_type='binary-clf'
+                 dtype=np.float64, handle_unknown='error',
+                 ):
+        self.categories = categories
+        self.dtype = dtype
+        self.clf_type = clf_type
+        self.handle_unknown = handle_unknown
+
+    def fit(self, X, y=None):
+        if self.handle_unknown not in ['error', 'ignore']:
+            template = ("handle_unknown should be either 'error' or "
+                        "'ignore', got %s")
+            raise ValueError(template % self.handle_unknown)
+
+        if self.categories != 'auto':
+            for cats in self.categories:
+                if not np.all(np.sort(cats) == np.array(cats)):
+                    raise ValueError("Unsorted categories are not yet "
+                                     "supported")
+
+        X_temp = check_array(X, dtype=None)
+        if not hasattr(X, 'dtype') and np.issubdtype(X_temp.dtype, np.str_):
+            X = check_array(X, dtype=np.object)
+        else:
+            X = X_temp
+
+        n_samples, n_features = X.shape
+
+        self._label_encoders_ = [LabelEncoder() for _ in range(n_features)]
+
+        for i in range(n_features):
+            le = self._label_encoders_[i]
+            Xi = X[:, i]
+            if self.categories == 'auto':
+                le.fit(Xi)
+            else:
+                if self.handle_unknown == 'error':
+                    valid_mask = np.in1d(Xi, self.categories[i])
+                    if not np.all(valid_mask):
+                        diff = np.unique(Xi[~valid_mask])
+                        msg = ("Found unknown categories {0} in column {1}"
+                               " during fit".format(diff, i))
+                        raise ValueError(msg)
+                le.classes_ = np.array(self.categories[i])
+
+        self.categories_ = [le.classes_ for le in self._label_encoders_]
+
+        self.Eyx_ = [{cat: np.mean(y[X[:, i] == cat])
+                      for cat in self.categories_[i]}
+                     for i in range(len(self.categories_))]
+        self.Ey_ = [np.mean(y)
+                    for i in range(len(self.categories_))]
+        return self
+
+    def transform(self, X):
+        """Transform X using specified encoding scheme.
+        Parameters
+        ----------
+        X : array-like, shape [n_samples, n_features]
+            The data to encode.
+        Returns
+        -------
+        X_out : sparse matrix or a 2-d array
+            Transformed input.
+        """
+        X_temp = check_array(X, dtype=None)
+        if not hasattr(X, 'dtype') and np.issubdtype(X_temp.dtype, np.str_):
+            X = check_array(X, dtype=np.object)
+        else:
+            X = X_temp
+
+        n_samples, n_features = X.shape
+        X_int = np.zeros_like(X, dtype=np.int)
+        X_mask = np.ones_like(X, dtype=np.bool)
+
+        for i in range(n_features):
+            Xi = X[:, i]
+            valid_mask = np.in1d(Xi, self.categories_[i])
+
+            if not np.all(valid_mask):
+                if self.handle_unknown == 'error':
+                    diff = np.unique(X[~valid_mask, i])
+                    msg = ("Found unknown categories {0} in column {1}"
+                           " during transform".format(diff, i))
+                    raise ValueError(msg)
+                else:
+                    # Set the problematic rows to an acceptable value and
+                    # continue `The rows are marked `X_mask` and will be
+                    # removed later.
+                    X_mask[:, i] = valid_mask
+                    Xi = Xi.copy()
+                    Xi[~valid_mask] = self.categories_[i][0]
+            X_int[:, i] = self._label_encoders_[i].transform(Xi)
+
+        if self.clf_type in ['binary-clf', 'regression']:
+            out = []
+            for j, cats in enumerate(self.categories_):
+                counter = collections.Counter(X[:, j])
+                unqX = np.unique(X[:, j])
+                n = len(X[:, j])
+                k = len(cats)
+                encoder = {x: 0 for x in unqX}
+                if self.clf_type in ['binary-clf', 'regression']:
+                    for x in unqX:
+                        if x not in cats:
+                            Eyx = 0
+                        else:
+                            Eyx = self.Eyx_[j][x]
+                        lambda_n = lambda_(counter[x], n/k)
+                        encoder[x] = lambda_n*Eyx + \
+                            (1 - lambda_n)*self.Ey_[j]
+                    x_out = np.zeros((len(X[:, j]), 1))
+                    for i, x in enumerate(X[:, j]):
+                        x_out[i, 0] = encoder[x]
+                    out.append(x_out.reshape(-1, 1))
+            out = np.hstack(out)
+        # if self.clf_type == 'multiclass-clf':
+        #     x_out = np.zeros((len(cats), len(np.unique(y_train))))
+        #     lambda_n = {x: 0 for x in unqA}
+        #     y_train2 = {x: 0 for x in unqA}
+        #     for x in unqA:
+        #         lambda_n[x] = lambda_(counter[x], n/k)
+        #         y_train2[x] = y_train[B == x]
+        #     for j, y in enumerate(np.unique(y_train)):
+        #         Ey = sum(y_train == y)/n
+        #         for x in unqA:
+        #             if len(y_train2[x]) == 0:
+        #                 Eyx = 0
+        #             else:
+        #                 Eyx = sum(y_train2[x] == y)/len(y_train2[x])
+        #             encoder[x] = lambda_n[x]*Eyx + (1 - lambda_n[x])*Ey
+        #         for i, x in enumerate(A):
+        #             x_out[i, j] = encoder[x]
+        #
+        #     return out
