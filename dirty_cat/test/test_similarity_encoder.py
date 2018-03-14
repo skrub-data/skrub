74e1ab50e9701455ff28bddaf9b84aa8a78d3210
pcerda <patricio.cerda@inria.fr>
2018-03-14 16:00:28 +0100

    testing test

--- dirty_cat/test/test_similarity_encoder.py
+++ dirty_cat/test/test_similarity_encoder.py
@@ -2,7 +2,7 @@ import numpy as np
 import Levenshtein as lev
 from jellyfish import jaro_distance
 
-from dirty_cat import similarity_encoder, target_encoder
+from dirty_cat import similarity_encoder, target_encoder, string_distances
 
 
 def test_similarity_encoder():
@@ -32,4 +32,10 @@ def test_similarity_encoder():
             for i, x_t in enumerate(X_test.reshape(-1)):
                 for j, x in enumerate(X.reshape(-1)):
                     ans[i, j] = jaro_distance(x_t, x)
-            assert np.array_equal(encoder, ans - 1)
+            assert np.array_equal(encoder, ans)
+        if similarity_type == 'ngram':
+            ans = np.zeros((len(X_test), len(X)))
+            for i, x_t in enumerate(X_test.reshape(-1)):
+                for j, x in enumerate(X.reshape(-1)):
+                    ans[i, j] = string_distances.ngram_similarity(x_t, x, 3)
+            assert np.array_equal(encoder, ans)
