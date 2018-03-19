71960e8211d8ed7b4664a2e463dc86296d03c598
pcerda <patricio.cerda@inria.fr>
2018-03-19 15:04:51 +0100

    test and multiclass-clf implementation for target-encoding

--- dirty_cat/test/test_target_encoder.py
+++ dirty_cat/test/test_target_encoder.py
@@ -22,6 +22,8 @@ def test_similarity_encoder():
                    'female',
                    'male']).reshape(-1, 1)
     X = np.hstack([X1, X2])
+
+    # Case 1: binary-classification and regression
     y = np.array([1, 0, 0, 1, 0, 1, 0, 0])
     n = len(y)
     Ey_ = 3/8
@@ -70,7 +72,99 @@ def test_similarity_encoder():
     encoder = target_encoder.TargetEncoder()
     encoder.fit(X, y)
     Xout = encoder.transform(Xtest)
-    encoder.Eyx_
-    encoder.Ey_
-    encoder.classes_
+    assert np.array_equal(Xout, ans)
+
+    # Case 2: multiclass-classification
+    y = np.array([1, 0, 2, 1, 0, 1, 0, 0])
+    n = len(y)
+    Ey_ = {0: 4/8, 1: 3/8, 2: 1/8}
+    Eyx_ = {0: {}, 1: {}, 2: {}}
+    Eyx_[0] = {'color': {'Red': 0,
+                         'red': 1,
+                         'green': 1/3,
+                         'blue': .5},
+               'gender': {'male': .5,
+                          'female': .5}}
+    Eyx_[1] = {'color': {'Red': 1,
+                         'red': 0,
+                         'green': 1/3,
+                         'blue': .5},
+               'gender': {'male': .5,
+                          'female': .25}}
+    Eyx_[2] = {'color': {'Red': 0,
+                         'red': 0,
+                         'green': 1/3,
+                         'blue': 0},
+               'gender': {'male': 0,
+                          'female': .25}}
+
+    count_ = {'color': {'Red': 1,
+                        'red': 2,
+                        'green': 3,
+                        'blue': 2},
+              'gender': {'male': 4,
+                         'female': 4}}
+    ans_dict = {0: {}, 1: {}, 2: {}}
+    for k in [0, 1, 2]:
+        ans_dict[k] = {var:
+                       {cat:
+                        Eyx_[k][var][cat] *
+                        lambda_(count_[var][cat], n/len(count_[var])) +
+                        Ey_[k] *
+                        (1 - lambda_(count_[var][cat], n/len(count_[var])))
+                        for cat in Eyx_[k][var]}
+                       for var in Eyx_[k]
+                       }
+
+    ans = np.zeros((n, 2*3))
+    Xtest1 = np.array(['Red',
+                       'red',
+                       'blue',
+                       'green',
+                       'Red',
+                       'red',
+                       'blue',
+                       'green']).reshape(-1, 1)
+    Xtest2 = np.array(['male',
+                       'male',
+                       'male',
+                       'male',
+                       'female',
+                       'female',
+                       'female',
+                       'female']).reshape(-1, 1)
+    Xtest = np.hstack([Xtest1, Xtest2])
+    for k in [0, 1, 2]:
+        for j, col in enumerate(['color', 'gender']):
+            for i in range(n):
+                ans[i, 2*j+k] = ans_dict[k][col][Xtest[i, j]]
+
+    encoder = target_encoder.TargetEncoder(clf_type='multiclass-clf')
+    encoder.fit(X, y)
+    Xout = encoder.transform(Xtest)
+    ans = np.zeros((n, 2*3))
+    Xtest1 = np.array(['Red',
+                       'red',
+                       'blue',
+                       'green',
+                       'Red',
+                       'red',
+                       'blue',
+                       'green']).reshape(-1, 1)
+    Xtest2 = np.array(['male',
+                       'male',
+                       'male',
+                       'male',
+                       'female',
+                       'female',
+                       'female',
+                       'female']).reshape(-1, 1)
+    Xtest = np.hstack([Xtest1, Xtest2])
+    for k in [0, 1, 2]:
+        for j, col in enumerate(['color', 'gender']):
+            for i in range(n):
+                ans[i, j*3+k] = ans_dict[k][col][Xtest[i, j]]
+    encoder = target_encoder.TargetEncoder(clf_type='multiclass-clf')
+    encoder.fit(X, y)
+    Xout = encoder.transform(Xtest)
     assert np.array_equal(Xout, ans)
