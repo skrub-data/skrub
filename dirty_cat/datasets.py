5cbf5d3bfd055b646e26906d6d28ba044abe4112
Gael Varoquaux <gael.varoquaux@normalesup.org>
2018-03-20 09:36:17 +0100

    DOC: document data downloading functions

--- dirty_cat/datasets.py
+++ dirty_cat/datasets.py
@@ -6,14 +6,19 @@ FOLDER_PATH = os.path.dirname(os.path.realpath(__file__))
 
 def fetch_midwest_survey():
     """
-    function that fetches example data
-    - either from the internet
-    - or locally
-    because the base example dataset (midwest survey) chosen is very small,
-    it is locally stored in data/midwest_survey.csv
-    in the future, for bigger dataset, this function will get the data
-    from a specific url, and store it (optional) before returning it.
-    :return:
+    Function that returns the path to "midwest survey" example dataset.
+
+    Returns
+    ========
+
+    filename: string
+        The absolute path to the CSV file
+
+    Notes
+    =====
+
+    The original data is retrieved from:
+    https://github.com/fivethirtyeight/data/tree/master/region-survey
     """
 
     return os.path.join(FOLDER_PATH, 'data', 'midwest_survey.csv.gz')
@@ -21,7 +26,19 @@ def fetch_midwest_survey():
 
 def fetch_employee_salaries():
     '''
-    Fetch employee_salaries dataset
+    Function that returns the path to "employee_salaries" example dataset.
+
+    Returns
+    ========
+
+    filename: string
+        The absolute path to the CSV file
+
+    Notes
+    =====
+
+    The original data is retrieved from:
+    https://catalog.data.gov/dataset/employee-salaries-2016
     '''
 
     return os.path.join(FOLDER_PATH, 'data', 'employee_salaries.csv.gz')
