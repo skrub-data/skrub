4facb0b3e5bc4bd1dd4d702257ae0339328a9e70
Pierre Glaser <pierreglaser@msn.com>
2018-04-12 15:07:30 +0200

    better hierarchical structure for fetcher config
    a dataset config is now basically a list of urlinfos,
    where each urlinfos contains
    - the name of each file downloaded
    - the url string
    - whether to uncompress or not the content of the request

--- dirty_cat/datasets.py
+++ dirty_cat/datasets.py
@@ -18,6 +18,8 @@ from clint.textui import \
     progress  # package developed by the same guyy than requests
 import warnings
 import zipfile
+from collections import namedtuple
+
 
 # in nilearn, urllib is used. Here the request package will be used
 # trying to use requests as much as possible (everything excpet the parsing function)
@@ -40,35 +42,94 @@ class FileFetcherConfig:
 # this is true for bigml and midwest survey
 
 
-midwest_survey_fetcher_config = FileFetcherConfig(
-    'midwest_survey',
-    "https://github.com/fivethirtyeight/data/tree/master/"
-    "region-survey/FiveThirtyEight_Midwest_Survey.csv",
-    "FiveThirtyEight_Midwest_Survey.csv")
+FetcherConfig = namedtuple('FetcherConfig',
+                           ['dataset_name', 'urlinfos'])
 
-road_safety_config = FileFetcherConfig(
+# a FetcherConfig Object is basically a tuple of UrlInfos object
+# an UrlInfo object is composed of an url and the filenames containted
+# in the request content
+UrlInfo = namedtuple('UrlInfo', ['url', 'filenames', 'opts'])
+
+road_safety_config = FetcherConfig(
     'road_safety',
-    ("http://data.dft.gov.uk/road-accidents-safety-data/RoadSafetyData_Vehicles_2015.zip",
-     "http://data.dft.gov.uk/road-accidents-safety-data/RoadSafetyData_2015.zip",
-     "http://data.dft.gov.uk/road-accidents-safety-data/MakeModel2015.zip"),
-    ("RoadSafetyData_Vehicles_2015.zip",
-     "RoadSafetyData_2015.zip",
-     "MakeModel2015.zip"),
-    opts={'uncompress': True}
+    (
+        UrlInfo(
+            "http://data.dft.gov.uk/road-accidents-safety-data/"
+            "RoadSafetyData_2015.zip",
+            (
+                "Casualties_2015.csv",
+                "Vehicles_2015.csv",
+                "Accidents_2015.csv"
+            ),
+            {"uncompress": True}
+        ),
+        UrlInfo(
+            "http://data.dft.gov.uk/road-accidents-safety-data/"
+            "MakeModel2015.zip",
+            ("2015_Make_Model.csv",),
+            {"uncompress": True}
+        )
+    )
 )
 
-medical_charge_config = FileFetcherConfig(
+open_payments_config = FetcherConfig(
+    'open_payments',
+    (
+        UrlInfo(
+            'http://download.cms.gov/openpayments/PGYR13_P011718.ZIP',
+            None,
+            {"uncompress": True}
+        ),
+    )
+)
+
+midwest_survey_config = FetcherConfig(
+    'midwest_survey',
+    (
+        UrlInfo(
+            "https://github.com/fivethirtyeight/data/tree/"
+            "master/region-survey/FiveThirtyEight_Midwest_Survey.csv",
+            (
+                "FiveThirtyEight_Midwest_Survey.csv",
+            ),
+            {"uncompress": False}
+        ),
+    )
+)
+medical_charge_config = FetcherConfig(
     'medical_charge',
-    "https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Downloads/Inpatient_Data_2011_CSV.zip",
-    "MedicalProviderChargeInpatient.csv",
-    opts={'uncompress': True}
+    (
+        UrlInfo(
+            "https://www.cms.gov/Research-Statistics-Data-and-Systems/"
+            "Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/"
+            "Downloads/Inpatient_Data_2011_CSV.zip",
+            (
+                "MedicalProviderChargeInpatient.csv",
+            ),
+            {'uncompress': True}
+        ),
+    )
 )
 
-employee_salaries = FileFetcherConfig(
+employee_salaries_config = FetcherConfig(
     'employee_salaries',
-    "https://data.montgomerycountymd.gov/api/views/xj3h-s2i7/rows.csv?accessType=DOWNLOAD",
-    "/Users/pierreglaser/INRIA/dirty_cat/dirty_cat/data/RoadSafetyData_Vehicles_2015.zip",
-    opts={'uncompress': True})
+    (UrlInfo(
+        "https://data.montgomerycountymd.gov/api/views/xj3h-s2i7/rows.csv?accessType=DOWNLOAD",
+        ("rows.csv",),
+        {'uncompress': False}),))
+
+traffic_violations_config = FetcherConfig(
+    'traffic_violations',
+    (
+        UrlInfo(
+            "https://data.montgomerycountymd.gov/api/views/4mse-ku6q/rows.csv?accessType=DOWNLOAD",
+            (
+                "rows.csv",
+            ),
+            {"uncompress": True}),
+
+    )
+)
 
 
 def md5_hash(string):
@@ -127,11 +188,37 @@ def _uncompress_file(file_, delete_archive=True):
         raise IOError('[Compression] unknown archive format {}'.format(ext))
 
 
-def _fetch_file(url, data_dir, overwrite=False,
-                md5sum=None,uncompress=True):
+def _check_if_exists(path, remove=False):
+    if remove:
+        try:
+            os.remove(path)
+        except OSError:
+            pass
+        return False
+    else:
+        return os.path.exists(path)
+
+
+def fetch_dataset(configfile: FetcherConfig):
+    data_dir = os.path.join(get_data_dir(), configfile.dataset_name)
+    for urlinfo in configfile.urlinfos:
+        _fetch_file(urlinfo.url, data_dir, filenames=urlinfo.filenames,
+                    uncompress=urlinfo.opts['uncompress'])
+
+
+def _fetch_file(url, data_dir, filenames=None, overwrite=False,
+                md5sum=None, uncompress=True):
     """
     fetching function that load the requested file,
     downloading it if needed or requested
+
+
+    IF the downloaded file is compressed, then the fetcher
+    looks also for the uncompressed files before downloading .
+    This only works for a one-to-one/one-to many configuration when one zipped file
+    gives one or several uncompressed files. but we don't handle many-to-many yet
+
+    maybe a better behavior is possible
     :param url:
     :param data_dir:
 
@@ -154,100 +241,85 @@ def _fetch_file(url, data_dir, overwrite=False,
     # provide parsing tools
     parse = urllib.parse.urlparse(url)
     file_name = os.path.basename(parse.path)
+    download = False
     if file_name == '':
         file_name = md5_hash(parse.path)
 
     temp_file_name = file_name + ".part"
     full_name = os.path.join(data_dir, file_name)
     temp_full_name = os.path.join(data_dir, temp_file_name)
-    if os.path.exists(full_name):
-        if overwrite:
-            os.remove(full_name)
-        else:
-            return full_name
-    if os.path.exists(temp_full_name):
-        if overwrite:
-            os.remove(temp_full_name)
+    if overwrite:
+        download = True
+        for name in [file_name, temp_file_name, *filenames]:
+            # remove all compressed/uncompressed files
+            _check_if_exists(os.path.join(data_dir, name), remove=True)
+    else:
+        # if no filenames info provided, overwrite
+        is_file_missing = True
+        if filenames is not None:
+            # first look for uncompressed files
+            is_file_missing = any(
+                [not _check_if_exists(os.path.join(data_dir, name),
+                                      remove=False)
+                 for name in filenames])
+        if is_file_missing:
+            # then look for compressed files
+            if not _check_if_exists(full_name, remove=False):
+                download = True
 
-    try:
-        # using stream=True to download the response body only when accessing
-        # the content attribute
-        with requests.get(url, stream=True) as r:
-            total_length = r.headers.get('Content-Length')
-            if total_length is not None:
-                with open(temp_full_name, 'wb') as local_file:
-                    for chunk in progress.bar(r.iter_content(chunk_size=1024),
-                                              expected_size=(int(total_length) /
-                                                             1024) + 1):
-                        if chunk:
-                            local_file.write(chunk)
-                            local_file.flush()
-            else:
-                warnings.warn('content size cannot be found, '
-                              'downloading file from {} as a whole'.format(url))
-                with open(temp_full_name, 'wb') as local_file:
-                    local_file.write(r.content)
+    if download:
+        try:
+            # using stream=True to download the response body only when accessing
+            # the content attribute
+            with requests.get(url, stream=True) as r:
+                total_length = r.headers.get('Content-Length')
+                if total_length is not None:
+                    with open(temp_full_name, 'wb') as local_file:
+                        for chunk in progress.bar(
+                                r.iter_content(chunk_size=1024),
+                                expected_size=(int(total_length) /
+                                               1024) + 1):
+                            if chunk:
+                                local_file.write(chunk)
+                                local_file.flush()
+                else:
+                    warnings.warn('content size cannot be found, '
+                                  'downloading file from {} as a whole'.format(
+                        url))
+                    with open(temp_full_name, 'wb') as local_file:
+                        local_file.write(r.content)
 
-    except requests.RequestException as e:
-        # pretty general request exception. subject to change
-        raise Exception('error while fetching: {}'.format(e))
+        except requests.RequestException as e:
+            # pretty general request exception. subject to change
+            raise Exception('error while fetching: {}'.format(e))
 
     # chunk writing is not implemented, see if necessary
+    if _check_if_exists(temp_full_name, remove=False):
+        if md5sum is not None:
+            if (_md5_sum_file(temp_full_name) != md5sum):
+                raise ValueError("File %s checksum verification has failed."
+                                 "Dataset fetching aborted." % temp_full_name)
 
-    if md5sum is not None:
-        if (_md5_sum_file(temp_full_name) != md5sum):
-            raise ValueError("File %s checksum verification has failed."
-                             " Dataset fetching aborted." % local_file)
-
-    shutil.move(temp_full_name, full_name)
-    if uncompress:
-        _uncompress_file(full_name,delete_archive=True)
+        shutil.move(temp_full_name, full_name)
+    if _check_if_exists(full_name, remove=False) and uncompress:
+        _uncompress_file(full_name, delete_archive=True)
 
     return full_name
 
 
-def fetch_employee_salaries():
-    '''
-    Function that returns the path to "employee_salaries" example dataset.
-
-    Returns
-    ========
-
-    filename: string
-        The absolute path to the CSV file
-
-    Notes
-    =====
-
-    The original data is retrieved from:
-    https://catalog.data.gov/dataset/employee-salaries-2016
-    '''
-
-    url = "https://data.montgomerycountymd.gov/api/views/xj3h-s2i7/rows.csv?accessType=DOWNLOAD"
-    local_path = '/Users/pierreglaser/INRIA/dirty_cat/dirty_cat/data'
-
-    final_path = _fetch_file(url, local_path)
-    return final_path
-
-
-def fetch_road_safety():
-    data_dir = get_data_dir()
-    for url in road_safety_config.urls:
-        _fetch_file(url, data_dir,uncompress=road_safety_config.opts['uncompress'])
-    return
-
-
-def get_data_dir():
+def get_data_dir(dataset_name=None):
     """ Returns the directories in which nilearn looks for data.
     This is typically useful for the end-user to check where the data is
     downloaded and stored.
 
     *** taken from nilearn.datasets.utils ***
     """
-    return os.path.join(os.path.dirname(__file__),'data')
-
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
+    if dataset_name is not None:
+        data_dir = os.path.join(data_dir, dataset_name)
+    return data_dir
 
 
 if __name__ == '__main__':
-    data_dir=get_data_dir()
-    fetch_road_safety()
+    pass
+    # fetch_dataset(medical_charge_config)
