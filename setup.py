15030b644c3fdaa7b4b86ebdf231242815378c5b
CERDA REYES Patricio <patricio.cerda@inria.fr>
2018-09-20 14:28:30 +0200

    add find_packages to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 import sys
 
 # For some commands, use setuptools
-if len(set(('develop', 'sdist', 'release', 'bdist', 'bdist_egg', 'bdist_dumb',
-            'bdist_rpm', 'bdist_wheel', 'bdist_wininst', 'install_egg_info',
-            'egg_info', 'easy_install', 'upload',
-            )).intersection(sys.argv)) > 0:
-    import setuptools
 
 version_file = os.path.join(
     os.path.dirname(__file__), 'dirty_cat', 'VERSION.txt')
@@ -43,7 +38,7 @@ if __name__ == '__main__':
               'Topic :: Software Development :: Libraries',
           ],
           platforms='any',
-          packages=['dirty_cat'],
-          package_data={'dirty_cat': ['VERSION.txt', 'data/*.csv.gz']},
+          packages=find_packages(),
+          package_data={'dirty_cat': ['VERSION.txt', 'data/*.csv']},
           install_requires=['sklearn', 'numpy', 'scipy', 'requests'],
           **extra_setuptools_args)
