719ad416ede7344945c72471b0b728bffef8156c
dsleo <leooleds@gmail.com>
2018-11-08 14:37:22 +0100

    Added test for n_prototypes

--- dirty_cat/test/test_similarity_encoder.py
+++ dirty_cat/test/test_similarity_encoder.py
@@ -3,13 +3,14 @@ import numpy as np
 from dirty_cat import similarity_encoder, string_distances
 
 
-def _test_similarity(similarity, similarity_f, hashing_dim=None):
+def _test_similarity(similarity, similarity_f, hashing_dim=None, categories='auto', n_prototypes=None):
     X = np.array(['aa', 'aaa', 'aaab']).reshape(-1, 1)
     X_test = np.array([['Aa', 'aAa', 'aaa', 'aaab', ' aaa  c']]).reshape(-1, 1)
 
     model = similarity_encoder.SimilarityEncoder(
         similarity=similarity, handle_unknown='ignore',
-        hashing_dim=hashing_dim)
+        hashing_dim=hashing_dim, categories=categories,
+        n_prototypes=n_prototypes)
 
     encoder = model.fit(X).transform(X_test)
 
@@ -25,8 +26,18 @@ def _test_similarity(similarity, similarity_f, hashing_dim=None):
 
 def test_similarity_encoder():
 
-    _test_similarity('levenshtein-ratio', string_distances.levenshtein_ratio)
-    _test_similarity('jaro-winkler', string_distances.jaro_winkler)
-    _test_similarity('jaro', string_distances.jaro)
-    _test_similarity('ngram', string_distances.ngram_similarity)
-    _test_similarity('ngram', string_distances.ngram_similarity, hashing_dim=2**16)
+    categories = ['auto', 'most_frequent']
+
+    for category in categories:
+        if category=='auto':
+            _test_similarity('levenshtein-ratio', string_distances.levenshtein_ratio, categories=category, n_prototypes=None)
+            _test_similarity('jaro-winkler', string_distances.jaro_winkler, categories=category, n_prototypes=None)
+            _test_similarity('jaro', string_distances.jaro, categories=category, n_prototypes=None)
+            _test_similarity('ngram', string_distances.ngram_similarity, categories=category, n_prototypes=None)
+            _test_similarity('ngram', string_distances.ngram_similarity, hashing_dim=2**16, categories=category, n_prototypes=None)
+        else:
+            _test_similarity('levenshtein-ratio', string_distances.levenshtein_ratio, categories=category, n_prototypes=1)
+            _test_similarity('jaro-winkler', string_distances.jaro_winkler, categories=category, n_prototypes=1)
+            _test_similarity('jaro', string_distances.jaro, categories=category, n_prototypes=1)
+            _test_similarity('ngram', string_distances.ngram_similarity, categories=category, n_prototypes=1)
+            _test_similarity('ngram', string_distances.ngram_similarity, hashing_dim=2**16, categories=category, n_prototypes=1)
