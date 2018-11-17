72b4c4ed30e8fed61c2b906dab7255bd9e1a503c
Maxime Cuny <maximehenri@hotmail.fr>
2018-11-17 14:26:33 +0100

    Corrected bad parameter style when calling kmean strategy

--- dirty_cat/similarity_encoder.py
+++ dirty_cat/similarity_encoder.py
@@ -238,7 +238,7 @@ class SimilarityEncoder(_BaseEncoder):
             elif self.categories == 'most_frequent':
                 self.categories_.append(self.get_most_frequent(Xi))
             elif self.categories == 'k-means':
-                self.categories_.append(get_kmeans_protoypes(Xi, self.n_prototypes, self.random_state_))
+                self.categories_.append(get_kmeans_protoypes(Xi, self.n_prototypes, random_state=self.random_state_))
             else:
                 if self.handle_unknown == 'error':
                     valid_mask = np.in1d(Xi, self.categories[i])
