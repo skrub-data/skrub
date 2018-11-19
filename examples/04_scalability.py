80154a44fb2866964c28a9ac1d3295e021dace11
Gael Varoquaux <gael.varoquaux@normalesup.org>
2018-11-19 17:26:54 +0100

    improve example

--- examples/04_scalability.py
+++ examples/04_scalability.py
@@ -1,21 +1,27 @@
 """
-Scalability considerations for  similarity encoding
+Scalability considerations for similarity encoding
 ===================================================
 
+Here we discuss how to apply efficiently SimilarityEncoder to larger
+datasets: reducing the number of reference categories to "prototypes",
+either chosen as the most frequent categories, or with kmeans clustering.
+
 """
+
+# Avoid the warning in scikit-learn's LogisticRegression for the change
+# in the solver
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 ################################################################################
-# A tool to report memory usage and run time of a function
-# ---------------------------------------------------------
+# A tool to report memory usage and run time
+# -------------------------------------------
 #
-# For the sake of this example, we build a small tool that reports memory
+# For this example, we build a small tool that reports memory
 # usage and compute time of a function
 from time import time
 import functools
 import memory_profiler
-
 
 
 def resource_used(func):
@@ -59,7 +65,7 @@ df = df.reset_index()
 print(df['Description'].nunique())
 
 ################################################################################
-print(df['Description'].value_counts()[:30])
+print(df['Description'].value_counts()[:20])
 
 ################################################################################
 # As we will see,SimilarityEncoder takes a while on such data
@@ -111,57 +117,83 @@ from sklearn import linear_model, pipeline, model_selection
 log_ref = linear_model.LogisticRegression()
 
 model = pipeline.make_pipeline(column_trans, log_ref)
-print("Cross-validation score: %s" %
-      resource_used(model_selection.cross_val_score)(model, df, y))
+results = resource_used(model_selection.cross_validate)(model, df, y)
+print("Cross-validation score: %s" % results['test_score'])
+
+################################################################################
+# Store results for later
+scores = dict()
+scores['Default options'] = results['test_score']
+times = dict()
+times['Default options'] = results['fit_time']
 
 
 ################################################################################
-# SimilarityEncoder with most frequent strategy
+# Most frequent strategy to define prototypes
 # ---------------------------------------------
 #
 # The most frequent strategy selects the n most frequent values in a dirty
 # categorical variable to reduce the dimensionality of the problem and thus
 # speed things up. We select manually the number of prototypes we want to use.
-sim_enc = SimilarityEncoder(similarity='ngram', categories='most_frequent', n_prototypes=100)
+sim_enc = SimilarityEncoder(similarity='ngram', categories='most_frequent',
+                            n_prototypes=100)
 
 column_trans = ColumnTransformer(
     # adding the dirty column
     transformers=transformers + [('sim_enc', sim_enc, ['Description'])],
     remainder='drop')
 
-t0 = time()
-X = column_trans.fit_transform(df)
-t1 = time()
-print('Time to vectorize: %s' % (t1 - t0))
-
 ################################################################################
 # Check now that prediction is still as good
 model = pipeline.make_pipeline(column_trans, log_ref)
-print("Cross-validation score: %s" %
-      resource_used(model_selection.cross_val_score)(model, df, y))
+results = resource_used(model_selection.cross_validate)(model, df, y)
+print("Cross-validation score: %s" % results['test_score'])
+
+################################################################################
+# Store results for later
+scores['Most frequent'] = results['test_score']
+times['Most frequent'] = results['fit_time']
 
 
 ################################################################################
-# SimilarityEncoder with k-means strategy
+# KMeans strategy to define prototypes
 # ---------------------------------------
 #
-# K-means strategy is also a dimensionality reduction technique. But we apply
-# a K-means and nearest neighbors algorithm to find the prototypes. The number
-# of prototypes is set manually.
-sim_enc = SimilarityEncoder(similarity='ngram', categories='k-means', n_prototypes=100)
+# K-means strategy is also a dimensionality reduction technique.
+# SimilarityEncoder can apply a K-means and nearest neighbors algorithm
+# to find the prototypes. The number of prototypes is set manually.
+sim_enc = SimilarityEncoder(similarity='ngram', categories='k-means',
+                            n_prototypes=100)
 
 column_trans = ColumnTransformer(
     # adding the dirty column
     transformers=transformers + [('sim_enc', sim_enc, ['Description'])],
     remainder='drop')
 
-t0 = time()
-X = column_trans.fit_transform(df)
-t1 = time()
-print('Time to vectorize: %s' % (t1 - t0))
-
 ################################################################################
 # Check now that prediction is still as good
 model = pipeline.make_pipeline(column_trans, log_ref)
-print("cross-validation score: %s" %
-      resource_used(model_selection.cross_val_score)(model, df, y))
+results = resource_used(model_selection.cross_validate)(model, df, y)
+print("Cross-validation score: %s" % results['test_score'])
+
+################################################################################
+# Store results for later
+scores['KMeans'] = results['test_score']
+times['KMeans'] = results['fit_time']
+
+
+################################################################################
+# Plot a summary figure
+# ----------------------
+import seaborn
+import matplotlib.pyplot as plt
+_, (ax1, ax2) = plt.subplots(nrows=2, figsize=(4, 3))
+seaborn.boxplot(data=pd.DataFrame(scores), orient='h', ax=ax1)
+ax1.set_xlabel('Prediction accuracy', size=16)
+[t.set(size=16) for t in ax1.get_yticklabels()]
+
+seaborn.boxplot(data=pd.DataFrame(times), orient='h', ax=ax2)
+ax2.set_xlabel('Computation time', size=16)
+[t.set(size=16) for t in ax2.get_yticklabels()]
+plt.tight_layout()
+
