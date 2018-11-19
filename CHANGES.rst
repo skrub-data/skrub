96a8fc015eba5272ab51c6f205e15185e0936c9c
Gael Varoquaux <gael.varoquaux@normalesup.org>
2018-11-19 23:34:29 +0100

    REL 0.0.5
    Another fix in the dependencies

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,4 +1,4 @@
-Release 0.0.4
+Release 0.0.5
 =============
 
 * **SimilarityEncoder**: Change the default ngram range to (2, 4) which
--- setup.py
+++ setup.py
@@ -36,5 +36,5 @@ if __name__ == '__main__':
           platforms='any',
           packages=find_packages(),
           package_data={'dirty_cat': ['VERSION.txt', 'data/midwest_survey/*.csv']},
-          install_requires=['sklearn>=0.20', 'numpy', 'scipy', 'requests'],
+          install_requires=['scikit-learn>=0.20', 'numpy', 'scipy', 'requests'],
           )
