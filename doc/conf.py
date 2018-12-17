f16f164f492109ecba26a238e88d9c1c842b08fd
Pierre Glaser <pierreglaser@msn.com>
2018-12-17 12:13:15 +0100

    DOC change note cell background

--- doc/conf.py
+++ doc/conf.py
@@ -137,6 +137,7 @@ html_theme_options = {
     'link': "#076B00",
 #    'gray_3': "#090",
     'fixed_sidebar': 'true',
+    'note_bg': "#E9E9E9"
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
