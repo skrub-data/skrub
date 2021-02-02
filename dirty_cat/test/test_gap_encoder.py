32a04f05d122c63e0da3e6b03d65667b43be41b9
Alexis CVETKOV-ILIEV <alexis.cvetkov-iliev@institutoptique.fr>
2021-02-02 16:32:28 +0100

    added test for get_feature_names

--- dirty_cat/test/test_gap_encoder.py
+++ dirty_cat/test/test_gap_encoder.py
@@ -37,18 +37,48 @@ def test_gap_encoder(hashing, init, analyzer, add_words, n_samples=70):
     encoder.fit(X)
     y2 = encoder.transform(X)
     np.testing.assert_array_equal(y, y2)
+    return
 
 
 def test_input_type():
     # Numpy array
     X = np.array(['alice', 'bob'])
-    enc = GapEncoder(n_components=2)
-    enc.fit_transform(X)
+    enc = GapEncoder(n_components=2, random_state=42)
+    X_enc_array = enc.fit_transform(X)
     # List
     X = ['alice', 'bob']
-    enc = GapEncoder(n_components=2)
-    enc.fit_transform(X)
+    enc = GapEncoder(n_components=2, random_state=42)
+    X_enc_list = enc.fit_transform(X)
+    # Check if the encoded vectors are the same
+    np.testing.assert_array_equal(X_enc_array, X_enc_list)
+    return
 
+
+def test_partial_fit(n_samples=70):
+    X_txt = fetch_20newsgroups(subset='train')['data']
+    X = X_txt[:n_samples]
+    # Gap encoder with fit on one batch
+    enc = GapEncoder(random_state=42, batch_size=n_samples, max_iter=1)
+    X_enc = enc.fit_transform(X)
+    # Gap encoder with partial fit
+    enc = GapEncoder(random_state=42)
+    enc.partial_fit(X)
+    X_enc_partial = enc.transform(X)
+    # Check if the encoded vectors are the same
+    np.testing.assert_array_equal(X_enc, X_enc_partial)
+    return
+
+
+def test_get_feature_names(n_samples=70):
+    X_txt = fetch_20newsgroups(subset='train')['data']
+    X = X_txt[:n_samples]
+    enc = GapEncoder()
+    enc.fit(X)
+    topic_labels = enc.get_feature_names()
+    # Check number of labels
+    assert len(topic_labels) == enc.n_components
+    return
+    
 
 def profile_encoder(Encoder, init):
     # not an unit test
@@ -73,6 +103,12 @@ if __name__ == '__main__':
     print('start test_input_type')
     test_input_type()
     print('test_input_type passed')
+    print('start test_partial_fit')
+    test_partial_fit()
+    print('test_partial_fit passed')
+    print('start test_get_feature_names')
+    test_get_feature_names()
+    print('test_get_feature_names passed')
     
     for _ in range(3):
         print('time profile_encoder(GapEncoder, init="k-means++")')
