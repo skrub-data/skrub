5476cbbe8a3e004762e5401178b45a53c29aa0d5
Lilian <lilian@boulard.fr>
2021-02-04 14:16:08 +0100

    Fix import spacing

--- dirty_cat/datasets/__init__.py
+++ dirty_cat/datasets/__init__.py
@@ -2,14 +2,14 @@
 Datasets module of dirty cat
 """
 
-from.fetching import get_data_dir
-from.fetching import fetch_medical_charge
-from.fetching import fetch_midwest_survey
-from.fetching import fetch_employee_salaries
-from.fetching import fetch_road_safety
-from.fetching import fetch_open_payments
-from.fetching import fetch_drug_directory
-from.fetching import fetch_traffic_violations
+from .fetching import get_data_dir
+from .fetching import fetch_medical_charge
+from .fetching import fetch_midwest_survey
+from .fetching import fetch_employee_salaries
+from .fetching import fetch_road_safety
+from .fetching import fetch_open_payments
+from .fetching import fetch_drug_directory
+from .fetching import fetch_traffic_violations
 
 __all__ = [
     'get_data_dir',
