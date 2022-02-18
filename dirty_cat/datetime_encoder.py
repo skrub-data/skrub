a7e05e7ae878e8aab4a1d22e3f6222abd9c97cd9
LeoGrin <leo.grinsztajn@polytechnique.edu>
2022-02-18 17:57:24 +0100

    add holidays option + gather time below extract_until into one numerical feature

--- dirty_cat/datetime_encoder.py
+++ dirty_cat/datetime_encoder.py
@@ -1,34 +1,52 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import numpy as np
 import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
+
 
 
 class DatetimeEncoder(TransformerMixin, BaseEstimator):
+    """
+    Parameters
+    ----------
+    extract_until : {"year", "month", "day", "hour", "minute", "second",
+    "millisecond", "microsecond", "nanosecond"}, default="hour"
+        Extract up to this granularity. For instance, if you specify "day", only "year", "month", and "day"
+        features will be created. The rest will me gathered into the "other" feature.
+    add_day_of_the_week: bool, default=True
+        Add day of the week feature (if day is extracted).
+    add_holidays : bool, default=False
+        Whether to add a categorical variable encoding if the day of the date is a holiday (and if day is extracted).
+        Uses pandas calendar, which for now only supports US holidays.
+    """
     def __init__(self,
-                 stop_extraction="minute",
+                 extract_until="hour",
                  add_day_of_the_week=True,
                  add_holidays=False):
         # TODO validate_keywords
         # TODO doc
-        """
-        Parameters
-        ----------
-        stop_extraction : {"year", "month", "day", "hour", "minute", "second",
-        "millisecond", "microsecond", "nanosecond"}, default="minutes"
-            Extract up to this granularity. For instance, if you specify "days", only "year", "month", and "day"
-            features will be created.
-        add_day_of_the_week: bool, default=True
-            Add day of the week feature (if day is extracted).
-        add_holidays : bool, default=False
-            Whether to add a categorical variable encoding if the day of the date is a holiday. For now uses pandas
-            calendar, which only support US holidays.
-        """
         to_extract = ["year", "month", "day", "hour", "minute", "second", "millisecond", "microsecond", "nanosecond"]
-        self.to_extract_full = to_extract[:to_extract.index(stop_extraction) + 1]
+        self.extract_until = extract_until
+        self._validate_keywords()
+        self.to_extract_full = to_extract[:to_extract.index(extract_until) + 1]
+        self.to_extract_full.append("other")
         self.add_day_of_the_week = add_day_of_the_week
         self.add_holidays = add_holidays
         # number of new columns created per feature, before removing constant columns
         self.n_features_per_col_full = len(self.to_extract_full) + self.add_day_of_the_week + self.add_holidays
+
+        self.word_to_alias = {"year": "Y", "month": "M", "day": "D", "hour": "H", "minute": "min", "second": "S",
+                              "millisecond": "ms", "microsecond": "us", "nanosecond": "N"}
+
+    def _validate_keywords(self):
+        if self.extract_until not in ["year", "month", "day", "hour", "minute", "second", "millisecond", "microsecond", "nanosecond"]:
+            msg = (
+                """extract_until should be one of ["year", "month", "day", "hour", "minute", "second", "millisecond", 
+                "microsecond", "nanosecond"], got {0}.""".format(
+                    self.extract_until
+                )
+            )
+            raise ValueError(msg)
 
     def _extract_from_date(self, date_series, feature):
         if feature == "year":
@@ -52,23 +70,16 @@ class DatetimeEncoder(TransformerMixin, BaseEstimator):
         elif feature == "dayofweek":
             return pd.DatetimeIndex(date_series).dayofweek.to_numpy()
         elif feature == "holiday":
-            return None
-
-    def _create_datetime_features(self, X):
-        """Create datetime features
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, 1)
-            The input samples, containing 1 datetime column.
-        Returns
-        -------
-        X_new : array-like of shape (n_samples, n_features_out)
-            The transformed dataset.
-        """
-        X_ = np.empty((X.shape[0], self.n_features_per_col_full), dtype=np.int64)
-        for i, feature in enumerate(self.to_extract):
-            X_[:, i] = self._extract_from_date(X, feature)
-        return X_
+            # Create an indicator for holidays
+            cal = calendar()
+            holidays = cal.holidays(start=date_series.min(), end=date_series.max())
+            return np.isin(date_series, holidays).astype(int)
+        elif feature == "other":
+            # Gather all the variables below the extract_until into one numerical variable
+            res = (date_series - pd.to_datetime(pd.DatetimeIndex(date_series).floor(
+                self.word_to_alias[self.extract_until]))).to_numpy()
+            # Convert to the extract_until unit (e.g if I extract until "minute", then convert to minutes)
+            return res / pd.to_timedelta(1, self.word_to_alias[self.extract_until])
 
     def fit(self, X, y=None):
         self.to_extract = {}  # Features to extract for each column, after removing constant features
@@ -87,8 +98,11 @@ class DatetimeEncoder(TransformerMixin, BaseEstimator):
 
         self.n_features_out = len(np.concatenate(list(self.to_extract.values())))
 
+        return self
+
     def transform(self, X, y=None):
-        X_ = np.empty((X.shape[0], self.n_features_out), dtype=np.int64)
+        # Create a new dataframe with the extracted features, choosing only features that weren't constant during fit
+        X_ = np.empty((X.shape[0], self.n_features_out), dtype=np.float64)
         idx = 0
         for i in range(X.shape[1]):
             for j, feature in enumerate(self.to_extract[i]):
--- dirty_cat/test/test_datetime_encoder.py
+++ dirty_cat/test/test_datetime_encoder.py
@@ -3,37 +3,98 @@ from dirty_cat.datetime_encoder import DatetimeEncoder
 import numpy as np
 
 
-def get_datetimes_array():
+def get_date_array():
     return np.array([pd.to_datetime(["2020-01-01", "2020-01-02", "2020-01-03"]),
                   pd.to_datetime(["2021-02-03", "2020-02-04", "2021-02-05"]),
-                  pd.to_datetime(["2022-01-01", "2020-01-02", "2022-01-03"]),
+                  pd.to_datetime(["2022-01-01", "2020-12-25", "2022-01-03"]),
                   pd.to_datetime(["2023-02-03", "2020-02-04", "2023-02-05"])])
+
+def get_datetime_array():
+    return np.array([pd.to_datetime(["2020-01-01 10:12:01", "2020-01-02 10:23:00", "2020-01-03 10:00:00"]),
+                  pd.to_datetime(["2021-02-03 12:45:23", "2020-02-04 22:12:00", "2021-02-05 12:00:00"]),
+                  pd.to_datetime(["2022-01-01 23:23:43", "2020-12-25 11:12:00", "2022-01-03 11:00:00"]),
+                  pd.to_datetime(["2023-02-03 11:12:12", "2020-02-04 08:32:00", "2023-02-05 23:00:00"])])
 
 def test_fit():
-    X = np.array([pd.to_datetime(["2020-01-01", "2020-01-02", "2020-01-03"]),
-                  pd.to_datetime(["2021-02-03", "2020-02-04", "2021-02-05"]),
-                  pd.to_datetime(["2022-01-01", "2020-01-02", "2022-01-03"]),
-                  pd.to_datetime(["2023-02-03", "2020-02-04", "2023-02-05"])])
-
-    expected_to_extract_full = ["year", "month", "day", "hour", "minute"]
+    # Dates
+    X = get_date_array()
+    enc = DatetimeEncoder()
+    expected_to_extract_full = ["year", "month", "day", "hour", "other"]
     expected_to_extract = {0: ["year", "month", "day", "dayofweek"],
                            1: ["month", "day", "dayofweek"],
                            2: ["year", "month", "day", "dayofweek"]}
-
-    enc = DatetimeEncoder()
     enc.fit(X)
+    assert enc.to_extract_full == expected_to_extract_full
+    assert enc.to_extract == expected_to_extract
 
+    X = get_date_array()
+    enc = DatetimeEncoder(add_holidays=True)
+    expected_to_extract_full = ["year", "month", "day", "hour", "other"]
+    expected_to_extract = {0: ["year", "month", "day", "dayofweek", "holiday"],
+                           1: ["month", "day", "dayofweek", "holiday"],
+                           2: ["year", "month", "day", "dayofweek", "holiday"]}
+    enc.fit(X)
+    assert enc.to_extract_full == expected_to_extract_full
+    assert enc.to_extract == expected_to_extract
+
+
+    # Datetimes
+    X = get_datetime_array()
+    enc = DatetimeEncoder()
+    expected_to_extract_full = ["year", "month", "day", "hour", "other"]
+    expected_to_extract = {0: ["year", "month", "day", "hour", "other", "dayofweek"],
+                           1: ["month", "day", "hour", "other", "dayofweek"],
+                           2: ["year", "month", "day", "hour", "dayofweek"]}
+    enc.fit(X)
+    assert enc.to_extract_full == expected_to_extract_full
+    assert enc.to_extract == expected_to_extract
+
+    X = get_datetime_array()
+    enc = DatetimeEncoder(extract_until="minute")
+    expected_to_extract_full = ["year", "month", "day", "hour", "minute", "other"]
+    expected_to_extract = {0: ["year", "month", "day", "hour", "minute", "other", "dayofweek"],
+                           1: ["month", "day", "hour", "minute", "dayofweek"],
+                           2: ["year", "month", "day", "hour", "dayofweek"]}
+    enc.fit(X)
     assert enc.to_extract_full == expected_to_extract_full
     assert enc.to_extract == expected_to_extract
 
 
 def test_transform():
-    X = get_datetimes_array()
+    # Dates
+    X = get_date_array()
+    enc = DatetimeEncoder()
     expected_result = np.array([[2020, 1, 1, 2, 1, 2, 3, 2020, 1, 3, 4],
                                 [2021, 2, 3, 2, 2, 4, 1, 2021, 2, 5, 4],
-                                [2022, 1, 1, 5, 1, 2, 3, 2022, 1, 3, 0],
+                                [2022, 1, 1, 5, 12, 25, 4, 2022, 1, 3, 0],
                                 [2023, 2, 3, 4, 2, 4, 1, 2023, 2, 5, 6]])
-
-    enc = DatetimeEncoder()
     enc.fit(X)
-    assert (enc.transform(X) == expected_result).all()
+    assert np.array_equal(enc.transform(X), expected_result)
+
+    enc = DatetimeEncoder(add_day_of_the_week=False)
+    expected_result = np.array([[2020, 1, 1, 1, 2, 2020, 1, 3],
+                                [2021, 2, 3, 2, 4, 2021, 2, 5],
+                                [2022, 1, 1, 12, 25, 2022, 1, 3],
+                                [2023, 2, 3, 2, 4, 2023, 2, 5]])
+    enc.fit(X)
+    assert np.array_equal(enc.transform(X), expected_result)
+
+    enc = DatetimeEncoder(add_holidays=True)
+    expected_result = np.array([[2020, 1, 1, 2, 1, 1, 2, 3, 0, 2020, 1, 3, 4, 0],
+                                [2021, 2, 3, 2, 0, 2, 4, 1, 0, 2021, 2, 5, 4, 0],
+                                [2022, 1, 1, 5, 0, 12, 25, 4, 1, 2022, 1, 3, 0, 0],
+                                [2023, 2, 3, 4, 0, 2, 4, 1, 0, 2023, 2, 5, 6, 0]])
+    enc.fit(X)
+    assert np.array_equal(enc.transform(X), expected_result)
+
+    #Datetimes
+    X = get_datetime_array()[:, 0].reshape(-1, 1)
+    enc = DatetimeEncoder()
+    # Check that the "other" feature is the right value and unit
+    expected_result = np.array([[2020, 1, 1, 10, 12 / 60 + 1 / 3600, 2],
+                                [2021, 2, 3, 12, 45 / 60 + 23 / 3600, 2],
+                                [2022, 1, 1, 23, 23 / 60 + 43 / 3600, 5],
+                                [2023, 2, 3, 11, 12 / 60 + 12 / 3600, 4]])
+    enc.fit(X)
+    X_trans = enc.transform(X)
+    assert np.allclose(X_trans, expected_result)
