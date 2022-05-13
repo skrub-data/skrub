a501995089a8ad1b1a5dfb0e7b08e3a41acd3b5e
STOJANOVIC Jovan <jovan.stojanovic@inria.fr>
2022-05-13 17:47:37 +0200

    Add Python 3.10

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ if __name__ == '__main__':
               'Programming Language :: Python :: 3.7',
               'Programming Language :: Python :: 3.8',
               'Programming Language :: Python :: 3.9',
+              'Programming Language :: Python :: 3.10',
               'Topic :: Scientific/Engineering',
               'Topic :: Software Development :: Libraries',
           ],
