e1e174da59e3e05c133b39f1204505c9c4d351f9
LeoGrin <leo.grinsztajn@polytechnique.edu>
2022-06-29 19:58:58 +0200

    fix test bug

--- dirty_cat/test/test_minhash_encoder.py
+++ dirty_cat/test/test_minhash_encoder.py
@@ -16,17 +16,17 @@ def test_MinHashEncoder(n_sample=70, minmax_hash=False):
         for hashing in ['fast', 'murmur']:
 
             if minmax_hash and hashing == 'murmur':
-                pass # not implemented
+                continue # not implemented
 
             # Test output shape
-            encoder = MinHashEncoder(n_components=50, hashing=hashing)
+            encoder = MinHashEncoder(n_components=50, hashing=hashing, minmax_hash=minmax_hash)
             encoder.fit(X)
             y = encoder.transform(X)
             assert y.shape == (n_sample, 50), str(y.shape)
             assert len(set(y[0])) == 50
 
             # Test same seed return the same output
-            encoder = MinHashEncoder(50, hashing=hashing)
+            encoder = MinHashEncoder(50, hashing=hashing, minmax_hash=minmax_hash)
             encoder.fit(X)
             y2 = encoder.transform(X)
             np.testing.assert_array_equal(y, y2)
@@ -35,7 +35,7 @@ def test_MinHashEncoder(n_sample=70, minmax_hash=False):
             if not minmax_hash:
                 X_substring = [x[:x.find(' ')] for x in X[:,0]]
                 X_substring = np.array(X_substring)[:,None]
-                encoder = MinHashEncoder(50, hashing=hashing)
+                encoder = MinHashEncoder(50, hashing=hashing, minmax_hash=minmax_hash)
                 encoder.fit(X_substring)
                 y_substring = encoder.transform(X_substring)
                 np.testing.assert_array_less(y - y_substring, 0.0001)
