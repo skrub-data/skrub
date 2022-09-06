c74b887ec2467af0832d4aa50ec54d42085a6a79
Moritz Boos <m.boos@eyeo.com>
2022-09-06 20:05:58 +0100

    add docstrings

--- dirty_cat/deduplicate.py
+++ dirty_cat/deduplicate.py
@@ -1,15 +1,17 @@
 """
-Implements deduplication
+Implements deduplication based on clustering string similarity matrices.
+This works best if there is a number of underlying categories that
+sometimes appear in the data with small variations and/or misspellings.
 """
-from typing import Sequence, Optional, Tuple, List, Union
-from numpy.typing import NDArray
+from typing import List, Optional, Sequence, Tuple
+
 import numpy as np
 import pandas as pd
+from numpy.typing import NDArray
 from scipy.cluster.hierarchy import fcluster, linkage
-from scipy.spatial.distance import squareform
-from sklearn.metrics import silhouette_score
+from scipy.spatial.distance import pdist, squareform
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-from sklearn.preprocessing import minmax_scale
+from sklearn.metrics import silhouette_score
 
 
 def deduplicate(
@@ -17,77 +19,101 @@ def deduplicate(
     n_clusters: Optional[int] = None,
     ngram_range: Tuple[int, int] = (2, 4),
     analyzer: str = "char_wb",
-    return_corrections: bool = False,
     method: str = "average",
-) -> Union[List[str], Tuple[List[str], pd.Series]]:
-    """
+) -> List[str]:
+    """Deduplicates data by computing the n-gram similarity between unique
+    categories in data, performing hierarchical clustering on this similarity
+    matrix, and choosing the most frequent element in each cluster as the
+    'correct' spelling. This method works best if the true number of
+    categories is significantly smaller than the number of observed spellings.
 
-    :param data: Sequence of words with 
-    :type data: Sequence
-    :param n_clusters: _description_, defaults to None
-    :type n_clusters: Optional[int], optional
-    :param ngram_range: _description_, defaults to (2, 4)
-    :type ngram_range: Tuple[int, int], optional
-    :param analyzer: _description_, defaults to "char_wb"
-    :type analyzer: str, optional
-    :param return_corrections: _description_, defaults to False
-    :type return_corrections: bool, optional
-    :param method: _description_, defaults to "average"
-    :type method: str, optional
-    :return: _description_
-    :rtype: _type_
+    Parameters
+    ----------
+    data : Sequence[str]
+        The data to be deduplicated.
+    n_clusters : Optional[int], optional
+        number of clusters to use for hierarchical clustering, if None use the
+        number of clusters that lead to the lowest silhouette score,
+        by default None
+    ngram_range : Tuple[int, int], optional
+        range to use for computing n-gram similarity, by default (2, 4)
+    analyzer : str, optional
+        `CountVectorizer` analyzer for computing n-grams, by default "char_wb"
+    method : str, optional
+        linkage method to use for merging clusters, by default "average"
+
+    Returns
+    -------
+    List[str]
+       The deduplicated data.
     """
     unique_examples, counts = np.unique(data, return_counts=True)
     ex_series = pd.Series(counts, index=unique_examples)
-    scaled_sim = compute_ngram_similarity(
+    similarity_mat = compute_ngram_similarity(
         unique_examples, ngram_range=ngram_range, analyzer=analyzer
     )
 
-    dense_distance = squareform(scaled_sim, checks=False)
+    dense_distance = similarity_mat
     Z = linkage(dense_distance, method=method, optimal_ordering=True)
     if n_clusters is None:
-        n_clusters = guess_clusters(Z, scaled_sim)
-    clstrs = fcluster(Z, n_clusters, criterion="maxclust")
+        n_clusters = guess_clusters(Z, similarity_mat)
+    clusters = fcluster(Z, n_clusters, criterion="maxclust")
 
-    pd_spell_correct = create_spelling_correction(ex_series, clstrs)
+    pd_spell_correct = create_spelling_correction(ex_series, clusters)
     unrolled_corrections = pd_spell_correct[data]
-    if return_corrections:
-        return unrolled_corrections, pd_spell_correct
-    else:
-        return unrolled_corrections
+    return unrolled_corrections
 
 
-def guess_clusters(Z: NDArray, scaled_sim: NDArray) -> int:
-    """_summary_
+def guess_clusters(Z: NDArray, similarity_mat: NDArray) -> int:
+    """Finds the number of clusters that maximize the silhouette score
+    when clustering `similarity_mat`.
 
-    :param Z: _description_
-    :type Z: NDArray
-    :param scaled_sim: _description_
-    :type scaled_sim: NDArray
-    :return: _description_
-    :rtype: int
+    Parameters
+    ----------
+    Z : NDArray
+        hierarchical linkage matrix, specifies which clusters to merge.
+    similarity_mat : NDArray
+        similarity matrix either in square or condensed form.
+
+    Returns
+    -------
+    int
+        number of clusters that maximize the silhouette score.
     """
-    max_clusters = scaled_sim.shape[0]
+    max_clusters = similarity_mat.shape[0]
     n_clusters = np.arange(2, max_clusters)
+    # silhouette score needs a redundant distance matrix
+    redundant_dist = squareform(similarity_mat)
     silhouette_scores = []
     for n_clust in n_clusters:
         labels = fcluster(Z, n_clust, criterion="maxclust")
-        silhouette_avg = silhouette_score(scaled_sim, labels, metric="precomputed")
+        silhouette_avg = silhouette_score(redundant_dist, labels,
+                                          metric="precomputed")
         silhouette_scores.append(silhouette_avg)
     return n_clusters[np.argmax(silhouette_scores)]
 
 
 def create_spelling_correction(
-    count_series: pd.Series, clusters: Sequence
+    count_series: pd.Series, clusters: Sequence[int]
 ) -> pd.Series:
-    """_summary_
+    """Creates a pandas Series that map each cluster member to the most
+    frequent cluster member. The assumption is that the most common spelling
+    is the correct one.
 
-    :param count_series: _description_
-    :type count_series: pd.Series
-    :param clusters: _description_
-    :type clusters: Sequence
-    :return: _description_
-    :rtype: pd.Series
+    Parameters
+    ----------
+    count_series : pd.Series
+        A series with unique words (in the original data) as indices and number
+        of occurrences of each word in the original data as values.
+    clusters : Sequence[int]
+        A sequence of ints, indicating cluster membership of each unique word
+        in `count_series`.
+
+    Returns
+    -------
+    pd.Series
+        Series with unique (original) words as indices and (estimated)
+        corrected spelling of each word as values.
     """
     original_spelling: List[str] = []
     corrected_spelling: List[str] = []
@@ -98,6 +124,7 @@ def create_spelling_correction(
             .index.values
         )
         original_spelling.extend(sorted_spellings.tolist())
+        # assumes spelling that occurs most frequently in cluster is correct
         corrected_spelling.extend(
             np.repeat(sorted_spellings[0], sorted_spellings.shape[0])
         )
@@ -106,23 +133,29 @@ def create_spelling_correction(
 
 
 def compute_ngram_similarity(
-    unique_words: NDArray,
+    unique_words: Sequence[str],
     ngram_range: Tuple[int, int] = (2, 4),
     analyzer: str = "char_wb",
 ) -> NDArray:
-    """_summary_
+    """Computes the condensed n-gram similarity matrix between words in
+    `unique_words`, using `CountVectorizer` and `TfidfTransformer`.
 
-    :param unique_words: _description_
-    :type unique_words: Sequence
-    :param ngram_range: _description_, defaults to (2, 4)
-    :type ngram_range: Tuple[int, int], optional
-    :param analyzer: _description_, defaults to "char_wb"
-    :type analyzer: str, optional
-    :return: _description_
-    :rtype: NDArray
+    Parameters
+    ----------
+    unique_words : Sequence[str]
+        Sequence of unique words from the original data.
+    ngram_range : Tuple[int, int], optional
+        The n-gram range to compute the similarity in, by default (2, 4)
+    analyzer : str, optional
+        Analyzer to extract n-grams, by default "char_wb"
+
+    Returns
+    -------
+    NDArray
+        An n-by-(n-1)/2 matrix of n-gram similarities between `unique_words`.
     """
     enc = CountVectorizer(ngram_range=ngram_range, analyzer=analyzer)
     encoded = TfidfTransformer().fit_transform(enc.fit_transform(unique_words))
 
-    scaled_sim = minmax_scale(-encoded.dot(encoded.T).todense())
-    return scaled_sim
+    similarity_mat = pdist(-encoded.todense(), metric="euclidean")
+    return similarity_mat
