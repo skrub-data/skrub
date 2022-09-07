e5c8e62ec10a7386fd126b332d493d658c5d4c32
Jovan Stojanovic <jovanst98@yahoo.fr>
2022-09-07 12:37:50 +0200

    fix fetching test

--- dirty_cat/datasets/tests/test_fetching.py
+++ dirty_cat/datasets/tests/test_fetching.py
@@ -24,7 +24,7 @@ from dirty_cat.datasets.fetching import (
     _get_details,
     _get_features,
     _read_json_from_gz,
-    fetch_world_bank_data
+    fetch_world_bank_indicator
 )
 from dirty_cat.datasets.fetching import fetch_openml_dataset as _fetch_openml_dataset
 from dirty_cat.datasets.utils import get_data_dir as _get_data_dir
@@ -32,10 +32,10 @@ from dirty_cat.datasets.utils import get_data_dir as _get_data_dir
 
 def test_fetch_world_bank_data():
     indicator_name = 'gdppc'
-    df = fetch_world_bank_data('NY.GDP.PCAP.CD', indicator_name).X
+    df = fetch_world_bank_indicator('NY.GDP.PCAP.CD', indicator_name).X
     assert isinstance(df, pd.DataFrame)
     assert df.columns[1] == indicator_name
-    assert fetch_world_bank_data('NY.GDP.PCAP.CD', indicator_name, load_dataframe=False).name == indicator_name
+    assert fetch_world_bank_indicator('NY.GDP.PCAP.CD', indicator_name, load_dataframe=False).name == indicator_name
 
 @wraps(_fetch_openml_dataset)
 def fetch_openml_dataset(*args, **kwargs):
