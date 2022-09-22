31783fb680217d5ea10e8df14bec21ac2be82f79
Vincent M <maladiere.vincent@yahoo.fr>
2022-09-22 15:13:16 +0200

    replace hackish method with pkg_resources

--- dirty_cat/__init__.py
+++ dirty_cat/__init__.py
@@ -1,15 +1,23 @@
 """
 dirty_cat: Learning on dirty categories.
 """
-from ._check_dependencies import _version
-from ._datetime_encoder import DatetimeEncoder
-from ._gap_encoder import GapEncoder
-from ._minhash_encoder import MinHashEncoder
-from ._similarity_encoder import SimilarityEncoder
-from ._super_vectorizer import SuperVectorizer
-from ._target_encoder import TargetEncoder
+from pathlib import Path as _Path
 
-__version__ = _version
+from ._check_dependencies import check_dependencies
+
+check_dependencies()
+
+from ._datetime_encoder import DatetimeEncoder  # noqa: E402
+from ._gap_encoder import GapEncoder  # noqa: E402
+from ._minhash_encoder import MinHashEncoder  # noqa: E402
+from ._similarity_encoder import SimilarityEncoder  # noqa: E402
+from ._super_vectorizer import SuperVectorizer  # noqa: E402
+from ._target_encoder import TargetEncoder  # noqa: E402
+
+parent_dir = _Path(__file__).parent
+with open(parent_dir / "VERSION.txt") as _fh:
+    __version__ = _fh.read().strip()
+
 
 __all__ = [
     "SimilarityEncoder",
--- dirty_cat/_check_dependencies.py
+++ dirty_cat/_check_dependencies.py
@@ -1,43 +1,15 @@
-"""
-Check versions from .requirements.txt to raise error at init.
-.requirements.txt is written from setup.cfg during setup.
-"""
-import re
-from importlib.metadata import version
-from pathlib import Path as _Path
-
-from ._config_requirements import deps
+import pkg_resources
 
 
-def _check_pack_version(dep, package_name, required_version, sign):
-    installed_version = version(package_name)
-    error_msg = (
-        f"dirty_cat {_version} requires {dep}, but you have "
-        f"{package_name} {installed_version} which is incompatible."
-    )
-    try:
-        from sklearn.externals._packaging.version import parse as parse_version
-    except ModuleNotFoundError:
-        raise ImportError(error_msg)
-    parsed_installed_version = parse_version(installed_version)
-    parsed_required_version = parse_version(required_version)
-    if not eval(f"'{parsed_installed_version}' {sign} '{parsed_required_version}'"):
-        raise ImportError(error_msg)
-
-
-parent_dir = _Path(__file__).parent
-with open(parent_dir / "VERSION.txt") as _fh:
-    _version = _fh.read().strip()
-
-deps = deps["deps"].split("\n")
-for dep in deps:
-    dep = dep.strip()
-    matches_package = re.findall(r"^[\sa-zA-Z0-9-]+", dep)
-    package_name = matches_package[0]
-    signs = ["<", "<=", ">", ">=", "==", "!="]
-    for sign in signs:
-        pattern = rf"{sign}[a-zA-Z0-9.*]+"
-        matches_version = re.findall(pattern, dep)
-        if len(matches_version) > 0:
-            required_version = matches_version[0].replace(sign, "")
-            _check_pack_version(dep, package_name, required_version, sign)
+def check_dependencies():
+    package_name = "dirty-cat"
+    env = pkg_resources.Environment()
+    package = env[package_name][0]
+    dependencies = package.requires()
+    for dep in dependencies:
+        installed_dep = env[dep.name][0]
+        if installed_dep not in dep:
+            raise ImportError(
+                f"{package_name} {package.version} requires {dep} "
+                f"but you have {installed_dep}, which is not compatible."
+            )
