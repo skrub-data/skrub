27335c101f494cb778d84ae57c72aafe75eae02d
Vincent M <maladiere.vincent@yahoo.fr>
2022-09-25 14:40:19 +0200

    add setup and fix typo

--- dirty_cat/_check_dependencies.py
+++ dirty_cat/_check_dependencies.py
@@ -5,11 +5,26 @@ def check_dependencies():
     package_name = "dirty-cat"
     env = pkg_resources.Environment()
     package = env[package_name][0]
-    dependencies = package.requires()
-    for dep in dependencies:
-        installed_dep = env[dep.name][0]
-        if installed_dep not in dep:
+    requirements = package.requires()
+    for req in requirements:
+        try:
+            installed_dep = next(
+                iter(
+                    (
+                        installed_dep
+                        for installed_dep in env[req.name]
+                        if installed_dep.project_name == req.name
+                    )
+                )
+            )
+        except StopIteration:
             raise ImportError(
-                f"{package_name} {package.version} requires {dep} "
+                f"{package_name} {package.version} requires {req}, "
+                "which you don't have."
+            )
+
+        if installed_dep not in req:
+            raise ImportError(
+                f"{package_name} {package.version} requires {req} "
                 f"but you have {installed_dep}, which is not compatible."
             )
