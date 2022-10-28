fb727f8333be242d35cecb2f7c9ad8cee8ece665
Jovan Stojanovic <jovanst98@yahoo.fr>
2022-10-28 11:32:00 +0200

    Remove fetching data for test of fast_hash

--- dirty_cat/tests/test_fast_hash.py
+++ dirty_cat/tests/test_fast_hash.py
@@ -1,11 +1,27 @@
-from sklearn.datasets import fetch_20newsgroups
+import random
 
 from dirty_cat._fast_hash import ngram_min_hash
 
 
+def generate_data(n_samples):
+    MAX_LIMIT = 255  # extended ASCII Character set
+    i = 0
+    str_list = []
+    for i in range(n_samples):
+        random_string = "category "
+        for _ in range(100):
+            random_integer = random.randint(0, MAX_LIMIT)
+            random_string += chr(random_integer)
+            if random_integer < 50:
+                random_string += "  "
+        i += 1
+        str_list += [random_string]
+    return str_list
+
+
 def test_fast_hash():
-    data = fetch_20newsgroups()
-    a = data.data[0]
+    data = generate_data(100)
+    a = data[0]
 
     min_hash = ngram_min_hash(a, seed=0)
     min_hash2 = ngram_min_hash(a, seed=0)
