190addfa29bee6ce6a6ece60f01e3a619e62dc50
Lilian <lilian@boulard.fr>
2022-11-03 14:41:21 +0100

    Update dirty_cat/tests/utils.py

--- dirty_cat/tests/utils.py
+++ dirty_cat/tests/utils.py
@@ -5,7 +5,6 @@ import numpy as np
 
 def generate_data(n_samples, as_list=False):
     MAX_LIMIT = 255  # extended ASCII Character set
-    i = 0
     str_list = []
     for i in range(n_samples):
         random_string = "category "
@@ -14,7 +13,6 @@ def generate_data(n_samples, as_list=False):
             random_string += chr(random_integer)
             if random_integer < 50:
                 random_string += "  "
-        i += 1
         str_list += [random_string]
     if as_list is True:
         X = str_list
