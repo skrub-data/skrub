e25ebe8b1a5c29ac97ace46b91e1ef79bad9cc50
Lilian <lilian@boulard.fr>
2022-12-13 16:06:41 +0100

    Implement framework

--- benchmarks/utils/monitor.py
+++ benchmarks/utils/monitor.py
@@ -11,13 +11,47 @@ from pathlib import Path
 from git import Repo
 
 
+def repr_func(f: Callable, args: tuple, kwargs: dict) -> str:
+    """
+    Takes a function (f) and its arguments (args, kwargs), and
+    returns it represented as "f(*args, **kwargs)".
+    For example, with ``f=do_smth``, ``args=(10, 5)`` and
+    ``kwargs={"keyboard": "qwerty"}``,
+    returns "do_smth(10, 5, keyboard=qwerty)".
+    Can be parsed with the function `parse_func_repr` below.
+    """
+    str_args = ", ".join(args)
+    str_kwargs = ", ".join(f"{k}={v}" for k, v in kwargs.items())
+    return f"{f.__name__}({', '.join(st for st in [str_args, str_kwargs] if st)})"
+
+
+def parse_func_repr(representation: str) -> Tuple[str, tuple, dict]:
+    """
+    Takes the representation of a function and its arguments created by
+    `repr_func`, and returns the function name,
+    the positional arguments as a tuple,
+    and the keyword arguments as a dictionary.
+    """
+    func_name, args_repr = representation[:-1].split("(", 1)
+    args = []
+    kwargs = {}
+    for arg in args_repr.split(", "):
+        if "=" in arg:
+            keyword, argument = arg.split("=", 1)
+            if keyword.isidentifier():
+                kwargs.update({keyword: argument})
+                continue
+        args.append(arg)
+    return func_name, tuple(args), kwargs
+
+
 def monitor(
     memory: bool,
     time: bool,
     parametrize: Union[Collection[Collection[Any]], Dict[str, Collection[Any]]],
     repeat: int = 1,
-    save_benchmark_as: Optional[str] = None,
-):
+    save_as: Optional[str] = None,
+) -> Callable[..., Callable[..., pd.DataFrame]]:
     """Decorator used to monitor the execution of a function.
 
     The decorated function should return nothing (even if it does, nothing will
@@ -44,7 +78,7 @@ def monitor(
     repeat : int
         How many times we want to repeat the execution of the function for more
         representative time and memory extracts.
-    save_benchmark_as : str, optional
+    save_as : str, optional
         Can be specified as a benchmark name for the results to be automatically
         saved on disk.
         E.g. "supervectorizer_tuning"
@@ -53,12 +87,8 @@ def monitor(
     Returns
     -------
 
-    Dict[str, Dict[str, List[float]]]
-        A mapping of parameters (parametrized value) to their results:
-        a mapping of monitored ressource name to a list of values:
-        - "time": how long the execution took, in seconds.
-        - "memory": The number of MB of memory used.
-        The size of these lists is equal to `repeat`.
+    Callable[..., Callable[..., pd.DataFrame]]
+        A double-nested callable that returns a DataFrame of the results.
 
     """
     if not memory and not time:
@@ -67,20 +97,20 @@ def monitor(
             "there is therefore nothing to monitor for, returning empty. ",
             stacklevel=2,
         )
-        return lambda: lambda: {}
+        return lambda *_, **__: lambda *_, **__: pd.DataFrame()
 
     def decorator(func: Callable[[Any], None]):
         """Only catches the decorated function."""
 
-        def wrapper(*call_args, **call_kwargs) -> Dict[str, Dict[str, List[float]]]:
+        def wrapper(*_, **__) -> pd.DataFrame:
             """
             Parameters
             ----------
 
-            *call_args : Tuple[Any]
+            *_ : Tuple[Any]
                 Arguments passed by the function call. Ignored.
                 Use `parametrize` instead.
-            **call_kwargs : Dict[str, Any]
+            **__ : Dict[str, Any]
                 Keyword arguments passed by the function call. Ignored.
                 Use `parametrize` instead.
 
@@ -112,71 +142,71 @@ def monitor(
                 **kwargs : Dict[str, Any]
                     Keyword arguments to pass to the function.
 
+                Returns
+                -------
+                Dict[str, List[float]]
+                    a mapping of monitored resource name to a list of values:
+                    - "time": how long the execution took, in seconds.
+                      Only present if ``time=True``.
+                    - "memory": The number of MB of memory used.
+                      Only present if ``memory=True``.
+                    The size of these lists is equal to `repeat`.
+
                 """
                 _monitored = defaultdict(lambda: [])
 
+                # Global initialization of monitored values
                 if memory:
                     tracemalloc.start()
 
                 for n in range(repeat):
+                    # Initialize loop monitored values
                     if memory:
                         tracemalloc.reset_peak()
                     if time:
                         t0 = perf_counter()
+
                     func(*args, **kwargs)
 
+                    # Collect and store loop monitored values
                     if time:
                         _monitored["time"].append(perf_counter() - t0)
                     if memory:
                         _, peak = tracemalloc.get_traced_memory()
                         _monitored["memory"].append(peak / (1024**2))
 
+                # Global cleanup of monitored values
                 if memory:
                     tracemalloc.stop()
 
                 return dict(_monitored)
 
-            def repr_func(f: Callable, args: tuple, kwargs: dict) -> str:
-                """
-                Takes a function (f) and its arguments (args, kwargs), and
-                returns it represented as "f(*args, **kwargs)".
-                For exemple, with ``f=do_smth``, ``args=(10, 5)`` and
-                ``kwargs={"azerty": "qwerty"}``,
-                returns "do_smth(10, 5, azerty=qwerty)".
-                """
-                str_args = ", ".join(args)
-                str_kwargs = ", ".join(f"{k}={v}" for k, v in kwargs.items())
-                return (
-                    f"{f.__name__}"
-                    f"({', '.join(st for st in [str_args, str_kwargs] if st)})"
-                )
-
-            def to_df(results: Dict[str, Dict[str, List[float]]]):
+            def to_df(
+                results: Dict[str, Dict[str, List[float]]],
+            ) -> pd.DataFrame:
                 """
                 Converts the result of the benchmark to a pandas DataFrame.
                 """
                 index = list(results.keys())
-                columns = results[index[0]].keys()  # Take the first one for sample
+                columns = results[index[0]].keys()  # Use the first one as sample
                 data = [[results[idx][col] for col in columns] for idx in index]
-                df = pd.DataFrame(data, index=index, columns=columns)
-                return df
+                return pd.DataFrame(data, index=index, columns=columns)
 
             results = {
                 repr_func(func, args, kwargs): exec_func(*args, **kwargs)
                 for args, kwargs in product(parametrize)
             }
+            df = to_df(results)
 
-            if save_benchmark_as is not None:
+            if save_as is not None:
                 save_dir = Path(__file__).parent.parent / "results"
                 save_dir.mkdir(exist_ok=True)
                 now = datetime.now()
                 commit = Repo(Path(__file__).parent.parent.parent).active_branch.commit
-                file = (
-                    f"{save_benchmark_as}-{now.year}{now.month}{now.day}-{commit}.csv"
-                )
-                to_df(results).to_csv(save_dir / file, index_label="call")
+                file = f"{save_as}-{now.year}{now.month}{now.day}-{commit}.csv"
+                df.to_csv(save_dir / file, index_label="call")
 
-            return results
+            return df
 
         return wrapper
 
