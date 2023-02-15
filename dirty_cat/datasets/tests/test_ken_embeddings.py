92c26da53fc93e2dad06ab96444e0f0d73231675
Jovan Stojanovic <jovanst98@yahoo.fr>
2023-02-15 13:04:19 +0100

    fix test error

--- dirty_cat/datasets/tests/test_ken_embeddings.py
+++ dirty_cat/datasets/tests/test_ken_embeddings.py
@@ -6,14 +6,14 @@ def test_fuzzy_join_dtypes():
     Test if the embeddings were fetched correctly
     """
     emb = get_ken_embeddings(
-        types="game_designers", emb_id="39254360", emb_type_id="39266216"
+        types="game_designers", emb_id="39254360", emb_type_id="39266678"
     )
     assert emb.shape[1] == 202
 
     emb2 = get_ken_embeddings(
         types="game_designers",
         emb_id="39254360",
-        emb_type_id="39266216",
+        emb_type_id="39266678",
         pca_components=5,
     )
     assert emb2.shape[1] == 7
