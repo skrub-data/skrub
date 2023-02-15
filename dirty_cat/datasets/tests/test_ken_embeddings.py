7c4155dfa3750757cff4ee29258c00a1885805ae
Jovan Stojanovic <jovanst98@yahoo.fr>
2023-02-15 12:20:52 +0100

    add lighter test files

--- dirty_cat/datasets/tests/test_ken_embeddings.py
+++ dirty_cat/datasets/tests/test_ken_embeddings.py
@@ -6,14 +6,14 @@ def test_fuzzy_join_dtypes():
     Test if the embeddings were fetched correctly
     """
     emb = get_ken_embeddings(
-        types="game_designers", emb_id="39254360", emb_type_id="38879442"
+        types="game_designers", emb_id="39254360", emb_type_id="39266216"
     )
     assert emb.shape[1] == 202
 
     emb2 = get_ken_embeddings(
         types="game_designers",
         emb_id="39254360",
-        emb_type_id="38879442",
+        emb_type_id="39266216",
         pca_components=5,
     )
     assert emb2.shape[1] == 7
