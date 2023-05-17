c9f9fb33a99da6158c0c776b55aac626150cef31
Jovan Stojanovic <jovanst98@yahoo.fr>
2023-05-17 16:18:14 +0200

    try alabaster theme

--- doc/conf.py
+++ doc/conf.py
@@ -128,7 +128,7 @@ autodoc_default_flags = ["members", "inherited-members"]
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pydata_sphinx_theme"
+html_theme = "alabaster"
 
 # html_sidebars = {
 #    "**": [
