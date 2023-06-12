47eceb16c91aeb795bc549c0e640d808f2373ddf
Lilian <lilian@boulard.fr>
2023-06-12 15:51:41 +0200

    Update benchmarks/utils/_various.py

--- benchmarks/utils/_various.py
+++ benchmarks/utils/_various.py
@@ -84,7 +84,7 @@ def get_regression_datasets() -> List[Tuple[DatasetAll, str]]:
     ]
 
 
-def get_dataset(info: Tuple[dict, str]) -> Tuple[pd.DataFrame, pd.Series]:
+def get_dataset(info: Tuple[DatasetAll, str]) -> Tuple[pd.DataFrame, pd.Series]:
     y = info.y
     X = info.X
     return X, y
