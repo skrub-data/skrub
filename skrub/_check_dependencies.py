6e3fe3094711f33cd43efd87a84f572b8aedb46d
jeremie du boisberranger <jeremiedbb@yahoo.fr>
2023-07-18 14:59:25 +0200

    use importlib + packaging instead of pkg_resources

--- skrub/_check_dependencies.py
+++ skrub/_check_dependencies.py
@@ -1,30 +1,29 @@
-import pkg_resources
+from importlib.metadata import requires, version, PackageNotFoundError
+from packaging.requirements import Requirement
 
 
 def check_dependencies():
     package_name = "skrub"
-    env = pkg_resources.Environment()
-    package = env[package_name][0]
-    requirements = package.requires()
-    for req in requirements:
-        try:
-            installed_dep = next(
-                iter(
-                    (
-                        installed_dep
-                        for installed_dep in env[req.name]
-                        if installed_dep.project_name == req.name
-                    )
-                )
-            )
-        except StopIteration:
-            raise ImportError(
-                f"{package_name} {package.version} requires {req}, "
-                "which you don't have."
-            )
+    package_version = version(package_name)
+    requirements = requires(package_name)
 
-        if installed_dep not in req:
+    for req in requirements:
+        req = Requirement(req)
+
+        if req.marker is not None:
+            # skip extra requirements
+            continue
+
+        try:
+            installed_dep = version(req.name)
+            if not req.specifier.contains(installed_dep):
+                raise ImportError(
+                    f"{package_name} {package_version} requires {str(req)} "
+                    f"but you have {req.name}={installed_dep}, which is not compatible."
+                )
+
+        except PackageNotFoundError:
             raise ImportError(
-                f"{package_name} {package.version} requires {req} "
-                f"but you have {installed_dep}, which is not compatible."
+                f"{package_name} {package_version} requires {str(req)}, "
+                "which you don't have."
             )
