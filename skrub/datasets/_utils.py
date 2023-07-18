3e00a8c426f1f7273ca40088e0a2ada939a5ca02
flefebv <felixlefebvre28@gmail.com>
2023-07-18 15:06:05 +0200

    Change default location of data_home

--- skrub/datasets/_utils.py
+++ skrub/datasets/_utils.py
@@ -1,8 +1,54 @@
 import os
+import shutil
 from pathlib import Path
 
 
-def get_data_dir(name: str | None = None) -> Path:
+def get_data_home(data_home=None) -> str:
+    """Returns the path of the skrub data directory.
+
+    This folder is used by some large dataset loaders to avoid downloading the
+    data several times.
+
+    By default the data directory is set to a folder named 'skrub_data' in the
+    user home folder.
+
+    Alternatively, it can be set programmatically by giving an explicit folder
+    path. The '~' symbol is expanded to the user home folder.
+
+    If the folder does not already exist, it is automatically created.
+
+    Parameters
+    ----------
+    data_home : str, default=None
+        The path to skrub data directory. If `None`, the default path
+        is `~/skrub_data`.
+
+    Returns
+    -------
+    data_home: str or path-like, default=None
+        The path to skrub data directory.
+    """
+    if data_home is None:
+        data_home = os.path.join("~", "skrub_data")
+    data_home = os.path.expanduser(data_home)
+    os.makedirs(data_home, exist_ok=True)
+    return data_home
+
+
+def clear_data_home(data_home=None):
+    """Delete all the content of the data home cache.
+
+    Parameters
+    ----------
+    data_home : str or path-like, default=None
+        The path to skrub data directory. If `None`, the default path
+        is `~/skrub_data`.
+    """
+    data_home = get_data_home(data_home)
+    shutil.rmtree(data_home)
+
+
+def get_data_dir(name: str | None = None, data_home: str | None = None) -> Path:
     """
     Returns the directory in which skrub looks for data.
 
@@ -11,13 +57,14 @@ def get_data_dir(name: str | None = None) -> Path:
 
     Parameters
     ----------
+    data_home : str, default=None
+        The path to skrub data directory. If `None`, the default path
+        is `~/skrub_data`.
     name: str, optional
         Subdirectory name. If omitted, the root data directory is returned.
     """
-    # Note: we stick to os.path instead of pathlib.Path because
-    # it's easier to test, and the functionality is the same.
-    module_path = Path(os.path.dirname(__file__)).resolve()
-    data_dir = module_path / "data"
+    data_home = get_data_home(data_home)
+    data_dir = Path(data_home).resolve()
     if name is not None:
         data_dir = data_dir / name
     return data_dir
--- skrub/datasets/tests/test_utils.py
+++ skrub/datasets/tests/test_utils.py
@@ -1,18 +1,59 @@
+import os
+import tempfile
 from pathlib import Path
-from unittest import mock
+
+from skrub.datasets._utils import clear_data_home, get_data_dir, get_data_home
 
 
-@mock.patch("os.path.dirname")
-def test_get_data_dir(mock_os_path_dirname):
+def test_get_data_dir():
     """
     Tests function ``get_data_dir()``.
     """
-    from skrub.datasets._utils import get_data_dir
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        expected_return_value_default = Path(tmpdirname)
+        assert get_data_dir(data_home=tmpdirname) == expected_return_value_default
 
-    expected_return_value_default = Path("/user/directory/data").absolute()
+        expected_return_value_custom = expected_return_value_default / "tests"
+        assert (
+            get_data_dir(name="tests", data_home=tmpdirname)
+            == expected_return_value_custom
+        )
 
-    mock_os_path_dirname.return_value = expected_return_value_default.parent
-    assert get_data_dir() == expected_return_value_default
 
-    expected_return_value_custom = expected_return_value_default / "tests"
-    assert get_data_dir("tests") == expected_return_value_custom
+def test_get_data_home_str():
+    """
+    Test function for ``get_data_home()`` when data_home is a string.
+    """
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # get_data_home will point to a pre-existing folder
+        assert os.path.exists(tmpdirname)
+        data_home = get_data_home(data_home=tmpdirname)
+        assert data_home == tmpdirname
+        assert os.path.exists(data_home)
+
+        # clear_data_home will delete both the content and the folder itself
+        clear_data_home(data_home=data_home)
+        assert not os.path.exists(data_home)
+
+        # if the folder is missing it will be created again
+        assert not os.path.exists(tmpdirname)
+        data_home = get_data_home(data_home=tmpdirname)
+        assert os.path.exists(data_home)
+
+
+def test_get_data_home_None():
+    """
+    Test function for ``get_data_home()`` with data_home=None.
+    """
+    # get path of the folder 'skrub_data' in the user home folder
+    user_path = os.path.join("~", "skrub_data")
+    user_path = os.path.expanduser(user_path)
+
+    # if the folder does not exist, create it, and clear it
+    if not os.path.exists(user_path):
+        data_home = get_data_home(data_home=None)
+        assert data_home == user_path
+        assert os.path.exists(data_home)
+
+        clear_data_home()
+        assert not os.path.exists(data_home)
