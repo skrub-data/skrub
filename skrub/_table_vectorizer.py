5d8da7aca64ec0803d48d392843e735d69632d49
Jérémie du Boisberranger <34657725+jeremiedbb@users.noreply.github.com>
2023-07-18 13:51:46 +0200

    CI Turn deprecation warnings into errors (#645)
    * turn future warnings into errors [deps nightly]
    
    * fix last one [deps nightly]

--- skrub/_table_vectorizer.py
+++ skrub/_table_vectorizer.py
@@ -137,7 +137,7 @@ def _replace_missing_in_cat_col(ser: pd.Series, value: str = "missing") -> pd.Se
     replaces the missing values, and returns it.
     """
     ser = _replace_false_missing(ser)
-    if pd.api.types.is_categorical_dtype(ser) and (value not in ser.cat.categories):
+    if isinstance(ser.dtype, pd.CategoricalDtype) and (value not in ser.cat.categories):
         ser = ser.cat.add_categories([value])
     ser = ser.fillna(value=value)
     return ser
@@ -555,16 +555,14 @@ class TableVectorizer(ColumnTransformer):
         for col, dtype in self.types_.items():
             # if categorical, add the new categories to prevent
             # them to be encoded as nan
-            if pd.api.types.is_categorical_dtype(dtype):
+            if isinstance(dtype, pd.CategoricalDtype):
                 known_categories = dtype.categories
                 new_categories = pd.unique(X[col])
                 dtype = pd.CategoricalDtype(
                     categories=known_categories.union(new_categories)
                 )
                 self.types_[col] = dtype
-            X[col] = X[col].astype(
-                dtype
-            )  # we dont use .loc (https://github.com/pandas-dev/pandas/issues/53729)
+        X = X.astype(self.types_)
         return X
 
     def fit_transform(self, X: ArrayLike, y: ArrayLike = None) -> ArrayLike:
