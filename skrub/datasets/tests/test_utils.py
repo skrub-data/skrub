34e29ae2bbc552935c876178a00dab9c06f8bb62
Gael Varoquaux <gael.varoquaux@normalesup.org>
2023-07-21 15:47:25 +0200

    make black happy

--- skrub/datasets/tests/test_utils.py
+++ skrub/datasets/tests/test_utils.py
@@ -18,8 +18,7 @@ def test_get_data_dir_existing_folder(data_home_type):
 
         assert data_home.exists()
 
-        assert (get_data_dir(name="tests", data_home=tmp_dir)
-            == Path(tmp_dir) / "tests")
+        assert get_data_dir(name="tests", data_home=tmp_dir) == Path(tmp_dir) / "tests"
 
         # if the folder is missing it will be created
         shutil.rmtree(tmp_dir)
