87f74f6997e7181c57ce01c400e4c34aafb4e334
Jovan Stojanovic <jovanst98@yahoo.fr>
2023-07-27 11:03:34 +0200

    new example

--- examples/07_multiple_key_join.py
+++ examples/07_multiple_key_join.py
@@ -2,29 +2,32 @@
 Joining with keys across multiple columns
 =========================================
 
-Here we show how to combine data from different sources,
-with a vocabulary not well normalized.
-
 Joining is difficult: one entry on one side does not have
 an exact match on the other side.
 
-Joiner allows you to perform spatial joins.
+This problem becomes even more complex when multiple columns
+are significant for the join.
 
-The |fj| function enables to join tables without cleaning the data by
-accounting for the label variations.
+Typically, this is the case in spatial joins: to join on
+the closest location you need to take into account both
+longitude and latitude.
 
-To illustrate, we will join data from the
-`2022 World Happiness Report <https://worldhappiness.report/>`_, with tables
-provided in `the World Bank open data platform <https://data.worldbank.org/>`_
-in order to create a first prediction model.
-
-Moreover, the |joiner| is a scikit-learn Transformer that makes it easy to
-use such fuzzy joining multiple tables to bring in information in a
-machine-learning pipeline. In particular, it enables tuning parameters of
-|fj| to find the matches that maximize prediction accuracy.
+|joiner| is a scikit-learn Transformer that allows you to perform
+joining across multiple keys, and independantly of the data
+type (numerical, string or mixed).
 
 
 .. |fj| replace:: :func:`~skrub.fuzzy_join`
 
 .. |joiner| replace:: :func:`~skrub.Joiner`
 """
+
+from skrub.datasets import fetch_road_safety
+
+data = fetch_road_safety()
+
+X = data.X
+
+X.head()
+
+X[["Longitude", "Latitude"]]
