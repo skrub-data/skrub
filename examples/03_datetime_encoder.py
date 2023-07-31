2a28999f3d86dc0d49dd6d244d88488ada776da7
Lilian <lilian@boulard.fr>
2023-07-31 15:47:32 +0200

    Rework datetime encoder example

--- examples/03_datetime_encoder.py
+++ examples/03_datetime_encoder.py
@@ -1,22 +1,42 @@
 """
+.. _example_datetime_encoder
+
+===================================================
 Handling datetime features with the DatetimeEncoder
 ===================================================
 
-We illustrate here how to handle datetime features with the
-DatetimeEncoder.
+In this example, we illustrate how to better integrate datetime features
+in machine learning models with the |DatetimeEncoder|.
 
-The |DtE| breaks down each datetime
-features into several numerical features, by extracting relevant
-information from the datetime features, such as the month, the day
-of the week, the hour of the day, etc.
-Used in the |TV|, which automatically detects
-the datetime features, the |DtE| allows to
-handle datetime features easily.
+This encoder breaks down passed datetime features into relevant numerical
+features, such as the month, the day of the week, the hour of the day, etc.
+
+It is used by default in the |TableVectorizer|.
 
 
-.. |DtE| replace:: :class:`~skrub.DatetimeEncoder`
+.. |DatetimeEncoder| replace::
+    :class:`~skrub.DatetimeEncoder`
 
-.. |TV| replace:: :class:`~skrub.TableVectorizer`
+.. |TableVectorizer| replace::
+    :class:`~skrub.TableVectorizer`
+
+.. |OneHotEncoder| replace::
+    :class:`~sklearn.preprocessing.OneHotEncoder`
+
+.. |TimeSeriesSplit| replace::
+    :class:`~sklearn.model_selection.TimeSeriesSplit`
+
+.. |ColumnTransformer| replace::
+    :class:`~sklearn.compose.ColumnTransformer`
+
+.. |make_column_transformer| replace::
+    :class:`~sklearn.compose.make_column_transformer`
+
+.. |TimeSeriesSplit| replace::
+    :class:`~sklearn.model_selection.TimeSeriesSplit`
+
+.. |HGBR| replace::
+    :class:`~sklearn.ensemble.HistGradientBoostingRegressor`
 """
 
 import warnings
@@ -24,66 +44,69 @@ import warnings
 warnings.filterwarnings("ignore")
 
 ###############################################################################
-# Data Importing
-# --------------
+# A problem with relevant datetime features
+# -----------------------------------------
 #
-# We first fetch the dataset.
-#
-# We want to predict the NO2 air concentration in different cities, based
-# on the date and the time of measurement.
+# We will use a dataset of air quality measurements in different cities.
+# In this setting, we want to predict the NO2 air concentration, based
+# on the location, date and time of measurement.
+
 import pandas as pd
 
 data = pd.read_csv(
-    "https://raw.githubusercontent.com/pandas-dev/pandas/main/doc/data/air_quality_no2_long.csv"  # noqa
+    "https://raw.githubusercontent.com/pandas-dev/pandas"
+    "/main/doc/data/air_quality_no2_long.csv"
 )
+# Extract our input data (X) and the target column (y)
 y = data["value"]
 X = data[["city", "date.utc"]]
+
 X
 
 ###############################################################################
-# Encoding the data to numerical representations
-# ----------------------------------------------
+# Encoding the features
+# .....................
 #
-# Encoders for categorical and datetime features
-# ..............................................
+# We will construct a |ColumnTransformer| with |make_column_transformer|.
+# We'll want to encode the city names with a |OneHotEncoder|, and the date
+# with a |DatetimeEncoder|.
+#
+# During the instantiation of the |DatetimeEncoder|, we specify that we want
+# to extract the day of the week, and that we don't want to extract anything
+# finer than minutes. This is because we don't want to extract seconds and
+# lower units, as they are probably unimportant.
+
 from sklearn.preprocessing import OneHotEncoder
 
 from skrub import DatetimeEncoder
 
-cat_encoder = OneHotEncoder(handle_unknown="ignore")
-# We encode dates using the day of the week as it is probably relevant,
-# but no longer than minutes: we are probably not interested in seconds
-# and below
-datetime_encoder = DatetimeEncoder(add_day_of_the_week=True, extract_until="minute")
-
 from sklearn.compose import make_column_transformer
 
-datetime_columns = ["date.utc"]
-categorical_columns = ["city"]
-
 encoder = make_column_transformer(
-    (cat_encoder, categorical_columns),
-    (datetime_encoder, datetime_columns),
+    (OneHotEncoder(handle_unknown="ignore"), ["city"]),
+    (DatetimeEncoder(add_day_of_the_week=True, extract_until="minute"), ["date.utc"]),
     remainder="drop",
 )
 
-###############################################################################
-# Transforming the input data
-# ...........................
-# We can see that the encoder is working as expected: the date feature has
-# been replaced by features for the month, day, hour, and day of the week.
-# Note that the year and minute features have been removed by the encoder
-# because they are constant.
-X_ = encoder.fit_transform(X)
+X_enc = encoder.fit_transform(X)
 encoder.get_feature_names_out()
-###############################################################################
-X_
 
 ###############################################################################
-# One-liner with the |TV|
-# .......................
-# The |DtE| is used by default in the |TV|, which
-# automatically detects datetime features.
+# We can see that the encoder is working as expected: the "date.utc" column has
+# been replaced by features extracting the month, day, hour, and day of the
+# week information.
+#
+# Noting the year and minute features are not present, this is because they
+# have been removed by the encoder as they are the same for the whole feature.
+
+###############################################################################
+# One-liner with the |TableVectorizer|
+# ....................................
+#
+# As mentioned earlier, the |TableVectorizer| makes use of the
+# |DatetimeEncoder| by default.
+
+from skrub import TableVectorizer
 from pprint import pprint
 
 from skrub import TableVectorizer
@@ -93,7 +116,12 @@ table_vec.fit_transform(X)
 pprint(table_vec.get_feature_names_out())
 
 ###############################################################################
-# If we want the day of the week, we can just replace |TV|'s default parameter:
+# If we want to customize the |DatetimeEncoder| inside the |TableVectorizer|,
+# we can replace its default parameter with a new, custom instance:
+#
+# Here, for example, we want to extract the day of the week (which it doesn't
+# do by default).
+
 table_vec = TableVectorizer(
     datetime_transformer=DatetimeEncoder(add_day_of_the_week=True),
 )
@@ -101,16 +129,30 @@ table_vec.fit_transform(X)
 table_vec.get_feature_names_out()
 
 ###############################################################################
-# We can see that the |TV| is indeed using
-# a |DtE| for the datetime features.
+# .. note:
+#     For more information on how to customize the |TableVectorizer|, see
+#     :ref:`sphx_glr_auto_examples_01_dirty_categories.py`.
+#
+# Inspecting the |TableVectorizer| further, we can check that the
+# |DatetimeEncoder| is used on the correct column(s).
 pprint(table_vec.transformers_)
 
 ###############################################################################
-# Predictions with date features
-# ------------------------------
-# For prediction tasks, we recommend using the |TV| inside a
-# pipeline, combined with a model that uses the features extracted by the
-# |DtE|.
+# Prediction with datetime features
+# ---------------------------------
+#
+# For prediction tasks, we recommend using the |TableVectorizer| inside a
+# pipeline, combined with a model that can use the features extracted by the
+# |DatetimeEncoder|.
+# Here's we'll use a |HGBR| as our learner.
+#
+# .. note:
+#    You might need to require the experimental feature for scikit-learn
+#    versions earlier than 1.0 with:
+#    ```py
+#    from sklearn.experimental import enable_hist_gradient_boosting
+#    ```
+
 import numpy as np
 from sklearn.ensemble import HistGradientBoostingRegressor
 from sklearn.pipeline import make_pipeline
@@ -118,17 +160,19 @@ from sklearn.pipeline import make_pipeline
 table_vec = TableVectorizer(
     datetime_transformer=DatetimeEncoder(add_day_of_the_week=True),
 )
-reg = HistGradientBoostingRegressor()
-pipeline = make_pipeline(table_vec, reg)
+pipeline = make_pipeline(table_vec, HistGradientBoostingRegressor())
 
 ###############################################################################
 # Evaluating the model
 # ....................
+#
 # When using date and time features, we often care about predicting the future.
 # In this case, we have to be careful when evaluating our model, because
-# standard tools like cross-validation do not respect the time ordering.
-# Instead, we can use the :class:`~sklearn.model_selection.TimeSeriesSplit`,
-# which makes sure that the test set is always in the future.
+# the standard settings of the cross-validation do not respect time ordering.
+#
+# Instead, we can use the |TimeSeriesSplit|,
+# which ensures that the test set is always in the future.
+
 X["date.utc"] = pd.to_datetime(X["date.utc"])
 sorted_indices = np.argsort(X["date.utc"])
 X = X.iloc[sorted_indices]
@@ -147,8 +191,10 @@ cross_val_score(
 ###############################################################################
 # Plotting the prediction
 # .......................
+#
 # The mean squared error is not obvious to interpret, so we compare
 # visually the prediction of our model with the actual values.
+
 import matplotlib.pyplot as plt
 from matplotlib.dates import ConciseDateFormatter
 
@@ -159,9 +205,12 @@ y_train = y[X["date.utc"] < "2019-06-01"]
 y_test = y[X["date.utc"] >= "2019-06-01"]
 
 pipeline.fit(X_train, y_train)
-fig, axs = plt.subplots(nrows=len(X_test.city.unique()), ncols=1, figsize=(12, 9))
 
-for i, city in enumerate(X_test.city.unique()):
+all_cities = X_test["city"].unique()
+
+fig, axs = plt.subplots(nrows=len(all_cities), ncols=1, figsize=(12, 9))
+
+for i, city in enumerate(all_cities):
     axs[i].plot(
         X.loc[X.city == city, "date.utc"],
         y.loc[X.city == city],
@@ -192,20 +241,19 @@ X_test_zoomed = X_zoomed[X_zoomed["date.utc"] >= "2019-06-03"]
 y_train_zoomed = y[X["date.utc"] < "2019-06-03"]
 y_test_zoomed = y[X["date.utc"] >= "2019-06-03"]
 
-pipeline.fit(X_train, y_train)
-fig, axs = plt.subplots(
-    nrows=len(X_test_zoomed.city.unique()), ncols=1, figsize=(12, 9)
-)
+zoomed_cities = X_test_zoomed["city"].unique()
 
-for i, city in enumerate(X_test_zoomed.city.unique()):
+fig, axs = plt.subplots(nrows=len(zoomed_cities), ncols=1, figsize=(12, 9))
+
+for i, city in enumerate(zoomed_cities):
     axs[i].plot(
-        X_zoomed.loc[X_zoomed.city == city, "date.utc"],
-        y_zoomed.loc[X_zoomed.city == city],
+        X_zoomed.loc[X_zoomed["city"] == city, "date.utc"],
+        y_zoomed.loc[X_zoomed["city"] == city],
         label="Actual",
     )
     axs[i].plot(
-        X_test_zoomed.loc[X_test_zoomed.city == city, "date.utc"],
-        pipeline.predict(X_test_zoomed.loc[X_test_zoomed.city == city]),
+        X_test_zoomed.loc[X_test_zoomed["city"] == city, "date.utc"],
+        pipeline.predict(X_test_zoomed.loc[X_test_zoomed["city"] == city]),
         label="Predicted",
     )
     axs[i].set_title(city)
@@ -219,9 +267,10 @@ plt.show()
 ###############################################################################
 # Feature importances
 # -------------------
-# Using the |DtE| allows us to better understand how the date
+#
+# Using the |DatetimeEncoder| allows us to better understand how the date
 # impacts the NO2 concentration. To this aim, we can compute the
-# importance of the features created by the |DtE|, using the
+# importance of the features created by the |DatetimeEncoder|, using the
 # :func:`~sklearn.inspection.permutation_importance` function, which
 # basically shuffles a feature and sees how the model changes its prediction.
 
@@ -255,3 +304,11 @@ plt.show()
 ###############################################################################
 # We can see that the hour of the day is the most important feature,
 # which seems reasonable.
+#
+# Conclusion
+# ----------
+#
+# In this example, we saw how to use the |DatetimeEncoder| to create
+# features from a date column.
+# Also check out the |TableVectorizer|, which automatically recognizes
+# and transforms datetime columns by default.
