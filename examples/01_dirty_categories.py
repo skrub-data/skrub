f4db3eae7a30218d5f1fd089540db56f7b40a02f
Lilian <lilian@boulard.fr>
2023-08-03 18:40:31 +0200

    Rename file again

--- examples/01_dirty_categories.py
+++ examples/01_dirty_categories.py
@@ -208,7 +208,7 @@ plt.tight_layout()
 # data than the |MinHash| and |Gap|. The most scalable encoder is
 # the |MinHash|. On the other hand, the |Gap| has the benefit of
 # providing interpretable features
-# (see :ref:`sphx_glr_auto_examples_02_feature_importance_with_gapencoder.py`)
+# (see :ref:`sphx_glr_auto_examples_02_feature_interpretation_with_gapencoder.py`)
 #
 # |
 #
