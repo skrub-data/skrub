f167ecbc3a6de5b22b77e0b971978e8ff5d8bb80
Vincent M <maladiere.vincent@yahoo.fr>
2023-08-21 15:23:18 +0200

    actually, remove the warning entirely

--- skrub/__init__.py
+++ skrub/__init__.py
@@ -3,18 +3,7 @@ skrub: Prepping tables for machine learning.
 """
 from pathlib import Path as _Path
 
-try:
-    from ._check_dependencies import check_dependencies
-
-    check_dependencies()
-except ModuleNotFoundError:
-    import warnings
-
-    warnings.warn(
-        "the 'packaging' library is not available, dependencies versions will not be"
-        " checked."
-    )
-
+from ._check_dependencies import check_dependencies
 from ._datetime_encoder import DatetimeEncoder
 from ._deduplicate import compute_ngram_distance, deduplicate
 from ._fuzzy_join import fuzzy_join
@@ -24,6 +13,8 @@ from ._minhash_encoder import MinHashEncoder
 from ._similarity_encoder import SimilarityEncoder
 from ._table_vectorizer import SuperVectorizer, TableVectorizer
 from ._target_encoder import TargetEncoder
+
+check_dependencies()
 
 with open(_Path(__file__).parent / "VERSION.txt") as _fh:
     __version__ = _fh.read().strip()
