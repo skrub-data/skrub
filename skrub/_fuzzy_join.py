d0555b8f94747b58eed208846d0b5670d7c88d51
Juan Orduz <juanitorduz@gmail.com>
2023-10-02 15:40:41 +0200

    Update skrub/_fuzzy_join.py
    Co-authored-by: Vincent M <maladiere.vincent@yahoo.fr>

--- skrub/_fuzzy_join.py
+++ skrub/_fuzzy_join.py
@@ -205,7 +205,7 @@ def fuzzy_join(
     sort: bool = False,
     suffixes: tuple[str, str] = ("_x", "_y"),
 ) -> pd.DataFrame:
-    """Join two tables based on approximate matching using the appropriate similarity
+    """Join two tables based on approximate matching using the appropriate similarity \
     metric.
 
     The principle is as follows:
