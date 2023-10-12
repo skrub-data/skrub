65657c3736c7742c96916e7e679d2a100240fafd
Vincent M <maladiere.vincent@yahoo.fr>
2023-10-12 17:25:02 +0200

    docstring typo

--- skrub/_datetime_encoder.py
+++ skrub/_datetime_encoder.py
@@ -259,7 +259,7 @@ def _to_datetime_2d(
 
     Parameters
     ----------
-    X : list of 1d array of length n_features
+    X_split : list of 1d array of length n_features
         The 2d input, chunked into a list of array. This format allows us
         to treat each column individually and preserve their dtype, because
         dataframe.to_numpy() casts all columns to object is any column dtype
