5c5c076e0b210046a0414bd6e9f71b0904e7aa67
Jerome Dockes <jerome@dockes.org>
2023-10-19 11:18:40 +0200

    verbose

--- build_tools/github/test.sh
+++ build_tools/github/test.sh
@@ -3,6 +3,6 @@
 pytest --pyargs skrub --cov=skrub -n auto --doctest-modules
 
 
-sqlite3 ./.coverage 'with pat(pat) as (select "site-packages/") update file set path=substring(path, instr(path, pat) + length(pat)) from pat;'
+# sqlite3 ./.coverage 'with pat(pat) as (select "site-packages/") update file set path=substring(path, instr(path, pat) + length(pat)) from pat;'
 
 coverage xml
