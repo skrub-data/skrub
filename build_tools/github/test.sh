b43a5e1d852bc175223458590f579cbee8ff06ee
Jerome Dockes <jerome@dockes.org>
2023-10-19 11:25:02 +0200

    remove comment

--- build_tools/github/test.sh
+++ build_tools/github/test.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -x
 
 pytest --pyargs skrub --cov=skrub -n auto --doctest-modules
-
-
-# sqlite3 ./.coverage 'with pat(pat) as (select "site-packages/") update file set path=substring(path, instr(path, pat) + length(pat)) from pat;'
-
 coverage xml
