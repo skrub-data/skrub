1ad1990a93751384371b68456a0fdd0f2acf56fc
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2024-01-11 21:23:32 +0100

    Draft multiaggjoiner and multijoiner

--- skrub/_multi_joiner.py
+++ skrub/_multi_joiner.py
@@ -5,8 +5,11 @@ to multiple auxiliary tables.
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin, clone
 
-# from skrub._agg_joiner import AggJoiner
-from skrub._joiner import DEFAULT_REF_DIST, DEFAULT_STRING_ENCODER  # Joiner
+from skrub._agg_joiner import AggJoiner
+from skrub._dataframe._namespace import is_pandas, is_polars
+from skrub._joiner import DEFAULT_REF_DIST, DEFAULT_STRING_ENCODER  # , Joiner
+
+# from sklearn.utils.validation import check_is_fitted
 
 
 def check_multi_key():
@@ -14,6 +17,123 @@ def check_multi_key():
 
 
 class MultiJoiner(BaseEstimator, TransformerMixin):
+    """Augment features in a main table by fuzzy-joining auxiliary tables to it.
+
+    This transformer is initialized with auxiliary tables ``aux_tables``. It
+    transforms a main table by joining it, with approximate ("fuzzy") matching,
+    to the auxiliary table. The output of ``transform`` has the same rows as
+    the main table (i.e. as the argument passed to ``transform``), but each row
+    is augmented with values from the best match in the auxiliary table.
+
+    To identify the best match for each row, values from the matching columns
+    (``main_key`` and ``aux_key``) are vectorized, i.e. represented by vectors of
+    continuous values. Then, the Euclidean distances between these vectors are
+    computed to find, for each main table row, its nearest neighbor within the
+    auxiliary table.
+
+    Optionally, a maximum distance threshold, ``max_dist``, can be set. Matches
+    between vectors that are separated by a distance (strictly) greater than
+    ``max_dist`` will be rejected. We will consider that main table rows that
+    are farther than ``max_dist`` from their nearest neighbor do not have a
+    matching row in the auxiliary table, and the output will contain nulls for
+    the entries that would normally have come from the auxiliary table (as in a
+    traditional left join).
+
+    To make it easier to set a ``max_dist`` threshold, the distances are
+    rescaled by dividing them by a reference distance, which can be chosen with
+    ``ref_dist``. The default is ``'random_pairs'``. The possible choices are:
+
+    'random_pairs'
+        Pairs of rows are sampled randomly from the auxiliary table and their
+        distance is computed. The reference distance is the first quartile of
+        those distances.
+
+    'second_neighbor'
+        The reference distance is the distance to the *second* nearest neighbor
+        in the auxiliary table.
+
+    'self_join_neighbor'
+        Once the match candidate (i.e. the nearest neigbor from the auxiliary
+        table) has been found, we find its nearest neighbor in the auxiliary
+        table (excluding itself). The reference distance is the distance that
+        separates those 2 auxiliary rows.
+
+    'no_rescaling'
+        The reference distance is 1.0, i.e. no rescaling of the distances is
+        applied.
+
+    Parameters
+    ----------
+    aux_table : :obj:`~pandas.DataFrame`
+        The auxiliary table, which will be fuzzy-joined to the main table when
+        calling ``transform``.
+    key : str or list of str, default=None
+        The column names to use for both ``main_key`` and ``aux_key`` when they
+        are the same. Provide either ``key`` or both ``main_key`` and ``aux_key``.
+    main_key : str or list of str, default=None
+        The column names in the main table on which the join will be performed.
+        Can be a string if joining on a single column.
+        If ``None``, `aux_key` must also be ``None`` and `key` must be provided.
+    aux_key : str or list of str, default=None
+        The column names in the auxiliary table on which the join will
+        be performed. Can be a string if joining on a single column.
+        If ``None``, `main_key` must also be ``None`` and `key` must be provided.
+    suffix : str, default=""
+        Suffix to append to the ``aux_table``'s column names. You can use it
+        to avoid duplicate column names in the join.
+    max_dist : float, default=np.inf
+        Maximum acceptable (rescaled) distance between a row in the
+        ``main_table`` and its nearest neighbor in the ``aux_table``. Rows that
+        are farther apart are not considered to match. By default, the distance
+        is rescaled so that a value between 0 and 1 is typically a good choice,
+        although rescaled distances can be greater than 1 for some choices of
+        ``ref_dist``. ``None``, ``"inf"``, ``float("inf")`` or ``numpy.inf``
+        mean that no matches are rejected.
+    ref_dist : reference distance for rescaling, default = 'random_pairs'
+        Options are {"random_pairs", "second_neighbor", "self_join_neighbor",
+        "no_rescaling"}. See above for a description of each option. To
+        facilitate the choice of ``max_dist``, distances between rows in
+        ``main_table`` and their nearest neighbor in ``aux_table`` will be
+        rescaled by this reference distance.
+    string_encoder : scikit-learn transformer used to vectorize text columns
+        By default a ``HashingVectorizer`` combined with a ``TfidfTransformer``
+        is used. Here we use raw TF-IDF features rather than transforming them
+        for example with ``GapEncoder`` or ``MinHashEncoder`` because it is
+        faster, these features are only used to find nearest neighbors and not
+        used by downstream estimators, and distances between TF-IDF vectors
+        have a somewhat simpler interpretation.
+    add_match_info : bool, default=True
+        Insert some columns whose names start with `skrub_Joiner` containing
+        the distance, rescaled distance and whether the rescaled distance is
+        above the threshold. Those values can be helpful for an estimator that
+        uses the joined features, or to inspect the result of the join and set
+        a ``max_dist`` threshold.
+
+    Attributes
+    ----------
+    max_dist_ : the maximum distance for a match to be accepted
+        Equal to the parameter ``max_dist`` except that ``"inf"`` and ``None``
+        are mapped to ``np.inf`` (i.e. accept all matches).
+
+    vectorizer_ : scikit-learn ColumnTransformer
+        The fitted transformer used to transform the matching columns into
+        numerical vectors.
+
+    See Also
+    --------
+    Joiner :
+        Augment features in a main table by fuzzy-joining an auxiliary table to it.
+
+    fuzzy_join :
+        Join two tables (dataframes) based on approximate column matching. This
+        is the same functionality as provided by the ``Joiner`` but exposed as
+        a function rather than a transformer.
+
+    Examples
+    --------
+    # TODO
+    """
+
     def __init__(
         self,
         aux_table,
@@ -79,7 +199,7 @@ class MultiAggJoiner(BaseEstimator, TransformerMixin):
         the aggregation operations.
         If None, cols are all columns from table, except `aux_key`.
 
-    operation : str or iterable of str, default=None
+    operations : str or iterable of str, default=None
         Aggregation operations to perform on the auxiliary table.
 
         numerical : {"sum", "mean", "std", "min", "max", "hist", "value_counts"}
@@ -90,7 +210,7 @@ class MultiAggJoiner(BaseEstimator, TransformerMixin):
 
         If set to None (the default), ['mean', 'mode'] will be used.
 
-    suffix : str or iterable of str, default=None
+    suffixes : str or iterable of str, default=None
         The suffixes that will be added to each table columns in case of
         duplicate column names.
         If set to None, the table index in 'aux_table' are used,
@@ -101,6 +221,10 @@ class MultiAggJoiner(BaseEstimator, TransformerMixin):
     --------
     AggJoiner :
         Aggregate an auxiliary dataframe before joining it on a base dataframe.
+
+    Examples
+    --------
+    # TODO
     """
 
     def __init__(
@@ -111,13 +235,134 @@ class MultiAggJoiner(BaseEstimator, TransformerMixin):
         main_keys=None,
         aux_keys=None,
         cols=None,
-        operation=None,
-        suffix=None,
+        operations=None,
+        suffixes=None,
     ):
         self.aux_tables = aux_tables
         self.keys = keys
         self.main_keys = main_keys
         self.aux_keys = aux_keys
         self.cols = cols
-        self.operation = operation
-        self.suffix = suffix
+        self.operations = operations
+        self.suffixes = suffixes
+
+    def _check_dataframes(self, X, aux_tables):
+        """Check dataframes input types.
+
+        Parameters
+        ----------
+        X : DataframeLike
+            The main table to augment.
+        aux_tables : DataframeLike
+            The auxiliary tables.
+
+        Raises
+        ------
+        TypeError
+            Raises an error if all the frames don't have the same type,
+            or if there is a Polars lazyframe.
+        """
+
+        # Polars lazyframes will raise an error here.
+        if not hasattr(X, "__dataframe__"):
+            raise TypeError(f"'X' must be a dataframe, got {type(X)}.")
+        if not all(hasattr(aux_table, "__dataframe__") for aux_table in aux_tables):
+            raise TypeError("'aux_tables' must all be dataframes.")
+
+        if is_pandas(X):
+            if not all(is_pandas(aux_table) for aux_table in aux_tables):
+                raise TypeError("All 'aux_tables' must be Pandas dataframes.")
+        if is_polars(X):
+            if not all(is_polars(aux_table) for aux_table in aux_tables):
+                raise TypeError("All 'aux_tables' must be Polars dataframes.")
+
+    def fit(self, X, y=None):
+        """Aggregate auxiliary tables based on the main keys.
+
+        Parameters
+        ----------
+        X : DataframeLike
+            Input data, based table on which to left join the
+            auxiliary tables.
+
+        y : array-like of shape (n_samples), default=None
+            Prediction target. Used to compute correlations between the
+            generated covariates and the target for screening purposes.
+
+        Returns
+        -------
+        AggJoiner
+            Fitted :class:`MultiAggJoiner` instance (self).
+        """
+
+        # TODO: check_input: X should be of same type
+        self._check_dataframes(X, self.aux_tables)
+
+        self.agg_joiners_ = []
+
+        for aux_table, key, main_key, aux_key, cols, operation, suffix in zip(
+            self.aux_tables,
+            self.keys,
+            self.main_keys,
+            self.aux_keys,
+            self.cols,
+            self.operations,
+            self.suffixes,
+        ):
+            agg_joiner = AggJoiner(
+                aux_table=aux_table,
+                key=key,
+                main_key=main_key,
+                aux_key=aux_key,
+                cols=cols,
+                operation=operation,
+                suffix=suffix,
+            )
+            self.agg_joiners_.append(agg_joiner)
+
+        for i, table in enumerate(X):
+            self.agg_joiners_[i].fit(table)
+
+        return self
+
+    def transform(self, X):
+        """Left-join pre-aggregated tables on `X`.
+
+        Parameters
+        ----------
+        X : DataFrameLike
+            The input data to transform.
+
+        Returns
+        -------
+        X_transformed : DataFrameLike
+            The augmented input.
+        """
+
+        # check_is_fitted(self, "agg_joiners_")
+        # skrub_px, _ = get_df_namespace(
+        # *[aux_table for aux_table, _ in self.aux_table_]
+        # )
+        #
+        # for agg_joiner in self.agg_joiners_:
+        #     X = skrub_px.join(
+        #         left=X,
+        #         right=aux_table,
+        #         left_on=self.main_key_,
+        #         right_on=aux_key,
+        #     )
+        #
+
+        # OR from old AggJoiner:
+        # check_is_fitted(self, "aux_table_")
+        # skrub_px, _ = get_df_namespace(*[aux_table
+        #                           for aux_table, _ in self.aux_table_])
+        #
+        # for aux_table, aux_key in self.aux_table_:
+        #     X = skrub_px.join(
+        #         left=X,
+        #         right=aux_table,
+        #         left_on=self.main_key_,
+        #         right_on=aux_key,
+        #     )
+        return X
