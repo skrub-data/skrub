7548739175cbe20c6a44ff24dccf29d6814a6eca
Jerome Dockes <jerome@dockes.org>
2024-01-27 18:09:58 +0100

    to_categorical dont' transform categorical

--- skrub/_to_categorical.py
+++ skrub/_to_categorical.py
@@ -15,22 +15,24 @@ class ToCategorical(BaseEstimator):
         if sbd.is_categorical(column):
             self.output_native_dtype_ = sbd.native_dtype(column)
             return column
-        if sbd.is_numeric(column) or sbd.is_anydate(column) or sbd.is_bool(column):
+        if not sbd.is_string(column):
             return NotImplemented
         categories = list(sbd.unique(column))
         if self.cardinality_threshold <= len(categories):
             return NotImplemented
         token = _utils.random_string()
         self.unknown_category_ = f"skrub_unknown_category_{token}"
-        self.categories_ = categories + [self.unknown_category_]
+        self._categories = categories + [self.unknown_category_]
         self.output_native_dtype_ = sbd.make_categorical_dtype_for(
-            column, self.categories_
+            column, self._categories
         )
         return self.transform(column)
 
     def transform(self, column):
+        if sbd.is_categorical(column):
+            return column
         column = asdfapi(column)
-        dfapi_categories = dfapi_ns(column).column_from_sequence(self.categories_)
+        dfapi_categories = dfapi_ns(column).column_from_sequence(self._categories)
         mask = asnative(column.is_in(dfapi_categories) | column.is_null())
         column = sbd.where(asnative(column), mask, [self.unknown_category_])
         return sbd.native_cast(asnative(column), self.output_native_dtype_)
