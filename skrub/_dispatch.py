38ad8ebd27c437f6b564435f9fac4e212f37ae36
Jerome Dockes <jerome@dockes.org>
2024-02-14 13:29:39 +0100

    add docstring to dispatch module

--- skrub/_dispatch.py
+++ skrub/_dispatch.py
@@ -1,3 +1,139 @@
+"""
+Allow specializing a function for different dataframe libraries.
+
+Many examples of functions using this decorator can be seen in the
+``skrub._dataframe._common`` module.
+
+When this decorator is applied to a function, the function becomes a generic
+function for which we can register several implementations. The implementation
+to use is chosen when the function is called, based on the type of the
+**first argument**.
+
+The decorator is a thin wrapper around the standard library's
+``functools.singledispatch``. The difference is that we register specializations
+by providing the dataframe library's name as a string, rather than providing an
+actual type. This is necessary because some backends (ATM, only polars) are
+optional dependencies and may not be installed. Therefore, it may not be
+possible to import the type ``polars.DataFrame`` and use it to register an
+implementation for a standard ``@singledispatch`` function.
+
+Once ``dispatch`` has been applied to a function, it has a ``specialize``
+attribute that can be used to register implementations.
+
+>>> from skrub._dispatch import dispatch
+
+>>> @dispatch
+... def my_function(df, other_arg=1.0):
+...     # here we can either provide a default implementation or:
+...     raise NotImplementedError()
+
+my_function now has a ``specialize`` attribute we can use to register
+implementations:
+
+>>> @my_function.specialize("pandas")
+... def _my_function_pandas(df, other_arg=1.0):
+...     print("calling pandas implementation")
+
+The name ``_my_function_pandas`` is arbitrary, we could use anything such as
+``_`` (unless we want to be able to call the specialized implementation
+directly, eg for testing).
+
+The tentative convention in skrub is to use
+``f"_{generic_function_name.lstrip('_')}_{library_name}"``.
+
+Note that we registered this implementation without needing to import pandas.
+In the same way, we can define an implementation for polars even if it is not
+installed.
+
+>>> @my_function.specialize("polars")
+... def _my_function_polars(df, other_arg=1.0):
+...     print("calling polars implementation")
+
+Now when we call ``my_function`` with a dataframe, the correct implementation is
+chosen based on the type of the first argument.
+
+>>> import pandas as pd
+>>> df = pd.DataFrame(dict(a=[3, 4]))
+>>> my_function(df)
+calling pandas implementation
+
+Inside a specialized implementation, it is safe to import the corresponding
+module or to call methods on the first argument that only exist for this
+specific type.
+
+
+>>> @my_function.specialize("polars")
+... def _my_function_polars(df, other_arg=1.0):
+...     print("calling polars implementation")
+...     # we know df is a polars DataFrame, LazyFrame or Series (see how to
+...     # specialize for only one of those types below)
+...     df.shape             # ok
+...     df.to_pandas()       # ok
+...     import polars as pl  # ok
+
+
+Outside such specialization, skrub code **must not** directly access methods of
+a container, as this would rely on backend-specific behavior:
+
+
+>>> def my_other_function(df):
+...     # not ok: will fail if df is a polars DataFrame
+...     df.values
+...     # not ok: only happens to work by chance because both polars and pandas use
+...     # the same name with the same signature and semantics, but could fail for
+...     # other backends added in the future
+...     df.shape
+...     # ok
+...     import skrub._dataframe as ns
+...     ns.shape(df)
+
+
+We can inspect which implementations were registered:
+
+>>> my_function.registry
+mappingproxy({<class 'object'>: <function my_function at 0x7f4a0f0fb2e0>, <class 'pandas.core.frame.DataFrame'>: <function _my_function_pandas at 0x7f4953291d00>, <class 'pandas.core.series.Series'>: <function _my_function_pandas at 0x7f4953291d00>, <class 'polars.dataframe.frame.DataFrame'>: <function _my_function_polars at 0x7f4953291e40>, <class 'polars.lazyframe.frame.LazyFrame'>: <function _my_function_polars at 0x7f4953291e40>, <class 'polars.series.series.Series'>: <function _my_function_polars at 0x7f4953291e40>})
+
+It is also possible to register a specialization specifically for dataframes or for columns
+
+>>> @my_function.specialize("pandas", "Column")
+... def _my_function_pandas_column(df, other_arg=1.0):
+...     print("calling pandas Series implementation")
+...     print("               ^^^^^^               ")
+
+>>> my_function(pd.Series([0]))
+calling pandas Series implementation
+               ^^^^^^
+>>> my_function(pd.DataFrame())
+calling pandas implementation
+
+Note that the last registered implementation wins -- there is no magic to
+prioritize specializations based on how specific they are.
+
+>>> @my_function.specialize("pandas")
+... def _override_all_pandas_specializations(df, other_arg=1.0):
+...     print("Any pandas (Series or DataFrame)")
+
+>>> my_function(pd.Series([0]))
+Any pandas (Series or DataFrame)
+>>> my_function(pd.DataFrame())
+Any pandas (Series or DataFrame)
+
+Importantly, single dispatch is done based on the type of the
+**first argument**. This means our generic function should be written in such a way
+that the first argument is the dataframe or column that will determine which
+implementation to use. For example the following would not work:
+
+>>> @dispatch
+... def sample(n_rows, df):
+...     ...
+
+Instead we must rewrite it to be:
+
+>>> @dispatch
+... def sample(df, n_rows):
+...     ...
+"""
+
 from functools import singledispatch
 
 
