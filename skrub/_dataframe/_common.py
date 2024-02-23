57fdccb2fe91c41df5d2d808ab06b4953c89d1be
Jerome Dockes <jerome@dockes.org>
2024-02-23 15:19:51 +0100

    bools are not numbers

--- skrub/_dataframe/_common.py
+++ skrub/_dataframe/_common.py
@@ -464,15 +464,15 @@ def is_numeric(column):
 
 @is_numeric.specialize("pandas")
 def _is_numeric_pandas(column):
-    return pandas.api.types.is_numeric_dtype(column) or pandas.api.types.is_bool_dtype(
+    # polars and pandas disagree about whether Booleans are numbers
+    return pandas.api.types.is_numeric_dtype(
         column
-    )
+    ) and not pandas.api.types.is_bool_dtype(column)
 
 
 @is_numeric.specialize("polars")
 def _is_numeric_polars(column):
-    # polars and pandas disagree about whether Booleans are numbers
-    return column.dtype.is_numeric() or column.dtype == pl.Boolean
+    return column.dtype.is_numeric()
 
 
 @dispatch
