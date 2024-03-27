5746b230663152a0ca45157a67c4228c4e464a23
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2024-03-27 14:37:39 +0100

    Rename _test_utils to _testing_utils, remove is_module_pandas/polars

--- skrub/_dataframe/_testing_utils.py
+++ skrub/_dataframe/_testing_utils.py
@@ -10,24 +10,16 @@ except ImportError:
 from .._dispatch import dispatch
 
 
-def is_module_pandas(px):
-    return px.__name__ == "pandas"
-
-
-def is_module_polars(px):
-    return px.__name__ == "polars"
-
-
 @dispatch
 def assert_frame_equal(left, right):
     raise NotImplementedError()
 
 
-@assert_frame_equal.specialize("pandas")
+@assert_frame_equal.specialize("pandas", argument_type="DataFrame")
 def _assert_frame_equal_pandas(left, right):
     return pd.testing.assert_frame_equal(left, right)
 
 
-@assert_frame_equal.specialize("polars")
+@assert_frame_equal.specialize("polars", argument_type="DataFrame")
 def assert_frame_equal_polars(left, right):
     return pl.testing.assert_frame_equal(left, right)
