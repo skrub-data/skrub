6b7e379b37a25c302f898e6f36ae36644f9c2c2b
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2024-06-18 17:35:21 +0200

    Iter test_joiner

--- skrub/tests/test_joiner.py
+++ skrub/tests/test_joiner.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 import pytest
 from numpy.testing import assert_array_equal
 
@@ -48,22 +47,17 @@ def test_fit_transform(main_table, aux_table):
 
 
 def test_wrong_main_key(main_table, aux_table):
-    false_joiner = Joiner(aux_table=aux_table, main_key="Countryy", aux_key="country")
-
-    with pytest.raises(
-        ValueError,
-        match="do not exist in 'X'",
-    ):
-        false_joiner.fit(main_table)
+    wrong_joiner = Joiner(aux_table=aux_table, main_key="wrong_key", aux_key="country")
+    with pytest.raises(ValueError, match="do not exist in 'X'"):
+        wrong_joiner.fit(main_table)
 
 
 def test_wrong_aux_key(main_table, aux_table):
-    false_joiner2 = Joiner(aux_table=aux_table, main_key="Country", aux_key="bad")
-    with pytest.raises(
-        ValueError,
-        match="do not exist in 'aux_table'",
-    ):
-        false_joiner2.fit(main_table)
+    wrong_joiner_2 = Joiner(
+        aux_table=aux_table, main_key="Country", aux_key="wrong_key"
+    )
+    with pytest.raises(ValueError, match="do not exist in 'aux_table'"):
+        wrong_joiner_2.fit(main_table)
 
 
 def test_multiple_keys(df_module):
@@ -75,8 +69,8 @@ def test_multiple_keys(df_module):
     )
     joiner_list = Joiner(
         aux_table=df2,
-        aux_key=["CO", "CA"],
         main_key=["Co", "Ca"],
+        aux_key=["CO", "CA"],
         add_match_info=False,
     )
     result = joiner_list.fit_transform(df)
@@ -91,9 +85,9 @@ def test_multiple_keys(df_module):
     df_module.assert_frame_equal(result, expected)
 
 
-def test_pandas_aux_table_index():
-    main_table = pd.DataFrame({"Country": ["France", "Italia", "Georgia"]})
-    aux_table = pd.DataFrame(
+def test_pandas_aux_table_index(df_module):
+    main_table = df_module.make_dataframe({"Country": ["France", "Italia", "Georgia"]})
+    aux_table = df_module.make_dataframe(
         {
             "Country": ["Germany", "France", "Italy"],
             "Capital": ["Berlin", "Paris", "Rome"],
@@ -114,15 +108,18 @@ def test_pandas_aux_table_index():
     ]
 
 
-def test_bad_ref_dist():
-    table = pd.DataFrame({"A": [1, 2]})
-    joiner = Joiner(table, key="A", ref_dist="bad")
-    with pytest.raises(ValueError, match="got 'bad'"):
+def test_wrong_ref_dist(df_module):
+    table = df_module.make_dataframe({"A": [1, 2]})
+    joiner = Joiner(table, key="A", ref_dist="wrong_ref_dist")
+    with pytest.raises(
+        ValueError, match=r"('ref_dist' should be one of)*(got 'wrong_ref_dist')"
+    ):
         joiner.fit(table)
 
 
 @pytest.mark.parametrize("max_dist", [np.inf, float("inf"), "inf", None])
-def test_max_dist(max_dist):
-    table = pd.DataFrame({"A": [1, 2]})
-    joiner = Joiner(table, key="A", max_dist=max_dist, suffix="_").fit(table)
+def test_max_dist(main_table, aux_table, max_dist):
+    joiner = Joiner(
+        aux_table, main_key="Country", aux_key="country", max_dist=max_dist
+    ).fit(main_table)
     assert joiner.max_dist_ == np.inf
