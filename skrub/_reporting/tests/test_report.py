0eebd4d37ac625d96e6f90965ea5f2c5a4ccdfc6
Jerome Dockes <jerome@dockes.org>
2024-07-04 16:03:59 +0200

    testing

--- skrub/_reporting/tests/test_report.py
+++ skrub/_reporting/tests/test_report.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import pandas as pd
+import pytest
 
 from skrub import TableReport
 
@@ -9,10 +10,14 @@ def test_report():
     data_dir = pathlib.Path(__file__).parent / "data"
     fname = "air_quality_no2_long.parquet"
     data_file = data_dir / fname
-    df = pd.read_parquet(data_file)
+    try:
+        df = pd.read_parquet(data_file)
+    except ImportError:
+        pytest.skip("missing pyarrow, cannot read parquet")
     report = TableReport(df)
     report.html
     report.html_snippet
     report.json
     report._any_summary
     report._repr_mimebundle_()
+    report._repr_html_()
