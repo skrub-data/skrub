a842a5f7d0cd405be2bf98ec35185dd97d1892bb
Jerome Dockes <jerome@dockes.org>
2024-07-04 10:46:46 +0200

    put the report in a shadowDOM

--- skrub/_reporting/_data/templates/skrubview.js
+++ skrub/_reporting/_data/templates/skrubview.js
@@ -1,49 +1,228 @@
-function updateColSelection(event) {
-    updateSelectedColsSnippet(event.target.dataset.reportId);
-}
+class SkrubTableReport extends HTMLElement {
+    constructor() {
+        super();
+    }
 
-function isSelectedCol(columnElem) {
-    const checkboxElem = columnElem.querySelector(
-        "input.skrubview-select-column-checkbox[type='checkbox']");
-    return checkboxElem && checkboxElem.checked;
-}
+    connectedCallback() {
+        const template = document.getElementById(`${this.id}-template`);
+        this.attachShadow({
+            mode: "open"
+        });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
 
-function updateSelectedColsSnippet(reportId) {
-    const reportElem = document.getElementById(reportId);
-    const allCols = reportElem.querySelectorAll(".skrubview-column-summary");
-    const selectedCols = Array.from(allCols).filter(c => isSelectedCol(c));
-    const snippet = selectedCols.map(col => col.dataset.nameRepr).join(", ");
-    const bar = reportElem.querySelector(".selected-columns-box");
-    bar.textContent = "[" + snippet + "]";
-}
+    updateSelectedColsSnippet() {
+        const allCols = this.shadowRoot.querySelectorAll(
+            ".skrubview-column-summary");
+        const selectedCols = Array.from(allCols).filter(c => this.isSelectedCol(c));
+        const snippet = selectedCols.map(col => col.dataset.nameRepr).join(", ");
+        const bar = this.shadowRoot.querySelector(".selected-columns-box");
+        bar.textContent = "[" + snippet + "]";
+    }
 
-function clearSelectedCols(reportId) {
-    const reportElem = document.getElementById(reportId);
-    reportElem.querySelectorAll(
-        "input.skrubview-select-column-checkbox[type='checkbox']").forEach(
-        box => {
-            box.checked = false;
+    isSelectedCol(columnElem) {
+        const checkboxElem = columnElem.querySelector(
+            "input.skrubview-select-column-checkbox[type='checkbox']");
+        return checkboxElem && checkboxElem.checked;
+    }
+
+    updateBarContent(barId) {
+        const bar = this.shadowRoot.getElementById(barId);
+        const select = this.shadowRoot.getElementById(bar.dataset.selectorId);
+        const selectedOption = select.options[select.selectedIndex];
+        const selectedOptionValue = selectedOption.value;
+        const contentAttribute = `data-content-${selectedOptionValue}`;
+        if (!bar.hasAttribute(contentAttribute)) {
+            bar.textContent = selectedOption.dataset.placeholder;
+            bar.dataset.showsPlaceholder = "";
+        } else {
+            bar.textContent = bar.getAttribute(contentAttribute);
+            bar.removeAttribute("data-shows-placeholder");
         }
-    );
-    updateSelectedColsSnippet(reportId);
-}
+    }
 
-function selectAllCols(reportId) {
-    const reportElem = document.getElementById(reportId);
-    reportElem.querySelectorAll(".skrubview-column-summary").forEach(
-        elem => {
-            const box = elem.querySelector(
-                "input.skrubview-select-column-checkbox[type='checkbox']");
-            if (!(box === null)) {
-                box.checked = !elem.hasAttribute("data-is-excluded-by-filter");
+    clearSelectedCols() {
+        this.shadowRoot.querySelectorAll(
+            "input.skrubview-select-column-checkbox[type='checkbox']").forEach(
+            box => {
+                box.checked = false;
             }
+        );
+        this.updateSelectedColsSnippet();
+    }
+
+    selectAllCols() {
+        this.shadowRoot.querySelectorAll(".skrubview-column-summary").forEach(
+            elem => {
+                const box = elem.querySelector(
+                    "input.skrubview-select-column-checkbox[type='checkbox']"
+                );
+                if (!(box === null)) {
+                    box.checked = !elem.hasAttribute(
+                        "data-is-excluded-by-filter");
+                }
+            }
+        );
+        this.updateSelectedColsSnippet();
+    }
+
+    displayTab(tabButtonId, removeWarnings = false) {
+        const tabButton = this.shadowRoot.getElementById(tabButtonId);
+        const tab = this.shadowRoot.getElementById(tabButton.dataset.targetTab);
+        tabButton.parentElement.querySelectorAll("button").forEach(elem => {
+            elem.removeAttribute("data-is-selected");
+        });
+        tab.parentElement.querySelectorAll(".skrubview-tab").forEach(elem => {
+            elem.removeAttribute("data-is-displayed");
+        });
+        tabButton.setAttribute("data-is-selected", "");
+        tab.setAttribute("data-is-displayed", "");
+        if (removeWarnings && tabButton.hasAttribute("data-has-warning")) {
+            tabButton.removeAttribute("data-has-warning");
         }
-    );
-    updateSelectedColsSnippet(reportId);
+    }
+
+    displayValue(event) {
+        const elem = event.target;
+        const table = elem.closest("table");
+        table.setAttribute("data-selected-column", elem.dataset.colNameStr);
+        table.querySelectorAll(".skrubview-table-cell").forEach(cell => {
+            cell.removeAttribute("data-is-selected");
+            if (cell.dataset.columnIdx === elem.dataset.columnIdx) {
+                cell.setAttribute("data-is-in-selected-column", "");
+            } else {
+                cell.removeAttribute("data-is-in-selected-column");
+            }
+        });
+        table.querySelectorAll("th").forEach(head => {
+            if (head.dataset.columnIdx === elem.dataset.columnIdx) {
+                head.setAttribute("data-is-in-selected-column", "");
+            } else {
+                head.removeAttribute("data-is-in-selected-column");
+            }
+        });
+        elem.setAttribute("data-is-selected", "");
+
+        const bar = this.shadowRoot.getElementById("top-bar");
+        const barToggle = bar.closest(".skrubview-top-bar-toggle");
+        barToggle.setAttribute("data-predicate", "true");
+        bar.setAttribute("data-content-table-cell-value", elem.dataset.valueStr);
+        bar.setAttribute("data-content-table-cell-repr", elem.dataset.valueRepr);
+        bar.setAttribute("data-content-table-column-name", elem.dataset.colNameStr);
+        bar.setAttribute("data-content-table-column-name-repr", elem.dataset
+            .colNameRepr);
+
+        const snippet = filterSnippet(elem.dataset.colNameRepr,
+            elem.dataset.valueRepr,
+            elem.hasAttribute("data-value-is-none"),
+            elem.dataset.dataframeModule);
+        bar.setAttribute(`data-content-table-cell-filter`, snippet);
+
+        this.revealColCard(elem.dataset.columnIdx);
+        this.updateBarContent("top-bar");
+    }
+
+    revealColCard(colIdx) {
+        const allCols = this.shadowRoot.querySelectorAll(
+            ".skrubview-columns-in-sample-tab .skrubview-column-summary");
+        allCols.forEach(col => {
+            col.removeAttribute("data-is-selected-in-table");
+        });
+        if (colIdx === null) {
+            return;
+        }
+        const targetCol = this.shadowRoot.getElementById(
+            `col_${colIdx}_in_sample_tab`);
+        targetCol.dataset.isSelectedInTable = "";
+
+    }
+
+    clearTableCellSelection() {
+        const tableElem = this.shadowRoot.getElementById("sample-table");
+        tableElem.querySelectorAll("th, td").forEach(
+            cell => {
+                cell.removeAttribute("data-is-selected");
+                cell.removeAttribute("data-is-in-selected-column");
+            });
+        tableElem.removeAttribute("data-selected-cell");
+        const bar = this.shadowRoot.getElementById("top-bar");
+        const barToggle = bar.closest(".skrubview-top-bar-toggle");
+        barToggle.setAttribute("data-predicate", "false");
+        bar.removeAttribute("data-content-table-cell-value");
+        bar.removeAttribute("data-content-table-cell-repr");
+        bar.removeAttribute("data-content-table-column-name");
+        bar.removeAttribute("data-content-table-column-name-repr");
+        bar.removeAttribute("data-content-table-cell-filter");
+        this.updateBarContent("top-bar");
+        this.revealColCard(null);
+    }
+
+    onFilterChange() {
+        const selectElem = this.shadowRoot.getElementById("col-filter-select");
+        const colFilters = window[`columnFiltersForReport${this.id}`];
+        const filterName = selectElem.value;
+        const acceptedCols = colFilters[filterName]["columns"];
+        const colElements = this.shadowRoot.querySelectorAll(
+            ".skrubview-filterable-column");
+        colElements.forEach(elem => {
+            if (acceptedCols.includes(elem.dataset.columnName)) {
+                elem.removeAttribute("data-is-excluded-by-filter");
+            } else {
+                elem.dataset.isExcludedByFilter = "";
+            }
+        });
+        this.shadowRoot.getElementById("display-n-columns").textContent =
+            acceptedCols
+            .length.toString();
+        const tableElem = this.shadowRoot.getElementById("sample-table");
+        if (!acceptedCols.includes(tableElem.dataset.selectedColumn)) {
+            this.clearTableCellSelection();
+        }
+        for (let toggleSelector of [".skrubview-table-sample-toggle",
+                ".skrubview-column-summaries-toggle"
+            ]) {
+            const toggle = this.shadowRoot.querySelector(toggleSelector);
+            toggle.dataset.predicate = acceptedCols.length === 0 ? "false" : "true";
+            const filterDisplay = toggle.querySelector(
+                ".skrubview-selected-filter-display");
+            filterDisplay.textContent = '"' + colFilters[filterName][
+                "display_name"
+            ] + '"';
+        }
+    }
+
+
+    clearColFilter(event) {
+        const selectElem = this.shadowRoot.getElementById("col-filter-select");
+        selectElem.value = "all()";
+        this.onFilterChange();
+    }
+
 }
 
-function copyTextToClipboard(elementID) {
-    const elem = document.getElementById(elementID);
+customElements.define("skrub-table-report", SkrubTableReport);
+
+function initReport(reportId) {
+    const report = document.getElementById(reportId);
+    report.updateBarContent("top-bar");
+    report.updateSelectedColsSnippet();
+    report.displayTab("sample-tab-button");
+    report.onFilterChange();
+}
+
+function displayTab(event) {
+    const button = event.target;
+    button.getRootNode().host.displayTab(button.id);
+}
+
+
+function copyButtonClick(event) {
+    const button = event.target;
+    const textElem = button.getRootNode().getElementById(button.dataset.targetId);
+    copyTextToClipboard(textElem);
+}
+
+function copyTextToClipboard(elem) {
     if (elem.hasAttribute("data-shows-placeholder")) {
         return;
     }
@@ -92,89 +271,13 @@ function filterSnippet(colName, value, valueIsNone, dataframeModule) {
     return `Unknown dataframe library: ${dataframeModule}`;
 }
 
-function updateBarContent(barId) {
-    const bar = document.getElementById(barId);
-    const select = document.getElementById(bar.dataset.selectorId);
-    const selectedOption = select.options[select.selectedIndex];
-    const selectedOptionValue = selectedOption.value;
-    const contentAttribute = `data-content-${selectedOptionValue}`;
-    if (!bar.hasAttribute(contentAttribute)) {
-        bar.textContent = selectedOption.dataset.placeholder;
-        bar.dataset.showsPlaceholder = "";
-    } else {
-        bar.textContent = bar.getAttribute(contentAttribute);
-        bar.removeAttribute("data-shows-placeholder");
-    }
-}
-
 function updateSiblingBarContents(event) {
     const select = event.target;
+    const report = select.getRootNode().host;
     select.parentElement.querySelectorAll(`*[data-selector-id=${select.id}]`).forEach(
         elem => {
-            updateBarContent(elem.id);
-        })
-}
-
-function displayValue(event) {
-    const elem = event.target;
-    const table = document.getElementById(elem.dataset.parentTableId);
-    table.setAttribute("data-selected-column", elem.dataset.colNameStr);
-    table.querySelectorAll(".skrubview-table-cell").forEach(cell => {
-        cell.removeAttribute("data-is-selected");
-        if (cell.dataset.columnIdx === elem.dataset.columnIdx) {
-            cell.setAttribute("data-is-in-selected-column", "");
-        } else {
-
-            cell.removeAttribute("data-is-in-selected-column");
-        }
-    });
-    table.querySelectorAll("th").forEach(head => {
-        if (head.dataset.columnIdx === elem.dataset.columnIdx) {
-            head.setAttribute("data-is-in-selected-column", "");
-        } else {
-            head.removeAttribute("data-is-in-selected-column");
-        }
-    });
-    elem.setAttribute("data-is-selected", "");
-
-    const topBarId = table.dataset.topBarId;
-    const bar = document.getElementById(topBarId);
-    const barToggle = bar.closest(".skrubview-top-bar-toggle");
-    barToggle.setAttribute("data-predicate", "true");
-    bar.setAttribute("data-content-table-cell-value", elem.dataset.valueStr);
-    bar.setAttribute("data-content-table-cell-repr", elem.dataset.valueRepr);
-    bar.setAttribute("data-content-table-column-name", elem.dataset.colNameStr);
-    bar.setAttribute("data-content-table-column-name-repr", elem.dataset.colNameRepr);
-
-    const snippet = filterSnippet(elem.dataset.columnNameRepr,
-        elem.dataset.valueRepr,
-        elem.hasAttribute("data-value-is-none"),
-        elem.dataset.dataframeModule);
-    bar.setAttribute(`data-content-table-cell-filter`, snippet);
-
-    revealColCard(table.dataset.reportId, elem.dataset.columnIdx);
-
-    updateBarContent(topBarId);
-}
-
-function clearTableCellSelection(tableElem) {
-    tableElem.querySelectorAll("th, td").forEach(
-        cell => {
-            cell.removeAttribute("data-is-selected");
-            cell.removeAttribute("data-is-in-selected-column");
+            report.updateBarContent(elem.id);
         });
-    tableElem.removeAttribute("data-selected-cell");
-    const topBarId = tableElem.dataset.topBarId;
-    const bar = document.getElementById(topBarId);
-    const barToggle = bar.closest(".skrubview-top-bar-toggle");
-    barToggle.setAttribute("data-predicate", "false");
-    bar.removeAttribute("data-content-table-cell-value");
-    bar.removeAttribute("data-content-table-cell-repr");
-    bar.removeAttribute("data-content-table-column-name");
-    bar.removeAttribute("data-content-table-column-name-repr");
-    bar.removeAttribute("data-content-table-cell-filter");
-    updateBarContent(topBarId);
-    revealColCard(tableElem.closest(".skrubview-report").id, null);
 }
 
 function displayFirstCellValue(event) {
@@ -185,75 +288,4 @@ function displayFirstCellValue(event) {
     if (firstCell) {
         firstCell.click();
     }
-}
-
-function revealColCard(reportId, colIdx) {
-    const reportElem = document.getElementById(reportId);
-    const allCols = reportElem.querySelectorAll(
-        ".skrubview-columns-in-sample-tab .skrubview-column-summary");
-    allCols.forEach(col => {
-        col.removeAttribute("data-is-selected-in-table");
-    });
-    if (colIdx === null) {
-        return;
-    }
-    const targetCol = document.getElementById(
-        `${reportId}_col_${colIdx}_in_sample_tab`);
-    targetCol.dataset.isSelectedInTable = "";
-
-}
-
-function displayTab(event, removeWarnings=false) {
-    const elem = event.target;
-    elem.parentElement.querySelectorAll("button").forEach(elem => {
-        elem.removeAttribute("data-is-selected");
-    });
-    elem.setAttribute("data-is-selected", "");
-    const tab = document.getElementById(elem.dataset.targetTab);
-    tab.parentElement.querySelectorAll(".skrubview-tab").forEach(elem => {
-        elem.removeAttribute("data-is-displayed");
-    });
-    tab.setAttribute("data-is-displayed", "");
-    if (removeWarnings && elem.hasAttribute("data-has-warning")) {
-        elem.removeAttribute("data-has-warning");
-    }
-}
-
-function onFilterChange(colFilterId) {
-    const selectElem = document.getElementById(colFilterId);
-    const reportId = selectElem.dataset.reportId;
-    const colFilters = window[`columnFiltersForReport${reportId}`];
-    const filterName = selectElem.value;
-    const acceptedCols = colFilters[filterName]["columns"];
-    const reportElem = document.getElementById(reportId);
-    const colElements = reportElem.querySelectorAll(".skrubview-filterable-column");
-    colElements.forEach(elem => {
-        if (acceptedCols.includes(elem.dataset.columnName)) {
-            elem.removeAttribute("data-is-excluded-by-filter");
-        } else {
-            elem.dataset.isExcludedByFilter = "";
-        }
-    });
-    document.getElementById(`${reportId}_display_n_columns`).textContent = acceptedCols
-        .length.toString();
-    const tableElem = reportElem.querySelector(".skrubview-dataframe-sample-table");
-    if (!acceptedCols.includes(tableElem.dataset.selectedColumn)) {
-        clearTableCellSelection(tableElem);
-    }
-    for (let toggleSelector of [".skrubview-table-sample-toggle",
-            ".skrubview-column-summaries-toggle"
-        ]) {
-        const toggle = reportElem.querySelector(toggleSelector);
-        toggle.dataset.predicate = acceptedCols.length === 0 ? "false" : "true";
-        const filterDisplay = toggle.querySelector(
-            ".skrubview-selected-filter-display");
-        filterDisplay.textContent = '"' + colFilters[filterName]["display_name"] + '"';
-    }
-}
-
-function clearColFilter(event){
-    const reportElem = event.target.closest(".skrubview-report");
-    const selectElem = reportElem.querySelector(".skrubview-col-filter-select");
-    selectElem.value = "all()";
-    onFilterChange(selectElem.id);
 }
