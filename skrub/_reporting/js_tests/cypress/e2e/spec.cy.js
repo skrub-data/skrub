9eec90c42519da24ec8d7dcaaa9b4fc8f8afd02d
Jerome Dockes <jerome@dockes.org>
2024-07-08 09:50:30 +0200

    test opening & closing card

--- skrub/_reporting/js_tests/cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-      cy.visit('_reports/employee_salaries.html');
-      cy.get('skrub-table-report').shadow().find('td').first().click();
-  })
-})
