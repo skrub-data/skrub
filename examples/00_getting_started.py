e5c25eb1a6ca6bcbde4a57149a52442828180746
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2024-07-16 16:38:15 +0200

    Format

--- examples/00_getting_started.py
+++ examples/00_getting_started.py
@@ -2,12 +2,13 @@
 Getting Started
 ===============
 
-The purpose of this guide is to provide an introduction to the functionalities of ``skrub``, an
-open-source package that aims at bridging the gap between tabular data sources and machine-learning models.
-Please refer to our `installation guidelines <https://skrub-data.org/stable/install.html>`_ for installing ``skrub``.
+The purpose of this guide is to provide an introduction to the functionalities of
+``skrub``, an open-source package that aims at bridging the gap between tabular
+data sources and machine-learning models. Please refer to our `installation guidelines
+<https://skrub-data.org/stable/install.html>`_ for installing ``skrub``.
 
-Much of ``skrub`` revolves around vectorizing, assembling, and encoding tabular data, to prepare data in a format that
-machine-learning models understand.
+Much of ``skrub`` revolves around vectorizing, assembling, and encoding tabular data,
+to prepare data in a format that machine-learning models understand.
 """
 
 # %%
@@ -15,7 +16,7 @@ machine-learning models understand.
 # ----------------------------
 #
 # The :obj:`~skrub.datasets` module allows us to download tabular datasets and
-# demonstrate skrub's features.
+# demonstrate ``skrub``'s features.
 
 # %%
 from skrub.datasets import fetch_employee_salaries
@@ -24,7 +25,8 @@ dataset = fetch_employee_salaries()
 employees_df, salaries = dataset.X, dataset.y
 
 # %%
-# Explore all the available `datasets <https://skrub-data.org/stable/reference/downloading_a_dataset>`_.
+# Explore all the available `datasets
+# <https://skrub-data.org/stable/reference/downloading_a_dataset>`_.
 
 
 # %%
@@ -45,9 +47,9 @@ TableReport(employees_df)
 # Easily building a strong baseline for tabular machine learning
 # --------------------------------------------------------------
 #
-# The goal of ``skrub`` is to ease tabular data preparation for machine learning. The :func:`~skrub.tabular_learner`
-# function provides an easy way to build a simple but reliable machine-learning model, working well on
-#  most tabular data.
+# The goal of ``skrub`` is to ease tabular data preparation for machine learning.
+# The :func:`~skrub.tabular_learner` function provides an easy way to build a simple
+# but reliable machine-learning model, working well on most tabular data.
 
 
 # %%
@@ -72,11 +74,12 @@ results["test_score"]
 # Assembling data
 # ---------------
 #
-# ``Skrub`` allows imperfect assembly of data, in the case where columns are dirty and can contain typos.
+# ``Skrub`` allows imperfect assembly of data, in the case where columns are dirty
+# and can contain typos.
 #
-# mThe :class:`~skrub.Joiner`
-# allows to fuzzy-join multiple tables, and each row of a main table will be augmented with values from the best match
-# in the auxiliary table. You can control how distant fuzzy-matches are allowed to be with the ``max_dist`` parameter.
+# The :class:`~skrub.Joiner` allows to fuzzy-join multiple tables, and each row of
+# a main table will be augmented with values from the best match in the auxiliary table.
+# You can control how distant fuzzy-matches are allowed to be with the ``max_dist`` parameter.
 
 # %%
 # In the following, we add information about capitals to a table of countries:
@@ -98,11 +101,12 @@ joiner = Joiner(
 joiner.fit_transform(main_table)
 
 # %%
-#  Information about capitals have been added for France and Italia, but not for Georgia since it's not
-# present in the auxiliary table.
+# Information about capitals have been added for France and Italia, but not for Georgia
+# since it's not present in the auxiliary table.
 #
-# It's also possible to augment data by joining and aggregating multiple dataframes with the :class:`~skrub.AggJoiner`. This is
-# particularly useful to summarize information scattered across tables:
+# It's also possible to augment data by joining and aggregating multiple
+# dataframes with the :class:`~skrub.AggJoiner`. This is particularly useful to summarize
+# information scattered across tables:
 
 # %%
 from skrub import AggJoiner
@@ -126,23 +130,26 @@ agg_joiner = AggJoiner(
 agg_joiner.fit_transform(main)
 
 # %%
-# For joining multiple auxiliary tables on a main table at once, use the :class:`~skrub.MultiAggJoiner`.
+# For joining multiple auxiliary tables on a main table at once, use the
+# :class:`~skrub.MultiAggJoiner`.
 #
-# See other ways to join multiple tables on `assembling data <https://skrub-data.org/stable/assembling>`_.
+# See other ways to join multiple tables on
+# `assembling data <https://skrub-data.org/stable/assembling>`_.
 
 
 # %%
 # Encoding data
 # -------------
 #
-# When a column contains dirty categories, it can be encoded using one of ``skrub``'s encoders, such as
-# the :class:`~skrub.GapEncoder`.
+# When a column contains dirty categories, it can be encoded using one
+# of ``skrub``'s encoders, such as the :class:`~skrub.GapEncoder`.
 #
-# The :class:`~skrub.GapEncoder` creates a continuous encoding, based on the activation of latent categories. It
-# will create the encoding based on combinations of substrings which frequently co-occur.
+# The :class:`~skrub.GapEncoder` creates a continuous encoding, based on
+# the activation of latent categories. It will create the encoding based on
+# combinations of substrings which frequently co-occur.
 #
-# For instance, we might want to encode a column ``X`` that we know contains information about cities, being
-# either Madrid or Rome :
+# For instance, we might want to encode a column ``X`` that we know contains
+# information about cities, being either Madrid or Rome :
 
 # %%
 from skrub import GapEncoder
@@ -168,8 +175,9 @@ out = enc.transform(X)
 out
 
 # %%
-# The higher the activation, the closer the row to the latent topic. These activations can then be used to encode
-# ``X``, for instance with a 0 if the city is Madrid, and 1 if the city is Rome:
+# The higher the activation, the closer the row to the latent topic. These
+# activations can then be used to encode ``X``, for instance with a 0 if the
+# city is Madrid, and 1 if the city is Rome:
 
 # %%
 madrid = out.iloc[:,0] > out.iloc[:,1]
@@ -178,8 +186,8 @@ X[~madrid] = 1
 X
 
 # %%
-# Which correspond to the respective positions of Madrid and Rome in the initial column ! This column can now be understood
-# by a machine-learning model.
+# Which correspond to the respective positions of Madrid and Rome in the initial
+# column ! This column can now be understood by a machine-learning model.
 #
 # The other encoders are presented in `encoding <https://skrub-data.org/stable/encoding>`_.
 
@@ -188,11 +196,13 @@ X
 # Next steps
 # ----------
 #
-# We have briefly covered pipeline creation, vectorizing, assembling, and encoding data. We presented the main functionalities of
-# ``skrub``, but there is much more to it !
+# We have briefly covered pipeline creation, vectorizing, assembling, and encoding
+# data. We presented the main functionalities of ``skrub``, but there is much more to it !
 #
-# Please refer to our `User Guide <https://skrub-data.org/stable/documentation>`_ for a more in-depth presentation of
-# ``skrub``'s concepts. You can also check out our `API reference <https://skrub-data.org/stable/api>`_ for the exhaustive
-# list of functionalities !
+# Please refer to our `User Guide <https://skrub-data.org/stable/documentation>`_
+# for a more in-depth presentation of ``skrub``'s concepts. You can also check out
+# our `API reference <https://skrub-data.org/stable/api>`_ for the exhaustive list
+# of functionalities !
 #
-# Visit ourm `examples <https://skrub-data.org/stable/auto_examples>`_ for more illustrations of the tools offered by ``skrub``.
+# Visit our `examples <https://skrub-data.org/stable/auto_examples>`_ for more
+# illustrations of the tools offered by ``skrub``.
