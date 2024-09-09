192fa81fe6b5f04da0e883cd6a0236cc45108877
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2024-09-09 18:08:28 +0200

    generate table report only once

--- doc/table_report.py
+++ doc/table_report.py
@@ -1,4 +1,3 @@
-# %%
 from skrub import TableReport
 from skrub.datasets import fetch_employee_salaries
 
@@ -7,8 +6,3 @@ def generate_demo():
     X = fetch_employee_salaries().X
     with open("_templates/demo_table_report_generated.html", "w") as f:
         f.write(TableReport(X).html_snippet())
-
-
-# %%
-generate_demo()
-# %%
