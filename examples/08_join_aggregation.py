04ebad36f8cf0350cf0e393fdfd3ab03e494c8d3
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2024-10-01 21:23:48 +0200

    add schemas

--- examples/08_join_aggregation.py
+++ examples/08_join_aggregation.py
@@ -12,8 +12,13 @@ Because fraud is rare, this dataset is extremely imbalanced, with a prevalence o
 
 The data consists of two distinct entities: e-commerce "baskets", and "products".
 Baskets can be tagged fraudulent (1) or not (0), and are essentially a list of products
-of variable size. Each basket is linked to many products, e.g. basket 1 can have
-product A, B and C.
+of variable size. Each basket is linked to at least one products, e.g. basket 1 can have
+product 1 and 2.
+
+.. image:: ../../_static/08_example_data.png
+    :width: 450 px
+
+|
 
 Our aim is to predict which baskets are fraudulent.
 
@@ -35,8 +40,14 @@ Each product has several attributes:
 .. |Joiner| replace::
      :class:`~skrub.Joiner`
 
+.. |DropCols| replace::
+     :class:`~skrub.DropCols`
+
 .. |TableVectorizer| replace::
      :class:`~skrub.TableVectorizer`
+
+.. |TableReport| replace::
+     :class:`~skrub.TableReport`
 
 .. |MinHashEncoder| replace::
      :class:`~skrub.MinHashEncoder`
@@ -52,6 +63,9 @@ Each product has several attributes:
 
 .. |HGBC| replace::
      :class:`~sklearn.ensemble.HistGradientBoostingClassifier`
+
+.. |OrdinalEncoder| replace::
+     :class:`~sklearn.preprocessing.OrdinalEncoder`
 
 .. |TunedThresholdClassifierCV| replace::
      :class:`~sklearn.model_selection.TunedThresholdClassifierCV`
@@ -80,10 +94,16 @@ TableReport(baskets)
 #
 # Let's explore a naive solution first.
 #
+# .. note::
+#
+#    Click :ref:`here<agg-joiner-anchor>` to skip this section and see the AggJoiner
+#    in action!
+#
+#
 # The first idea that comes to mind to merge these two tables is to aggregate the
 # products attributes into lists, using their basket IDs.
 products_grouped = products.groupby("basket_ID").agg(list)
-products_grouped
+TableReport(products_grouped)
 
 # %%
 # Then, we can expand all lists into columns, as if we were "flattening" the dataframe.
@@ -100,7 +120,7 @@ products_flatten.insert(0, "basket_ID", products_grouped.index)
 TableReport(products_flatten)
 
 # %%
-# Look at the "Stats" section of the ``TableReport`` above. Does anything strike you?
+# Look at the "Stats" section of the |TableReport| above. Does anything strike you?
 #
 # Not only did we create 144 columns, but most of these columns are filled with NaN,
 # which is very inefficient for learning!
@@ -111,11 +131,14 @@ TableReport(products_flatten)
 #
 # Moreover, if we wanted to replace text columns with encodings, we would create
 # :math:`d \times 24 \times 2` columns (encoding of dimensionality :math:`d`, for
-# 24 products, for the ``"item"`` and ``"make"`` columns), which would explode the memory usage.
+# 24 products, for the ``"item"`` and ``"make"`` columns), which would explode the
+# memory usage.
+#
+# .. _agg-joiner-anchor:
 #
 # AggJoiner
 # ---------
-# Let's now see how the AggJoiner can help us solve this. We begin with splitting our
+# Let's now see how the |AggJoiner| can help us solve this. We begin with splitting our
 # basket dataset in a training and testing set.
 from sklearn.model_selection import train_test_split
 
@@ -124,17 +147,18 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=
 X_train.shape, y_train.shape
 
 # %%
-# As the "item" and "model" columns both expose typos and text similarities, we use a
-# MinHashEncoder.
+# Before aggregating our product dataframe, we need to vectorize our categorical
+# columns. To do so, we use:
 #
-# "make" and "goods_code" on the other hand consist in orthogonal categories. We
-# encode them using a OrdinalEncoder.
+# - |MinHashEncoder| on "item" and "model" columns, because they both expose typos
+#   and text similarities.
+# - |OrdinalEncoder| on "make" and "goods_code" columns, because they consist in
+#   orthogonal categories.
 #
-# Finally, we bring this logic into a TableVectorizer to encode these columns in a
+# We bring this logic into a |TableVectorizer| to vectorize these columns in a
 # single step.
-#
-# See `here <https://skrub-data.org/stable/auto_examples/01_encodings.html#specializing-the-tablevectorizer-for-histgradientboosting>`_
-# for more details about these choices.
+# See `this example <https://skrub-data.org/stable/auto_examples/01_encodings.html#specializing-the-tablevectorizer-for-histgradientboosting>`_
+# for more details about these encoding choices.
 from sklearn.preprocessing import OrdinalEncoder
 
 from skrub import MinHashEncoder, TableVectorizer
@@ -149,24 +173,31 @@ products_transformed = vectorizer.fit_transform(products)
 TableReport(products_transformed)
 
 # %%
-# Our objective is now to aggregate this dataframe by ``"basket_ID"``, then merging it
-# on the baskets dataframe, still on the ``"basket_ID"``.
+# Our objective is now to aggregate this vectorized product dataframe by
+# ``"basket_ID"``, then to merge it on the baskets dataframe, still on
+# the ``"basket_ID"``.
 #
-# AggJoiner can help us achieve exactly this. We need to pass the product dataframe as
-# an auxiliary table argument to AggJoiner in ``__init__``. ``aux_key`` represent both
-# the columns used to groupby on, and the columns used to join on.
+# .. image:: ../../_static/08_example_aggjoiner.png
+#    :width: 900
+#
+# |
+#
+# |AggJoiner| can help us achieve exactly this. We need to pass the product dataframe as
+# an auxiliary table argument to |AggJoiner| in ``__init__``. The ``aux_key`` argument
+# represent both the columns used to groupby on, and the columns used to join on.
 #
 # The basket dataframe is our main table, and we indicate the columns to join on with
 # ``main_key``. Note that we pass the main table during ``fit``, and we discuss the
 # limitations of this design in the conclusion at the bottom of this notebook.
 #
-# The minimum is the most appropriate operation to aggregate encodings from
-# MinHashEncoder, for reasons that are out of the scope of this notebook.
+# The minimum ("min") is the most appropriate operation to aggregate encodings from
+# |MinHashEncoder|, for reasons that are out of the scope of this notebook.
 #
-# Notice that the ``"basket_ID"`` column is dropped from the joined table.
 from skrub import AggJoiner
 from skrub import _selectors as s
 
+# Skrub selectors allow us to select columns using regexes, which reduces
+# the boilerplate.
 minhash_cols_query = s.glob("item*") | s.glob("model*")
 minhash_cols = s.select(products_transformed, minhash_cols_query).columns
 
@@ -181,16 +212,16 @@ baskets_products = agg_joiner.fit_transform(baskets)
 TableReport(baskets_products)
 
 # %%
-# Now that we understand how to use the AggJoiner, we can now assemble our pipeline by
-# chaining two AggJoiners together:
+# Now that we understand how to use the |AggJoiner|, we can now assemble our pipeline by
+# chaining two |AggJoiner| together:
 #
-# - the first one to deal with the MinHashEncoder vectors as we just saw
+# - the first one to deal with the |MinHashEncoder| vectors as we just saw
 # - the second one to deal with the all the other columns
 #
-# For the second AggJoiner, we use the mean, standard deviation, minimum and maximum
+# For the second |AggJoiner|, we use the mean, standard deviation, minimum and maximum
 # operations to extract a representative summary of each distribution.
 #
-# DropCols is another skrub transformer which removes the "ID" column, which doesn't
+# |DropCols| is another skrub transformer which removes the "ID" column, which doesn't
 # bring any information after the joining operation.
 from scipy.stats import loguniform, randint
 from sklearn.ensemble import HistGradientBoostingClassifier
@@ -245,7 +276,7 @@ print(f"This operation took {time() - tic:.1f}s")
 # %%
 # The best hyper parameters are:
 
-search.best_params_
+pd.Series(search.best_params_)
 
 # %%
 # To benchmark our performance, we plot the log loss of our model on the test set
@@ -293,7 +324,7 @@ ax.set_title("Log loss (lower is better)")
 # %%
 # Conclusion
 # ----------
-# With AggJoiner, you can bring the aggregation and joining operations within a
+# With |AggJoiner|, you can bring the aggregation and joining operations within a
 # sklearn pipeline, and train models more efficiently.
 #
 # One known limitation of both the |AggJoiner| and |Joiner| is that the auxiliary data
@@ -305,7 +336,7 @@ ax.set_title("Log loss (lower is better)")
 # the model, it can result in a massive file size on disk.
 #
 # 2. **Inflexibility with new, unseen data in a production environment:** To use new
-# auxiliary data, you would need to replace the auxiliary table in the AggJoiner that
+# auxiliary data, you would need to replace the auxiliary table in the |AggJoiner| that
 # was used during ``fit`` with the updated data, which is a rather hacky approach.
 #
 # These limitations will be addressed later in skrub.
