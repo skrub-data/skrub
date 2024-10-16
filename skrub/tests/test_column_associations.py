041ad4fd0d2b27d20c2086c9cc69a3243817ebb5
Jerome Dockes <jerome@dockes.org>
2024-10-16 16:14:17 +0200

    fix test

--- skrub/tests/test_column_associations.py
+++ skrub/tests/test_column_associations.py
@@ -2,7 +2,6 @@ import numpy as np
 import pytest
 
 from skrub import _dataframe as sbd
-from skrub import _selectors as s
 from skrub import column_associations
 
 
@@ -13,19 +12,10 @@ def test_column_associations(df_module):
     df = df_module.make_dataframe(dict(x=x, y=y, z=z))
     asso = column_associations(df)
     asso = sbd.pandas_convert_dtypes(asso)
-    df_module.assert_frame_equal(
-        s.select(asso, sbd.column_names(asso)[:-1]),
-        sbd.pandas_convert_dtypes(
-            df_module.make_dataframe(
-                {
-                    "left_column_name": ["x", "y", "x"],
-                    "left_column_idx": [0, 1, 0],
-                    "right_column_name": ["y", "z", "z"],
-                    "right_column_idx": [1, 2, 2],
-                }
-            )
-        ),
-    )
+    assert sbd.to_list(sbd.col(asso, "left_column_name")) == ["x", "y", "x"]
+    assert sbd.to_list(sbd.col(asso, "left_column_idx")) == [0, 1, 0]
+    assert sbd.to_list(sbd.col(asso, "right_column_name")) == ["y", "z", "z"]
+    assert sbd.to_list(sbd.col(asso, "right_column_idx")) == [1, 2, 2]
     assert sbd.to_list(sbd.col(asso, "cramer_v")) == pytest.approx(
         [1.0, 0.6546536, 0.6546536]
     )
