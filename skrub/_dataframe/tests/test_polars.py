452d9580ca169f9243dca01d7d671593c7498476
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2024-10-17 17:41:06 +0200

    Skip test when polars not installed

--- skrub/_dataframe/tests/test_polars.py
+++ skrub/_dataframe/tests/test_polars.py
@@ -1,3 +1,5 @@
+import pytest
+
 from skrub._dataframe._polars import rename_columns
 from skrub.conftest import _POLARS_INSTALLED
 
@@ -5,6 +7,7 @@ if _POLARS_INSTALLED:
     import polars as pl
 
 
+@pytest.mark.skipif(not _POLARS_INSTALLED, reason="Polars is not available")
 def test_rename_columns():
     df = pl.DataFrame({"a column": [1], "another": [1]})
     df = rename_columns(df, str.swapcase)
