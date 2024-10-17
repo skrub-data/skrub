f249982fab6d72f8c08ced64530902404a807ed1
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-10-17 15:24:16 +0200

    Fixed code

--- skrub/_dropnull.py
+++ skrub/_dropnull.py
@@ -1,47 +1,34 @@
-# drop columns that contain all null values 
-from ._on_each_column import SingleColumnTransformer
-
-from . import _dataframe as sbd
-from ._dispatch import dispatch
+# drop columns that contain all null values
 from sklearn.utils.validation import check_is_fitted
 
-
-@dispatch
-def _is_all_null(col):
-    raise NotImplementedError()
-
-@_is_all_null.specialize("pandas", argument_type="Column")
-def _is_all_null_pandas(col):
-    return bool(col.isna().all())
-
-
-@_is_all_null.specialize("polars", argument_type="Column")
-def _is_all_null_polars(col):
-    return (col.is_null().all())
+from . import _dataframe as sbd
+from ._on_each_column import SingleColumnTransformer
 
 __all__ = ["DropNullColumn"]
 
+
 class DropNullColumn(SingleColumnTransformer):
-        
+
     def __init__(self):
         super().__init__()
         self._is_fitted = False
-        
+
     def fit_transform(self, column, y=None):
         del y
 
         self._is_fitted = True
         return self.transform(column)
-    
+
     def transform(self, column):
-        check_is_fitted(self, )
-        
-        if _is_all_null(column):
+        check_is_fitted(
+            self,
+        )
+
+        if sbd.is_all_null(column):
             return []
         else:
             return column
-        
-        
+
     def __sklearn_is_fitted__(self):
         """
         Check fitted status and return a Boolean value.
