d3f9c901799496d9d81a4e07a7917d7c3d67c855
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-10-17 15:23:38 +0200

    renamed script

--- mm_dropcol.py
+++ mm_dropcol.py
@@ -1,17 +1,20 @@
 
 #%%
-# %load_ext autoreload
-# %autoreload 2
+%load_ext autoreload
+%autoreload 2
 #%%
 import polars as pl
 import pandas as pd
 from skrub import _dropnull
+import numpy as np
 
 # %%
 df_pl = pl.DataFrame(
-    {"idx": [1,2,3], "value": [None, None, None]}
+    {"idx": [1,2,3], "value": [np.nan, np.nan, np.nan]}
 )
-df_pd = df_pl.to_pandas()
+df_pd = pd.DataFrame(
+    {"idx": [1,2,3], "value": [np.nan, np.nan, np.nan]}
+)
 
 # %%
 dn = _dropnull.DropNullColumn()
@@ -27,8 +30,10 @@ dn.fit_transform(df_pd["value"])
 
 from skrub import TableVectorizer
 # %%
-tv = TableVectorizer(drop_null_columns=False)
+tv = TableVectorizer(drop_null_columns=True)
+
 
 # %%
 tv.fit_transform(df_pl)
+
 # %%
