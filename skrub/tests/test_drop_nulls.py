e2ca33f5c28a42276a3e8eac2916de075b613a68
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-10-21 15:57:34 +0200

    Fixing imports and refactoring names

--- skrub/tests/test_drop_nulls.py
+++ skrub/tests/test_drop_nulls.py
@@ -4,10 +4,11 @@ from numpy.testing import assert_array_equal
 
 from skrub import TableVectorizer
 from skrub._drop_null import DropNullColumn
+from skrub import _dataframe as sbd
 
 
 @pytest.fixture
-def main_table(df_module):
+def drop_null_table(df_module):
     return df_module.make_dataframe(
         {
             "idx": [
@@ -29,25 +30,25 @@ def main_table(df_module):
     )
 
 
-def test_single_column(main_table):
+def test_single_column(drop_null_table):
     dn = DropNullColumn()
-    assert dn.fit_transform(main_table["value_nan"]) == []
-    assert dn.fit_transform(main_table["value_null"]) == []
+    assert dn.fit_transform(drop_null_table["value_nan"]) == []
+    assert dn.fit_transform(drop_null_table["value_null"]) == []
 
-    assert_array_equal(ns.to_numpy(ns.col(main_table, "idx")), np.array([1, 2, 3]))
+    assert_array_equal(sbd.to_numpy(sbd.col(drop_null_table, "idx")), np.array([1, 2, 3]))
 
 
-def test_transform_nullcolumn(main_table):
-    main_table_dropped = ns.drop(main_table, "value_null")
-    main_table_dropped = ns.drop(main_table_dropped, "value_nan")
+def test_drop_null_column(drop_null_table):
+    main_table_dropped = sbd.drop(drop_null_table, "value_null")
+    main_table_dropped = sbd.drop(main_table_dropped, "value_nan")
 
     # Don't drop null columns
     tv = TableVectorizer(drop_null_columns=False)
-    transformed = tv.fit_transform(main_table)
+    transformed = tv.fit_transform(drop_null_table)
 
-    assert ns.shape(transformed) == ns.shape(main_table)
+    assert sbd.shape(transformed) == sbd.shape(drop_null_table)
 
     # Drop null columns
     tv = TableVectorizer(drop_null_columns=True)
-    transformed = tv.fit_transform(main_table)
-    assert ns.shape(transformed) == (ns.shape(main_table)[0], 1)
+    transformed = tv.fit_transform(drop_null_table)
+    assert sbd.shape(transformed) == (sbd.shape(drop_null_table)[0], 1)
