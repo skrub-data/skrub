80bd4087e3af30609e413d054466fda889ee298e
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-10-21 15:51:58 +0200

    Added docstrings

--- skrub/_drop_null.py
+++ skrub/_drop_null.py
@@ -8,19 +8,37 @@ __all__ = ["DropNullColumn"]
 
 
 class DropNullColumn(SingleColumnTransformer):
-    """
-    """
+    '''Drop a single column if it contains only null values. 
+    '''
+
     def __init__(self):
         super().__init__()
         self._is_fitted = False
 
     def fit_transform(self, column, y=None):
+        """Fit the encoder and transform a column. 
+
+        Args:
+            column : Pandas or Polars series. The input column to check. 
+            y : None. Ignored.
+
+        Returns:
+            The input column, or an empty list if the column contains only null values.
+        """
         del y
 
         self._is_fitted = True
         return self.transform(column)
 
     def transform(self, column):
+        """Transform a column.
+
+        Args:
+            column : Pandas or Polars series. The input column to check. 
+
+        Returns:
+            The input column, or an empty list if the column contains only null values.
+        """
         check_is_fitted(
             self,
         )
