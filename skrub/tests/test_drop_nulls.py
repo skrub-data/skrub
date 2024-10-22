754e2efc593f10d1073fdcd1b0c6b5f66fc0341d
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-10-22 10:03:52 +0200

    Updated test

--- skrub/tests/test_drop_nulls.py
+++ skrub/tests/test_drop_nulls.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-from numpy.testing import assert_array_equal
 
 from skrub import TableVectorizer
 from skrub import _dataframe as sbd
@@ -40,25 +39,25 @@ def drop_null_table(df_module):
     )
 
 
-def test_single_column(drop_null_table):
+def test_single_column(drop_null_table, df_module):
     """Check that null columns are dropped and non-null columns are kept."""
     dn = DropNullColumn()
     assert dn.fit_transform(drop_null_table["value_nan"]) == []
     assert dn.fit_transform(drop_null_table["value_null"]) == []
 
-    assert_array_equal(
-        sbd.to_numpy(sbd.col(drop_null_table, "idx")), np.array([1, 2, 3])
+    df_module.assert_column_equal(
+        sbd.col(drop_null_table, "idx"), df_module.make_column("idx", [1, 2, 3])
     )
 
-    assert_array_equal(
-        sbd.to_numpy(sbd.col(drop_null_table, "value_almost_nan")),
-        np.array([2.5, np.nan, np.nan]),
+    df_module.assert_column_equal(
+        sbd.col(drop_null_table, "value_almost_nan"),
+        df_module.make_column("value_almost_nan", [2.5, np.nan, np.nan]),
     )
 
-    # assert_array_equal(
-    #     sbd.to_numpy(sbd.col(drop_null_table, "value_almost_null")),
-    #     np.array(["almost", None, None]),
-    # )
+    df_module.assert_column_equal(
+        sbd.col(drop_null_table, "value_almost_null"),
+        df_module.make_column("value_almost_null", ["almost", None, None]),
+    )
 
 
 def test_drop_null_column(drop_null_table):
