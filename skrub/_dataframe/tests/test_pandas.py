1f9e150246db2db7ffc55d3171fc7aa8083b9891
Théo Jolivet <theo.jolivet@ens-paris-saclay.fr>
2024-11-14 11:44:35 +0100

    Delete tests from files to be removed

--- skrub/_dataframe/tests/test_pandas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-from skrub._dataframe._pandas import rename_columns
-
-
-def test_rename_columns():
-    df = pd.DataFrame({"a column": [1], "another": [1]})
-    df = rename_columns(df, str.swapcase)
-    assert list(df.columns) == ["A COLUMN", "ANOTHER"]
