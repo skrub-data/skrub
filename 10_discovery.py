02017fa3c0c079b18693083923a3f3207cc6dd02
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-11-18 17:15:41 +0100

    Adding example

---
+++ 10_discovery.py
@@ -0,0 +1,22 @@
+# %%
+import polars as pl
+
+from skrub._discover import Discover, find_unique_values
+
+# %%
+# working with binary to debug
+data_lake_path = "data/binary_update/*.parquet"
+base_table_path = "data/source_tables/company_employees-yadl-depleted.parquet"
+query_column = "col_to_embed"
+
+
+base_table = pl.read_parquet(base_table_path)
+# %%
+find_unique_values(base_table, ["col_to_embed"])
+# %%
+discover = Discover(data_lake_path, [query_column])
+print("fitting")
+discover.fit(base_table)
+print("transforming")
+joined_table = discover.transform(base_table)
+print(joined_table)
