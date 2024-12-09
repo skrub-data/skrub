6c6d884c1ebc58970259ebc05825162f2c2ea667
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2024-12-09 16:24:08 +0100

    ✅ Fixing coverage

--- skrub/tests/test_string_encoder.py
+++ skrub/tests/test_string_encoder.py
@@ -42,10 +42,27 @@ def test_encoding(encode_column, df_module):
     df_module.assert_frame_equal(check_df, result)
 
 
-def test_get_feature_names_out(encode_column):
+def test_get_feature_names_out(encode_column, df_module):
     """Test that ``get_feature_names_out`` returns the correct feature names."""
     encoder = StringEncoder(n_components=4)
 
     encoder.fit(encode_column)
     expected_columns = ["col1_0", "col1_1", "col1_2", "col1_3"]
+    assert encoder.get_feature_names_out() == expected_columns
+
+    # Checking that a series with an empty name generates the proper column names
+    X = df_module.make_column(
+        None,
+        [
+            "this is the first document",
+            "this document is the second document",
+            "and this is the third one",
+            "is this the first document",
+        ],
+    )
+
+    encoder = StringEncoder(n_components=4)
+
+    encoder.fit(X)
+    expected_columns = ["tsvd_0", "tsvd_1", "tsvd_2", "tsvd_3"]
     assert encoder.get_feature_names_out() == expected_columns
