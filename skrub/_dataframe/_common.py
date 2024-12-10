7ca39f7e83a29ac086fef1e6e41c3f1266e4e239
Jérôme Dockès <jerome@dockes.org>
2024-12-10 13:02:16 +0100

    Revert "MAINT adapt for scikit-learn 1.6 (#1135)" (#1194)
    This reverts commit 18af508c7f279908cb0188140bfc4f336446c9ba.

--- skrub/_dataframe/_common.py
+++ skrub/_dataframe/_common.py
@@ -106,8 +106,6 @@ __all__ = [
     "total_seconds",
 ]
 
-pandas_version = parse_version(parse_version(pd.__version__).base_version)
-
 #
 # Inspecting containers' type and module
 # ======================================
@@ -332,8 +330,7 @@ def _concat_horizontal_pandas(*dataframes):
     init_index = dataframes[0].index
     dataframes = [df.reset_index(drop=True) for df in dataframes]
     dataframes = _join_utils.make_column_names_unique(*dataframes)
-    kwargs = {"copy": False} if pandas_version < parse_version("3.0") else {}
-    result = pd.concat(dataframes, axis=1, **kwargs)
+    result = pd.concat(dataframes, axis=1, copy=False)
     result.index = init_index
     return result
 
