6f9cafd906c38ce4f18b22b16966de74e2b1d1da
Thomas S <thomas@probabl.ai>
2025-01-15 00:31:07 +0100

    Add tests

--- skrub/datasets/tests/test_utils.py
+++ skrub/datasets/tests/test_utils.py
@@ -1,10 +1,11 @@
 import shutil
 import tempfile
+from importlib import reload
 from pathlib import Path
 
 import pytest
 
-from skrub.datasets._utils import get_data_dir, get_data_home
+from skrub.datasets import _utils
 
 
 @pytest.mark.parametrize("data_home_type", ["string", "path"])
@@ -14,20 +15,20 @@ def test_get_data_dir(data_home_type):
 
         # with a pre-existing folder
         assert Path(target_dir).exists()
-        data_home = get_data_home(data_home=tmp_dir)
+        data_home = _utils.get_data_home(data_home=tmp_dir)
         assert data_home == Path(target_dir).resolve()
 
         assert data_home.exists()
 
         assert (
-            get_data_dir(name="tests", data_home=tmp_dir)
+            _utils.get_data_dir(name="tests", data_home=tmp_dir)
             == data_home.resolve() / "tests"
         )
 
         # if the folder is missing it will be created
         shutil.rmtree(tmp_dir)
         assert not Path(target_dir).exists()
-        data_home = get_data_home(data_home=tmp_dir)
+        data_home = _utils.get_data_home(data_home=tmp_dir)
         assert data_home.exists()
 
 
@@ -38,7 +39,7 @@ def test_get_data_home_default():
     user_path = Path("~").expanduser() / "skrub_data"
     is_already_existing = user_path.exists()
 
-    data_home = get_data_home(data_home=None)
+    data_home = _utils.get_data_home(data_home=None)
     assert data_home == user_path
     assert data_home.exists()
 
@@ -46,3 +47,24 @@ def test_get_data_home_default():
         # Clear the folder if it was not already existing.
         shutil.rmtree(user_path)
         assert not user_path.exists()
+
+
+def test_get_data_home_with_parameter(tmp_path):
+    dirpath = tmp_path / "my-home"
+
+    assert _utils.get_data_home(dirpath) == dirpath
+    assert dirpath.exists()
+
+
+def test_get_data_home_with_envar(monkeypatch, tmp_path):
+    dirpath = tmp_path / "my-home"
+
+    try:
+        with monkeypatch.context() as mp:
+            mp.setenv(_utils.DATA_HOME_ENVAR_NAME, str(dirpath))
+            reload(_utils)
+
+            assert _utils.get_data_home() == dirpath
+            assert dirpath.exists()
+    finally:
+        reload(_utils)
