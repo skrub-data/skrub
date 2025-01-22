e9f20943b4f8410d680e7f298f7034b21fac6b4b
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2025-01-22 18:31:24 +0100

    wip

--- skrub/datasets/__init__.py
+++ skrub/datasets/__init__.py
@@ -1,21 +1,25 @@
 from ._fetching import (
+    fetch_bike_sharing,
+    fetch_country_happiness,
     fetch_credit_fraud,
     fetch_drug_directory,
     fetch_employee_salaries,
+    fetch_flight_delays,
     fetch_medical_charge,
     fetch_midwest_survey,
+    fetch_movielens,
     fetch_open_payments,
     fetch_toxicity,
     fetch_traffic_violations,
+    fetch_videogame_sales,
 )
 from ._generating import make_deduplication_data
+from ._ken_embeddings import (
+    fetch_ken_embeddings,
+    fetch_ken_table_aliases,
+    fetch_ken_types,
+)
 from ._utils import get_data_dir
-
-# from ._ken_embeddings import (
-#     fetch_ken_embeddings,
-#     fetch_ken_table_aliases,
-#     fetch_ken_types,
-# )
 
 __all__ = [
     "fetch_drug_directory",
@@ -24,13 +28,17 @@ __all__ = [
     "fetch_midwest_survey",
     "fetch_open_payments",
     "fetch_traffic_violations",
-    # "fetch_world_bank_indicator",
+    "fetch_world_bank_indicator",
     "fetch_credit_fraud",
     "fetch_toxicity",
-    # "fetch_movielens",
+    "fetch_videogame_sales",
+    "fetch_bike_sharing",
+    "fetch_movielens",
+    "fetch_flight_delays",
+    "fetch_country_happiness",
     "get_data_dir",
     "make_deduplication_data",
-    # "fetch_ken_embeddings",
-    # "fetch_ken_table_aliases",
-    # "fetch_ken_types",
+    "fetch_ken_embeddings",
+    "fetch_ken_table_aliases",
+    "fetch_ken_types",
 ]
