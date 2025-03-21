ff0e90459ade712e57cdd4ac04cc7ce57bab6fb0
Riccardo Cappuzzo <7548232+rcap107@users.noreply.github.com>
2025-03-21 15:22:50 +0100

    Adding a periodic encoder to the DatetimeEncoder (#1235)
    Co-authored-by: Gael Varoquaux <gael.varoquaux@normalesup.org>
    Co-authored-by: Vincent M <maladiere.vincent@yahoo.fr>
    Co-authored-by: Jérôme Dockès <jerome@dockes.org>

--- examples/03_datetime_encoder.py
+++ examples/03_datetime_encoder.py
@@ -32,13 +32,20 @@ It is used by default in the |TableVectorizer|.
 .. |make_column_transformer| replace::
     :class:`~sklearn.compose.make_column_transformer`
 
-.. |HGBR| replace::
-    :class:`~sklearn.ensemble.HistGradientBoostingRegressor`
+.. |RidgeCV| replace::
+    :class:`~sklearn.linear_model.RidgeCV`
+
+.. |SimpleImputer| replace::
+    :class:`~sklearn.impute.SimpleImputer`
+
+.. |StandardScaler| replace::
+    :class:`~sklearn.preprocessing.StandardScaler`
 
 .. |ToDatetime| replace::
     :class:`~skrub.ToDatetime`
 """
 
+# %%
 ###############################################################################
 # A problem with relevant datetime features
 # -----------------------------------------
@@ -80,12 +87,13 @@ print("original dtype:", X["date"].dtypes, "\n\nconverted dtype:", date.dtypes)
 # We now encode this column with a |DatetimeEncoder|.
 #
 # During the instantiation of the |DatetimeEncoder|, we specify that we want
-# to extract the day of the week, and that we don't want to extract anything
-# finer than hours. This is because we don't want to extract minutes, seconds
-# and lower units, as they are unimportant here.
+# don't want to extract features with a resolution finer than hours. This is
+# because we don't want to extract minutes, seconds and lower units, as they
+# are unimportant here.
 
 from skrub import DatetimeEncoder
 
+# DatetimeEncoder has "hour" as default resolution
 date_enc = DatetimeEncoder().fit_transform(date)
 
 print(date, "\n\nHas been encoded as:\n\n", date_enc)
@@ -115,7 +123,7 @@ pprint(table_vec.get_feature_names_out())
 #
 # Here, for example, we want it to extract the day of the week:
 
-# use the ``datetime`` argument to customize how datetimes are handled
+# use the ``datetime`` argument to use a custom DatetimeEncoder in the TableVectorizer
 table_vec_weekday = TableVectorizer(datetime=DatetimeEncoder(add_weekday=True)).fit(X)
 pprint(table_vec_weekday.get_feature_names_out())
 
@@ -129,19 +137,50 @@ pprint(table_vec_weekday.get_feature_names_out())
 pprint(table_vec_weekday.transformers_)
 
 ###############################################################################
+#
+# Feature engineering for linear models
+# ....................................................................
+#
+# The |DatetimeEncoder| can generate additional periodic features. These are
+# particularly useful for linear models. This is controlled by the
+# ``periodic encoding`` parameter which can be either  ``circular`` or ``spline``,
+# for trigonometric functions or B-Splines respectively. In this example, we use
+# ``spline``.
+# We can also add the day in the year with the parameter ``add_day_of_year``.
+
+table_vec_periodic = TableVectorizer(
+    datetime=DatetimeEncoder(
+        add_weekday=True, periodic_encoding="spline", add_day_of_year=True
+    )
+).fit(X)
+
+###############################################################################
 # Prediction with datetime features
 # ---------------------------------
 #
 # For prediction tasks, we recommend using the |TableVectorizer| inside a
 # pipeline, combined with a model that can use the features extracted by the
 # |DatetimeEncoder|.
-# Here we'll use a |HGBR| as our learner.
-#
-from sklearn.ensemble import HistGradientBoostingRegressor
-from sklearn.pipeline import make_pipeline
+# Here we'll use a |RidgeCV| model as our learner. We also fill null values with
+# |SimpleImputer| and then rescale numeric features with |StandardScaler|.
+# To test the effect of different datetime encodings on the linear model, we train
+# three separate pipelines.
 
-pipeline = make_pipeline(table_vec, HistGradientBoostingRegressor())
-pipeline_weekday = make_pipeline(table_vec_weekday, HistGradientBoostingRegressor())
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import RidgeCV
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+# Base pipeline with default DatetimeEncoder parameters
+pipeline = make_pipeline(table_vec, StandardScaler(), SimpleImputer(), RidgeCV())
+# Datetime encoder with weekday feature
+pipeline_weekday = make_pipeline(
+    table_vec_weekday, StandardScaler(), SimpleImputer(), RidgeCV()
+)
+# Datetime encoder with periodic features
+pipeline_periodic = make_pipeline(
+    table_vec_periodic, StandardScaler(), SimpleImputer(), RidgeCV()
+)
 
 ###############################################################################
 # Evaluating the model
@@ -155,13 +194,37 @@ pipeline_weekday = make_pipeline(table_vec_weekday, HistGradientBoostingRegresso
 # which ensures that the test set is always in the future.
 from sklearn.model_selection import TimeSeriesSplit, cross_val_score
 
-cross_val_score(
+score_base = cross_val_score(
     pipeline,
     X,
     y,
-    scoring="neg_mean_squared_error",
+    scoring="neg_root_mean_squared_error",
     cv=TimeSeriesSplit(n_splits=5),
 )
+
+score_weekday = cross_val_score(
+    pipeline_weekday,
+    X,
+    y,
+    scoring="neg_root_mean_squared_error",
+    cv=TimeSeriesSplit(n_splits=5),
+)
+
+score_periodic = cross_val_score(
+    pipeline_periodic,
+    X,
+    y,
+    scoring="neg_root_mean_squared_error",
+    cv=TimeSeriesSplit(n_splits=5),
+)
+
+print(f"Base transformer - Mean RMSE : {-score_base.mean():.2f}")
+print(f"Transformer with weekday -  Mean RMSE : {-score_weekday.mean():.2f}")
+print(f"Transformer with periodic features - Mean RMSE : {-score_periodic.mean():.2f}")
+
+###############################################################################
+# As expected for linear models, introducing the periodic features improved
+# the RMSE by a noticeable amount.
 
 ###############################################################################
 # Plotting the prediction
@@ -184,10 +247,16 @@ y_pred = pipeline.predict(X_test)
 pipeline_weekday.fit(X_train, y_train)
 y_pred_weekday = pipeline_weekday.predict(X_test)
 
+pipeline_periodic.fit(X_train, y_train)
+y_pred_periodic = pipeline_periodic.predict(X_test)
+
+X_plot = pd.to_datetime(X.tail(96)["date"]).values
+X_test_plot = pd.to_datetime(X_test.tail(96)["date"]).values
+
 fig, ax = plt.subplots(figsize=(12, 3))
-fig.suptitle("Predictions with tree models")
+fig.suptitle("Predictions with linear models")
 ax.plot(
-    X.tail(96)["date"],
+    X_plot,
     y.tail(96).values,
     "x-",
     alpha=0.2,
@@ -195,25 +264,41 @@ ax.plot(
     color="black",
 )
 ax.plot(
-    X_test.tail(96)["date"],
+    X_test_plot,
     y_pred[-96:],
     "x-",
-    label="DatetimeEncoder() + HGBR prediction",
+    label="DatetimeEncoder() + RidgeCV prediction",
 )
 ax.plot(
-    X_test.tail(96)["date"],
-    y_pred_weekday[-96:],
+    X_test_plot,
+    y_pred_periodic[-96:],
     "x-",
-    label="DatetimeEncoder(add_weekday=True) + HGBR prediction",
+    label='DatetimeEncoder(periodic_encoding="spline") + RidgeCV prediction',
 )
+
+
+ax.xaxis.set_major_locator(mdates.DayLocator())
+ax.xaxis.set_minor_locator(
+    mdates.HourLocator(
+        [0, 6, 12, 18],
+    )
+)
+
+# Major formatter: format date as "YYYY-MM-DD"
+ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
+# # Minor formatter: format time as "HH:MM"
+ax.xaxis.set_minor_formatter(mdates.DateFormatter("%H:%M"))
 
 ax.tick_params(axis="x", labelsize=7, labelrotation=75)
 ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m-%d"))
-_ = ax.legend()
+_ = fig.legend(loc="upper left")
 plt.tight_layout()
 plt.show()
+# %%
 ###############################################################################
-# As we can see, adding the weekday yields better predictions on our test set.
+# As we can see, the base RidgeCV model struggles to learn the the pattern well
+# enough, while the model that is trained on the additional periodic features
+# follows the actual demand more accurately.
 
 
 ###############################################################################
@@ -221,25 +306,25 @@ plt.show()
 # -------------------
 #
 # Using the |DatetimeEncoder| allows us to better understand how the date
-# impacts the bike sharing demand. To this aim, we can compute the
-# importance of the features created by the |DatetimeEncoder|, using the
-# :func:`~sklearn.inspection.permutation_importance` function, which
-# basically shuffles a feature and sees how the model changes its prediction.
+# impacts the bike sharing demand. To this aim, we can use the function
+# :func:`~sklearn.inspection.permutation_importance` to shuffle the features
+# created by the |DatetimeEncoder| and measure their importance by observing
+# how the model changes its prediction.
+
 
 ###############################################################################
 from sklearn.inspection import permutation_importance
 
 # In this case, we don't use the whole pipeline, because we want to compute the
 # importance of the features created by the DatetimeEncoder
-X_test_transform = pipeline[:-1].transform(X_test)
-
+X_test_transform = pipeline_periodic[:-1].transform(X_test)
 result = permutation_importance(
-    pipeline[-1], X_test_transform, y_test, n_repeats=10, random_state=0
+    pipeline_periodic[-1], X_test_transform, y_test, n_repeats=10, random_state=0
 )
 
 result = pd.DataFrame(
     dict(
-        feature_names=X_test_transform.columns,
+        feature_names=pipeline_periodic[0].all_outputs_,
         std=result.importances_std,
         importances=result.importances_mean,
     )
@@ -256,8 +341,10 @@ plt.tight_layout()
 plt.show()
 
 # %%
-# We can see that the hour of the day, the temperature and the humidity
-# are the most important features, which seems reasonable.
+# We can clearly see that some of the hour splines (``date_hour_spline_18``,
+# ``date_hour_spline_9``) are more important than other features, likely due to
+# the fact that they match rush hours in the day. Other features, such as the
+# temperature, the month, and the humidity are more important than others.
 #
 # Conclusion
 # ----------
