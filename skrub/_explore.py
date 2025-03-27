191e1bf361103582cc1891ce5f3d86233b3e205e
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-03-27 15:20:54 +0100

    error handling for CSV and Parquet reads,  column type selection using polars selectors.

--- skrub/_explore.py
+++ skrub/_explore.py
@@ -9,10 +9,17 @@ try:
 except ImportError:
     POLARS_INSTALLED = False
 
+import enum
+
 import pandas as pd
 
 
-def is_binary(file_path):
+class ParseStatus(enum.IntFlag):
+    SUCCESS = 0
+    FAILED = 1
+
+
+def _is_binary(file_path):
     """
     Check if a file is binary or text.
 
@@ -26,7 +33,7 @@ def is_binary(file_path):
         with open(file_path, "rb") as file:
             # Read the first 1024 bytes of the file
             chunk = file.read(1024)
-            # Check for the presence of null bytes, which are common in binary files
+            # Check for the presence of null bytes
             if b"\0" in chunk:
                 return True
             # Try to decode the chunk as UTF-8
@@ -54,18 +61,25 @@ def load_table_paths(path_to_tables: str | Path) -> list:
     return table_list
 
 
+def get_table_stats(df):
+    # TODO: implement using function dispatch
+    if isinstance(df, pd.DataFrame):
+        return _get_stats_pandas(df)
+    elif isinstance(df, pl.DataFrame):
+        return _get_stats_polars(df)
+    else:
+        raise ValueError("Dataframe type not supported.")
+
+
 def _get_stats_pandas(df):
     stats = {
         "n_rows": df.shape[0],
         "n_columns": df.shape[1],
     }
 
-    object_cols = df.select_dtypes(include=["object"]).columns
-    stats["n_object_columns"] = len(object_cols)
-    float_cols = df.select_dtypes(include=["float"]).columns
-    stats["n_float_columns"] = len(float_cols)
-    numerical_cols = df.select_dtypes(include=["number"]).columns
-    stats["n_numerical_columns"] = len(numerical_cols)
+    stats["n_object_columns"] = len(df.select_dtypes(include=["object"]).columns)
+    stats["n_float_columns"] = len(df.select_dtypes(include=["float"]).columns)
+    stats["n_numerical_columns"] = len(df.select_dtypes(include=["number"]).columns)
 
     return stats
 
@@ -75,22 +89,17 @@ def _get_stats_polars(df):
         "n_rows": df.shape[0],
         "n_columns": df.shape[1],
     }
-
     # get column types using polars selectors
-    object_cols = df.select(cs.string()).columns
-    stats["n_object_columns"] = len(object_cols)
-    float_cols = df.select(cs.float()).columns
-    stats["n_float_columns"] = len(float_cols)
-    numerical_cols = df.select(cs.numeric()).columns
-    stats["n_numerical_columns"] = len(numerical_cols)
+    stats["n_object_columns"] = len(df.select(cs.string()).columns)
+    stats["n_float_columns"] = len(df.select(cs.float()).columns)
+    stats["n_numerical_columns"] = len(df.select(cs.numeric()).columns)
 
     return stats
 
 
-def detect_csv_delimiter(path_to_file):
+def _detect_csv_delimiter(path_to_file):
     with open(path_to_file, "r") as file:
         sample = file.readline()
-
         # Create a Sniffer object and detect the delimiter
         sniffer = csv.Sniffer()
 
@@ -103,36 +112,39 @@ def detect_csv_delimiter(path_to_file):
             return None
 
 
-def handle_csv(path_to_file, engine):
+def test_parse_csv(path_to_file, engine):
     # sniff delimiter
-    delimiter = detect_csv_delimiter(path_to_file)
+    delimiter = _detect_csv_delimiter(path_to_file)
     # try to parse with delimiter
     if engine == "polars":
         try:
-            df = pl.read_csv(path_to_file, separator=delimiter, ignore_errors=True)
-            statistics = _get_stats_polars(df)
+            _ = pl.read_csv(path_to_file, separator=delimiter, ignore_errors=True)
+            parse_status = ParseStatus.SUCCESS
         except pl.exceptions.ComputeError:
-            statistics = {}
+            parse_status = ParseStatus.FAILED
     else:
-        df = pd.read_csv(path_to_file, delimiter=delimiter)
-        statistics = _get_stats_pandas(df)
-    return statistics
+        # TODO: Implement pandas
+        try:
+            _ = pd.read_csv(path_to_file, delimiter=delimiter)
+            parse_status = ParseStatus.SUCCESS
+        except ValueError:
+            parse_status = ParseStatus.FAILED
+    return parse_status
 
 
-def handle_parquet(path_to_file, engine):
+def test_parse_parquet(path_to_file, engine):
     if engine == "polars":
         try:
-            df = pl.read_parquet(path_to_file)
-            statistics = _get_stats_polars(df)
+            _ = pl.read_parquet(path_to_file)
+            parse_status = ParseStatus.SUCCESS
         except pl.exceptions.ComputeError:
-            statistics = {}
+            parse_status = ParseStatus.FAILED
     else:
         try:
-            df = pd.read_parquet(path_to_file)
-            statistics = _get_stats_pandas(df)
+            _ = pd.read_parquet(path_to_file)
         except ValueError:
-            statistics = {}
-    return statistics
+            parse_status = ParseStatus.FAILED
+    return parse_status
 
 
 def handle_json(path_to_file):
@@ -142,6 +154,41 @@ def handle_json(path_to_file):
 
 
 def evaluate_file(path_to_file, engine="polars"):
+    """
+    Evaluates a file's metadata and attempts to parse it based on its extension.
+    Parameters
+    ----------
+    path_to_file : str or Path
+        The path to the file to be evaluated.
+    engine : str, optional
+        The parsing engine to use. Defaults to "polars". If "polars" is selected
+        but the required library is not installed, a RuntimeError is raised.
+    Returns
+    -------
+    dict
+        A dictionary containing the following metadata about the file:
+        - is_symlink (bool): Whether the file is a symbolic link.
+        - is_dir (bool): Whether the path is a directory.
+        - absolute_path (str): The absolute path to the file.
+        - filename (str): The name of the file.
+        - parent (str): The parent directory of the file.
+        - extension (str): The file's extension.
+        - binary (bool): Whether the file is binary.
+        - file_size (float): The size of the file in kilobytes (KB).
+        - parse_status (ParseStatus): The status of parsing the file.
+    Raises
+    ------
+    RuntimeError
+        If the "polars" engine is selected but the required library is not installed.
+    Notes
+    -----
+    - The function attempts to determine the file type based on its extension.
+    - Supported extensions include ".parquet" and ".csv". Other extensions will
+      result in a parse status of `ParseStatus.FAILED`.
+    - The `is_binary` function is used to determine if the file is binary.
+    - The `test_parse_parquet` and `test_parse_csv` functions are used to test
+      parsing for ".parquet" and ".csv" files, respectively.
+    """
     if engine == "polars" and not POLARS_INSTALLED:
         raise RuntimeError("Importing polars failed.")
 
@@ -155,8 +202,7 @@ def evaluate_file(path_to_file, engine="polars"):
         "extension": "",
         "binary": False,
         "file_size": 0,
-        "file_stats": {},
-        "failed_parse": False,
+        "parse_status": ParseStatus.FAILED,
     }
 
     file_info["absolute_path"] = str(path_to_file)
@@ -171,27 +217,22 @@ def evaluate_file(path_to_file, engine="polars"):
         # trying to guess type
         ext = path_to_file.suffix
         file_info["extension"] = ext
-        file_info["binary"] = is_binary(path_to_file)
+        file_info["binary"] = _is_binary(path_to_file)
 
         if ext == ".parquet":
             # handle parquet
-            file_stats = handle_parquet(path_to_file, engine)
+            parse_status = test_parse_parquet(path_to_file, engine)
         elif ext == ".csv":
-            file_stats = handle_csv(path_to_file, engine)
+            parse_status = test_parse_csv(path_to_file, engine)
         else:
-            # extension is not recognized, do something
-            file_stats = {}
+            parse_status = ParseStatus.FAILED
 
-        file_info["file_stats"] = file_stats
-        if not file_stats:
-            file_info["failed_parse"] = True
-        else:
-            file_info["failed_parse"] = False
+        file_info["parse_status"] = parse_status
 
     return file_info
 
 
-def explore_data_lake(path_to_tables, engine="polars"):
+def explore_folder(path_to_tables, engine="polars"):
     """Given a path to a collection of tables, iterate over each of them and gather
     information.
 
@@ -214,19 +255,47 @@ def explore_data_lake(path_to_tables, engine="polars"):
         raise FileNotFoundError(f"Path {path_to_tables} does not exist.")
     table_list = load_table_paths(path_to_tables)
 
-    # overall statistics
-    # overall_statistics = {
-    #     "n_files": 0,
-    #     "n_failed_parses": 0,
-    #     "avg_n_rows": 0,
-    #     "avg_n_cols": 0,
-    # }
     stats = []
     # evaluate the statistics for every table
     for f in table_list:
         stats.append(evaluate_file(f, engine=engine))
 
     return stats
+
+
+def get_stats_on_tables(df_files: pl.DataFrame, engine="polars"):
+    if engine == "polars" and not POLARS_INSTALLED:
+        raise RuntimeError("Importing polars failed.")
+    if engine not in ["polars", "pandas"]:
+        raise ValueError(f"Dataframe engine {engine} not supported.")
+
+    stats = []
+    # evaluate the statistics for every table
+    for f_row in df_files.filter(parse_status=0).iter_rows(named=True):
+        ext = f_row["extension"]
+        if ext == ".csv":
+            if engine == "polars":
+                df = pl.read_csv(f_row["absolute_path"], ignore_errors=True)
+            else:
+                df = pd.read_csv(f_row["absolute_path"])
+        elif ext == ".parquet":
+            if engine == "polars":
+                df = pl.read_parquet(
+                    f_row["absolute_path"],
+                )
+            else:
+                df = pd.read_parquet(f_row["absolute_path"])
+        else:
+            raise ValueError(f"Extension {ext} not supported.")
+
+        f_path = Path(f_row["absolute_path"])
+        f_name = f_path.name
+        f_parent = f_path.parent
+        stats_this = {"file_name": f_name, "parent": f_parent, "extension": ext}
+        stats_this.update(get_table_stats(df))
+        stats.append(stats_this)
+
+    return pl.DataFrame(stats)
 
 
 def find_candidates(table, path_to_tables):
