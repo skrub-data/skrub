b092fdecc6f854336a408e31d8aaa96cab1ef172
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2025-04-01 16:30:04 +0200

    iter

--- skrub/_column_associations.py
+++ skrub/_column_associations.py
@@ -290,9 +290,9 @@ def _melt(df):
 @_melt.specialize("pandas", argument_type="DataFrame")
 def _melt_pandas(df, left_col, right_col, val):
     # Deal with multi-level index and columns
-    if df.index.nlevels() > 1:
+    if df.index.nlevels > 1:
         df.index = df.index.to_flat_index().astype(str)
-    if df.columns.nlevels() > 1:
+    if df.columns.nlevels > 1:
         df.columns = df.columns.to_flat_index().astype(str)
     return df.melt(ignore_index=False, var_name=right_col, value_name=val).reset_index(
         names=left_col
