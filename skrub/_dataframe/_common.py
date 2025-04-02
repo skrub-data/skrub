fb7e6dee3e328b3a6c0310f5511f467f9f14369a
Reshama Shaikh <reshama.stat@gmail.com>
2025-04-02 04:42:33 -0400

    Add Pearsons correlation to _column_associations (#1203)
    Co-authored-by: Vincent Maladiere <maladiere.vincent@yahoo.fr>

--- skrub/_dataframe/_common.py
+++ skrub/_dataframe/_common.py
@@ -86,6 +86,7 @@ __all__ = [
     "max",
     "std",
     "mean",
+    "pearson_corr",
     "sort",
     "value_counts",
     "quantile",
@@ -995,6 +996,21 @@ def _mean_pandas_col(col):
 @mean.specialize("polars", argument_type="Column")
 def _mean_polars_col(col):
     return col.mean()
+
+
+@dispatch
+def pearson_corr(df):
+    raise NotImplementedError()
+
+
+@pearson_corr.specialize("pandas", argument_type="DataFrame")
+def _pearson_corr_pandas(df):
+    return df.corr(method="pearson", numeric_only=True)
+
+
+@pearson_corr.specialize("polars", argument_type="DataFrame")
+def _pearson_corr_polars(df):
+    return pl.from_pandas(_pearson_corr_pandas(df.to_pandas()))
 
 
 @dispatch
