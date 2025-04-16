7ed9c21c588a6a11b8e5dec9d16b39ce4baad35b
Jerome Dockes <jerome@dockes.org>
2025-04-16 14:18:40 +0200

    _

--- skrub/_expressions/_subsampling.py
+++ skrub/_expressions/_subsampling.py
@@ -7,6 +7,8 @@ SHOULD_SUBSAMPLE_KEY = "_skrub_should_subsample"
 def _should_subsample(mode, environment):
     if mode == "preview":
         return True
+    if "fit" not in mode:
+        return False
     return environment.get(SHOULD_SUBSAMPLE_KEY, False)
 
 
