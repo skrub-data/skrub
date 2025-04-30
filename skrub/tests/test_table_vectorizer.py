5a60ad09543cb1eabe03e8a2f1a08d8932d47bf9
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-04-30 15:02:47 +0200

    _

--- skrub/tests/test_table_vectorizer.py
+++ skrub/tests/test_table_vectorizer.py
@@ -853,6 +853,7 @@ def test_accept_pipeline():
     parse_version(sklearn.__version__) < parse_version("1.4"),
     reason="set_output('polars') was added in scikit-learn 1.4",
 )
+@pytest.mark.filterwarnings("ignore:*high_cardinality*")
 def test_clean_null_downcast_warning():
     # non-regression test for https://github.com/skrub-data/skrub/issues/894
     pl = pytest.importorskip("polars")
