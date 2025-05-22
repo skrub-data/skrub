2e825aa959d9073b83ca49788506f39ad9bd30e1
Vitor Pohlenz <43419128+vitorpohlenz@users.noreply.github.com>
2025-05-22 11:38:48 -0300

    Update doc/table_report.py
    Accept Vincent sugestion about kwargs in open file
    
    Co-authored-by: Vincent M <maladiere.vincent@yahoo.fr>

--- doc/table_report.py
+++ doc/table_report.py
@@ -5,6 +5,6 @@ from skrub.datasets import fetch_employee_salaries
 def generate_demo():
     X = fetch_employee_salaries().X
     with open(
-        file="_templates/demo_table_report_generated.html", mode="w", encoding="utf-8"
+        "_templates/demo_table_report_generated.html", "w", encoding="utf-8"
     ) as f:
         f.write(TableReport(X, n_rows=5).html_snippet())
