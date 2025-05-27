81fe4658c6857c346f8479cc0c6c99f1277f7ec7
Vitor Pohlenz <vitorpohlenz@hotmail.com>
2025-05-27 18:37:53 -0300

    Using is_column() method to identify if dataframe is a Series in TableReport

--- skrub/_reporting/_table_report.py
+++ skrub/_reporting/_table_report.py
@@ -141,12 +141,7 @@ class TableReport:
         self.verbose = verbose
         self.max_plot_columns = max_plot_columns
         self.max_association_columns = max_association_columns
-        self.shape = sbd.shape(dataframe)
-        # If data has one column, as a pandas/polars Series, convert it to a DataFrame.
-        if (len(self.shape) < 2) and hasattr(dataframe, "to_frame"):
-            self.dataframe = dataframe.to_frame()
-        else:
-            self.dataframe = dataframe
+        self.dataframe = dataframe.to_frame() if sbd.is_column(dataframe) else dataframe
         self.n_columns = sbd.shape(self.dataframe)[1]
 
     def _set_minimal_mode(self):
