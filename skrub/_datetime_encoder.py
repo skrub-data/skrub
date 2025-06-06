7be44d3c273ba437d4b96b74d3f0eeb2f6c497b7
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-06-06 14:16:15 +0200

    adding todo

--- skrub/_datetime_encoder.py
+++ skrub/_datetime_encoder.py
@@ -548,6 +548,10 @@ class _SplineEncoder(SingleColumnTransformer):
         self.is_fitted = True
         self.n_components_ = X_out.shape[1]
 
+        # TODO: this will raise an error if X is None, but it should not happen
+        # since this function is always called by the DatetimeEncoder
+        # If we decide to expose this class, we should handle the case where X is None
+        # See https://github.com/skrub-data/skrub/pull/1405
         self.input_name_ = sbd.name(X) + "_spline"
         self.all_outputs_ = self.get_feature_names_out()
 
@@ -631,6 +635,10 @@ class _CircularEncoder(SingleColumnTransformer):
 
         self.n_components_ = 2
 
+        # TODO: this will raise an error if X is None, but it should not happen
+        # since this function is always called by the DatetimeEncoder
+        # If we decide to expose this class, we should handle the case where X is None
+        # See https://github.com/skrub-data/skrub/pull/1405
         self.input_name_ = sbd.name(X) + "_circular"
         self.all_outputs_ = self.get_feature_names_out()
 
