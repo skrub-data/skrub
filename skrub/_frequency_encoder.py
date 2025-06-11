3b7dac309d8d19952ebdbede6816ff190acaea4d
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-06-11 10:50:12 +0200

    picking up pr, some prep

--- skrub/_frequency_encoder.py
+++ skrub/_frequency_encoder.py
@@ -2,34 +2,42 @@
 Implements the Frequency Encoder, a transformer that allows
 encoding a feature using it's frequency.
 """
-
-
-
-import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator, TransformerMixin
+
+from ._dispatch import dispatch
+from ._on_each_column import SingleColumnTransformer
 
 
-class FrequencyEncoder(TransformerMixin, BaseEstimator):
-    def __init__(
-        self,
-        column, 
-        bins
-    ):
-        
+@dispatch
+def _cut(col):
+    raise NotImplementedError()
+
+
+@_cut.specialize("pandas", argument_type="Column")
+def _cut_pandas(col):
+    # TODO
+    pass
+
+
+@_cut.specialize("polars", argument_type="Column")
+def _cut_polars(col):
+    # TODO
+    pass
+
+
+class FrequencyEncoder(SingleColumnTransformer):
+    def __init__(self, column, bins):
         self.column = column
         self.bins = bins
         self.uniques_to_map = None
 
-        
-       
+    def fit(self, X, y=None):
+        del y
 
-    def fit(self, X:pd.DataFrame,  y=None):
         value_counts_series = X[self.column].value_counts()
         self.uniques_to_map = pd.cut(value_counts_series, self.bins, right=False)
 
         return self
 
-    
-    def transform(self, X) -> pd.DataFrame:
+    def transform(self, X):
         return X[self.column].map(self.uniques_to_map)
