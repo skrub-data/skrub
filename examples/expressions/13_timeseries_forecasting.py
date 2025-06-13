a40d49bf9b95c26e3b74a97eec27bac8ca5ec438
Riccardo Cappuzzo <7548232+rcap107@users.noreply.github.com>
2025-06-13 13:57:12 +0200

    Update examples/expressions/13_timeseries_forecasting.py
    Co-authored-by: Guillaume Lemaitre <guillaume@probabl.ai>

--- examples/expressions/13_timeseries_forecasting.py
+++ examples/expressions/13_timeseries_forecasting.py
@@ -76,7 +76,7 @@ datetime features on the prediction performance.
 # We use a dataset of bike sharing demand in 2011 and 2012.
 # In this setting, we want to predict the number of bike rentals, based
 # on the date, time and weather conditions.
-# In this example, we will use Polars dataframes instead of Pandas.
+# In this example, we use Polars dataframes instead of Pandas.
 
 
 import polars as pl
