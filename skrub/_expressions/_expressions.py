e6336236270dcf94fa18b8463fc1557973f9d8b7
Riccardo Cappuzzo <7548232+rcap107@users.noreply.github.com>
2025-06-18 14:33:43 +0200

    Update skrub/_expressions/_expressions.py
    Co-authored-by: Vincent M <maladiere.vincent@yahoo.fr>

--- skrub/_expressions/_expressions.py
+++ skrub/_expressions/_expressions.py
@@ -797,7 +797,7 @@ def check_value_type(value):
 
     if needs_eval(value):
         raise TypeError(
-            "The `value` of a `skrub.var()` must not be a skrub"
+            "The `value` of a `skrub.var()` must not be a skrub "
             f"expression or skrub choice. Got: {type(value)}."
         )
 
