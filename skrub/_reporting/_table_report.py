37e4cb92b39771a9f0d21c25305b742a61b62693
Vincent M <maladiere.vincent@yahoo.fr>
2025-06-23 14:02:59 +0200

    Add subsampling hint in TableReport used in expression (#1418)

--- skrub/_reporting/_table_report.py
+++ skrub/_reporting/_table_report.py
@@ -167,6 +167,9 @@ class TableReport:
         self.max_association_columns = 0
         self.max_plot_columns = 0
 
+    def _display_subsample_hint(self):
+        self._summary["is_subsampled"] = True
+
     def __repr__(self):
         return f"<{self.__class__.__name__}: use .open() to display>"
 
