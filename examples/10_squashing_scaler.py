da510d4b3688573786b41b4c7f5491b3dc8c8b12
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2025-06-30 17:39:05 +0200

    update squash scaler

--- examples/10_squashing_scaler.py
+++ examples/10_squashing_scaler.py
@@ -15,70 +15,60 @@ The dataset contains numerical features, categorical features, text features, an
 These features are first converted to numerical features using
 :class:`~skrub.TableVectorizer`. Since the encoded features are not normalized, we apply
 a numerical transformation to them.
+
 Finally, we fit a simple neural network and compare the R2 scores obtained with
 different numerical transformations.
+
 While we use a simple MLPRegressor here for simplicity,
 we generally recommend using better neural network implementations or tree-based models
 whenever low test errors are desired.
 """
 
-import warnings
-
-import numpy as np
-from sklearn.compose import TransformedTargetRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.neural_network import MLPRegressor
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import QuantileTransformer, StandardScaler
-
-from skrub import DatetimeEncoder, SquashingScaler, TableVectorizer
-from skrub.datasets import fetch_employee_salaries
-
-######################################################################
+# %%
 # Comparing numerical preprocessings
-# -----------------
+# ----------------------------------
 # We test the :class:`~skrub.SquashingScaler` vs the `StandardScaler`
 # and the `QuantileTransformer` from scikit-learn.
 # We put each of these together in a pipeline with a TableVectorizer
 # and a simple MLPRegressor.
 # In the end, we print the R2 scores of each fold's validation set
 # in a three-fold cross-validation.
+import numpy as np
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.model_selection import cross_validate
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import QuantileTransformer, StandardScaler
+
+from skrub import DatetimeEncoder, SquashingScaler, TableVectorizer
+from skrub.datasets import fetch_employee_salaries
+
+np.random.seed(0)
+data = fetch_employee_salaries()
 
 for num_transformer in [
     StandardScaler(),
     QuantileTransformer(output_distribution="normal", random_state=0),
     SquashingScaler(),
 ]:
-    np.random.seed(0)
-    data = fetch_employee_salaries()
-
-    model = TransformedTargetRegressor(MLPRegressor(), transformer=StandardScaler())
-
-    pipeline = Pipeline(
-        steps=[
-            (
-                "tv",
-                TableVectorizer(datetime=DatetimeEncoder(periodic_encoding="circular")),
-            ),
-            ("num", num_transformer),
-            ("model", model),
-        ]
+    pipeline = make_pipeline(
+        TableVectorizer(datetime=DatetimeEncoder(periodic_encoding="circular")),
+        num_transformer,
+        TransformedTargetRegressor(
+            MLPRegressor(solver="lbfgs", max_iter=100),
+            transformer=StandardScaler(),
+        ),
     )
-
-    with warnings.catch_warnings():
-        # ignore warning about unknown categories
-        warnings.simplefilter("ignore", category=UserWarning)
-
-        scores = cross_val_score(pipeline, data.X, data.y, cv=3, scoring="r2")
+    scores = cross_validate(pipeline, data.X, data.y, cv=3, scoring="r2")
 
     print(
         f"Cross-validation R2 scores for {num_transformer.__class__.__name__}"
-        f" (higher is better):\n{scores}"
+        f" (higher is better):\n{scores['test_score']}\n"
     )
 
-######################################################################
+# %%
 # Result
-# -----------------
+# ------
 # On the employee salaries dataset, the SquashingScaler performs
 # better than StandardScaler and QuantileTransformer on all
 # cross-validation folds.
--- skrub/_squashing_scaler.py
+++ skrub/_squashing_scaler.py
@@ -1,105 +1,129 @@
 import numbers
 
 import numpy as np
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import BaseEstimator, OneToOneFeatureMixin, TransformerMixin
+from sklearn.preprocessing import MinMaxScaler, RobustScaler
+from sklearn.utils._array_api import get_namespace
 from sklearn.utils.validation import check_is_fitted
 
-from . import _dataframe as sbd
-from ._on_each_column import OnEachColumn, RejectColumn, SingleColumnTransformer
+
+def _soft_clip(X, max_absolute_value=3.0):
+    """Apply a soft clipping to the data.
+
+    Parameters
+    ----------
+    X : array-like, shape (n_samples, n_features)
+        The data to be clipped.
+    max_absolute_value : float, default=3.0
+        Maximum absolute value that the transformed data can take.
+
+    Returns
+    -------
+    X_clipped : array-like, shape (n_samples, n_features)
+        The clipped version of the input.
+    """
+    xp, _ = get_namespace(X)
+    return X / xp.sqrt(1 + (X / max_absolute_value) ** 2)
 
 
-class SingleColumnSquashingScaler(SingleColumnTransformer):
-    r"""Preprocess a numerical column by robust centering, scaling, and soft clipping to a defined interval.
+class SquashingScaler(OneToOneFeatureMixin, TransformerMixin, BaseEstimator):
+    r"""Perform robust centering and scaling of followed by soft clipping.
 
-    This transformer has two stages:
-
-    1. Center the median of the data to zero
-       and multiply the data by a scaling factor
-       determined based on quantiles of the distribution.
-       This is similar to scikit-learn's RobustScaler
-       but with a min-max-scaling based handling
-       for edge cases in which the two quantiles are equal.
-    2. Apply a soft-clipping function to limit the data
-       to the interval [-max_absolute_value, max_absolute_value]
-       in an injective way.
-
-    Infinite values will be mapped to the corresponding boundaries of the interval.
-    NaN values will be preserved.
+    When features have large outliers, smooth clipping prevents the outliers from
+    affecting the result too strongly, while robust scaling prevents the outliers from
+    affecting the inlier scaling. Infinite values are mapped to the corresponding boundaries of the interval. NaN
+    values are preserved.
 
     Parameters
     ----------
     max_absolute_value : float, default=3.0
         Maximum absolute value that the transformed data can take.
-    lower_quantile : float, default=0.25
-        Value of the lower quantile that will be used
-        to determine the scaling factor to rescale the data.
-    upper_quantile : float, default=0.75
-        Value of the upper quantile that will be used
-        to determine the scaling factor to rescale the data.
+
+    quantile_range : tuple of float, default=(0.25, 0.75)
+        The quantiles used to compute the scaling factor. The first value is the lower
+        quantile and the second value is the upper quantile. The default values are
+        the 25th and 75th percentiles, respectively. The quantiles are used
+        to compute the scaling factor for the robust scaling step. The quantiles are
+        computed from the finite values in the input column. If the two quantiles are
+        equal, the scaling factor is computed from the 0th and 100th percentiles
+        (i.e., the minimum and maximum values of the finite values in the input column).
+
+    copy : bool, default=True
+        Whether to copy the input data or not. If set to False, the input data will
+        be modified in-place. This is useful for memory efficiency, but may lead to
+        unexpected results if the input data is used later in the code. If set to True,
+        a copy of the input data will be made, and the original data will remain
+        unchanged. This is the default behavior and is recommended for most use cases.
 
     Notes
     -----
+    This transformer is applied to each column independently. It uses two stages:
 
-    The formula for the transform is
+    1. The first stage centers the median of the data to zero and multiply the data by a
+       scaling factor determined from quantiles of the distribution, using
+        scikit-learn's :`~sklearn.preprocessing.RobustScaler`. It also handles
+        edge-cases in which the two quantiles are equal by following-up with a
+        :class:`~sklearn.preprocessing.MinMaxEncoder`.
+    2. The second stage applies a soft clipping to the transformed data to limit the
+       data to the interval ``[-max_absolute_value, max_absolute_value]`` in an
+       injective way.
+
+    Infinite values will be mapped to the corresponding boundaries of the interval. NaN
+    values will be preserved.
+
+    The formula for the transform is:
 
     .. math::
 
         \begin{align*}
             a &:= \begin{cases}
-                1/(q_{\beta} - q_{\alpha}) &, q_{\beta} \neq q_{\alpha} \\
-                2/(q_1 - q_0) &, q_{\beta} = q_{\alpha} \text{ and } q_1 \neq q_0 \\
-                0 &, \text{ otherwise.}
-            \end{cases} \\
-            z &:= a(x - q_{1/2}), \\
-            x_{\mathrm{out}} &:= \frac{z}{\sqrt{1 + (z/B)^2}},
+                1/(q_{\beta} - q_{\alpha}) &\mathrm{if} q_{\beta} \neq q_{\alpha} \\
+                2/(q_1 - q_0) &\mathrm{if} q_{\beta} = q_{\alpha} \text{ and } q_1 \neq
+                q_0 \\ 0 &\mathrm{if} \text{ otherwise.}
+            \end{cases} \\ z &:= a(x - q_{1/2}), \\ x_{\mathrm{out}} &:=
+            \frac{z}{\sqrt{1 + (z/B)^2}},
         \end{align*}
 
-    where
+    where:
 
     - :math:`x` is a value in the input column.
     - :math:`q_{\gamma}` is the :math:`\gamma`-quantile of the finite values in X,
     - :math:`B` is max_abs_value
     - :math:`\alpha` is lower_quantile
     - :math:`\beta` is upper_quantile.
-    - :math:`x_{\mathrm{out}}` is the result of the computation that will be returned.
 
     References
     ----------
-    This method has been introduced as the robust scaling and smooth clipping transform in
-    `Better by default: Strong pre-tuned MLPs and boosted trees on tabular data
+    This method has been introduced as the robust scaling and smooth clipping transform
+    in `Better by default: Strong pre-tuned MLPs and boosted trees on tabular data
     <https://arxiv.org/abs/2407.04491>`_ by Holzmüller, Grinsztajn, Steinwart (2024).
 
     Examples
     --------
     >>> import pandas as pd
     >>> import numpy as np
-    >>> from skrub._squashing_scaler import SingleColumnSquashingScaler
-
-    >>> tfm = SingleColumnSquashingScaler()
-    >>> X = pd.Series([-np.inf, -100.0, 0.0, 8.0, 9.0, 10.0, 20.0, 100.0, np.inf, np.nan], name='improvement')
-    >>> tfm.fit_transform(X)
-    0   -3.000000
-    1   -2.871295
-    2   -0.789352
-    3   -0.090867
-    4    0.000000
-    5    0.090867
-    6    0.948683
-    7    2.820284
-    8    3.000000
-    9         NaN
-    Name: improvement, dtype: float32
+    >>> from skrub import SquashingScaler
+    >>> X = pd.DataFrame(dict(a=[-100.0, 0.0, 8.0, 9.0, 10.0, 20.0, 100.0, np.nan]))
+    >>> SquashingScaler().fit_transform(X)
+    array([[-2.98982558],
+          [-2.12132034],
+          [-0.33129458],
+          [ 0.        ],
+          [ 0.33129458],
+          [ 2.3218719 ],
+          [ 2.98543462],
+          [        nan]])
     """  # noqa: E501
 
     def __init__(
         self,
         max_absolute_value=3.0,
-        lower_quantile=0.25,
-        upper_quantile=0.75,
+        quantile_range=(0.25, 0.75),
+        copy=True,
     ):
         self.max_absolute_value = max_absolute_value
-        self.lower_quantile = lower_quantile
-        self.upper_quantile = upper_quantile
+        self.quantile_range = quantile_range
+        self.copy = copy
 
     def fit_transform(self, X, y=None):
         """Fit the transformer and transform a column.
@@ -118,224 +142,62 @@ class SingleColumnSquashingScaler(SingleColumnTransformer):
         """
         del y
 
-        if not isinstance(self.max_absolute_value, numbers.Number):
+        if not (
+            isinstance(self.max_absolute_value, numbers.Number)
+            and self.max_absolute_value > 0
+            and np.isfinite(self.max_absolute_value)
+        ):
             raise TypeError(
-                "max_absolute_value needs to be a number, but got"
-                f" type(max_absolute_value)={type(self.max_absolute_value)}"
-            )
-        if not isinstance(self.lower_quantile, numbers.Number):
-            raise TypeError(
-                "lower_quantile needs to be a number, but got"
-                f" type(lower_quantile)={type(self.lower_quantile)}"
-            )
-        if not isinstance(self.upper_quantile, numbers.Number):
-            raise TypeError(
-                "upper_quantile needs to be a number, but got"
-                f" type(upper_quantile)={type(self.upper_quantile)}"
-            )
-        if self.max_absolute_value <= 0 or not np.isfinite(self.max_absolute_value):
-            raise ValueError(
-                "max_absolute_value needs to be a positive finite float, but got"
-                f" {self.max_absolute_value}"
-            )
-        if not (0 <= self.lower_quantile < self.upper_quantile <= 1):
-            raise ValueError(
-                "need 0 <= lower_quantile < upper_quantile <= 1, but got"
-                f" lower_quantile={self.lower_quantile} and"
-                f" upper_quantile={self.upper_quantile}"
+                f"Got {self.max_absolute_value=!r}, but expected a positive finite "
+                "float."
             )
 
-        self.col_name_ = sbd.name(X)
+        self.robust_scaler_ = RobustScaler(
+            with_centering=True,
+            with_scaling=True,
+            quantile_range=self.quantile_range,
+            copy=self.copy,
+        )
+        X_tr = self.robust_scaler_.fit_transform(X)
 
-        if not sbd.is_numeric(X):
-            raise RejectColumn(f"Column {self.col_name_} is not numeric.")
-
-        eps = np.finfo("float32").tiny
-
-        values = sbd.to_numpy(X).astype(np.float32)
-        finite_values = values[np.isfinite(values)]
-        if len(finite_values) > 0:
-            self.median_ = np.median(finite_values)
-            quantiles = np.quantile(
-                finite_values, [self.lower_quantile, self.upper_quantile]
+        if (minmax_indices := np.argwhere(self.robust_scaler_.scale_ == 1)).any():
+            # if the scale is 1, we can use a min-max scaler to handle the edge cases
+            self.minmax_scaler_ = MinMaxScaler(
+                feature_range=(-1, 1),
+                copy=self.copy,
+                clip=False,
             )
-            if quantiles[1] == quantiles[0]:
-                min = np.min(finite_values)
-                max = np.max(finite_values)
-                if min == max:
-                    self.scale_ = 0
-                else:
-                    self.scale_ = 2.0 / (max - min + eps)
-            else:
-                self.scale_ = 1.0 / (quantiles[1] - quantiles[0] + eps)
+            X_tr[:, minmax_indices] = self.minmax_scaler_.fit_transform(
+                X_tr[:, minmax_indices]
+            )
+            self.minmax_indices_ = minmax_indices
         else:
-            self.median_ = 0.0
-            self.scale_ = 1.0
+            self.minmax_scaler_ = None
+            self.minmax_indices_ = None
 
-        return self.transform(X)
+        print(X)
+        return _soft_clip(X_tr, self.max_absolute_value)
 
     def transform(self, X):
         """Transform a column.
 
         Parameters
         ----------
-        X : Pandas or Polars series
-            The column to transform.
-
-        Returns
-        -------
-        result: Pandas or Polars column with shape (len(X),)
-            The scaled and squashed representation of the input.
-        """
-
-        values_np = sbd.to_numpy(X).astype(np.float32, copy=True)
-        isfinite = np.isfinite(values_np)
-        scaled_finite = self.scale_ * (values_np[isfinite] - self.median_)
-        values_np[isfinite] = scaled_finite / np.sqrt(
-            1 + (scaled_finite / self.max_absolute_value) ** 2
-        )
-        isinf = np.isinf(values_np)
-        values_np[isinf] = np.sign(values_np[isinf]) * self.max_absolute_value
-
-        # this will use the column name of the column that was passed in fit_transform,
-        # not the name of X
-        result = sbd.make_column_like(X, values_np, self.col_name_)
-        result = sbd.copy_index(X, result)
-
-        return result
-
-
-class SquashingScaler(BaseEstimator, TransformerMixin):
-    r"""Preprocess numerical columns by robust centering, scaling, and soft clipping to a defined interval.
-
-        This transformer is applied to each column independently.
-        It uses two stages:
-
-        1. Center the median of the data to zero
-           and multiply the data by a scaling factor
-           determined based on quantiles of the distribution.
-           This is similar to scikit-learn's RobustScaler
-           but with a min-max-scaling based handling
-           for edge cases in which the two quantiles are equal.
-        2. Apply a soft-clipping function to limit the data
-           to the interval [-max_absolute_value, max_absolute_value]
-           in an injective way.
-
-        Infinite values will be mapped to the corresponding boundaries of the interval.
-        NaN values will be preserved.
-
-        Parameters
-        ----------
-        max_absolute_value : float, default=3.0
-            Maximum absolute value that the transformed data can take.
-        lower_quantile : float, default=0.25
-            Value of the lower quantile that will be used
-            to determine the scaling factor to rescale the data.
-        upper_quantile : float, default=0.75
-            Value of the upper quantile that will be used
-            to determine the scaling factor to rescale the data.
-
-        Notes
-        -----
-
-        The formula for the transform is
-
-        .. math::
-
-            \begin{align*}
-                a &:= \begin{cases}
-                    1/(q_{\beta} - q_{\alpha}) &, q_{\beta} \neq q_{\alpha} \\
-                    2/(q_1 - q_0) &, q_{\beta} = q_{\alpha} \text{ and } q_1 \neq q_0 \\
-                    0 &, \text{ otherwise.}
-                \end{cases} \\
-                z &:= a(x - q_{1/2}), \\
-                x_{\mathrm{out}} &:= \frac{z}{\sqrt{1 + (z/B)^2}},
-            \end{align*}
-
-        where
-
-        - :math:`x` is a value in the input column.
-        - :math:`q_{\gamma}` is the :math:`\gamma`-quantile of the finite values in X,
-        - :math:`B` is max_abs_value
-        - :math:`\alpha` is lower_quantile
-        - :math:`\beta` is upper_quantile.
-        - :math:`x_{\mathrm{out}}` is the result of the computation that will be returned.
-
-        References
-        ----------
-        This method has been introduced as the robust scaling and smooth clipping transform in
-        `Better by default: Strong pre-tuned MLPs and boosted trees on tabular data
-        <https://arxiv.org/abs/2407.04491>`_ by Holzmüller, Grinsztajn, Steinwart (2024).
-
-        Examples
-        --------
-        >>> import pandas as pd
-        >>> import numpy as np
-        >>> from skrub import SquashingScaler
-
-        >>> tfm = SquashingScaler()
-        >>> X = pd.Series([-np.inf, -100.0, 0.0, 8.0, 9.0, 10.0, 20.0, 100.0, np.inf, np.nan], name='improvement')
-        >>> tfm.fit_transform(X)
-        0   -3.000000
-        1   -2.871295
-        2   -0.789352
-        3   -0.090867
-        4    0.000000
-        5    0.090867
-        6    0.948683
-        7    2.820284
-        8    3.000000
-        9         NaN
-        Name: improvement, dtype: float32
-        """  # noqa: E501
-
-    def __init__(
-        self,
-        max_absolute_value=3.0,
-        lower_quantile=0.25,
-        upper_quantile=0.75,
-    ):
-        self.max_absolute_value = max_absolute_value
-        self.lower_quantile = lower_quantile
-        self.upper_quantile = upper_quantile
-
-    def fit(self, X, y=None):
-        """Fit the transformer and transform a column.
-
-        Parameters
-        ----------
-        X : Pandas or Polars dataframe or series
-            The column to transform.
-        y : None
-            Unused. Here for compatibility with scikit-learn.
-
-        Returns
-        -------
-        self: SquashingScaler
-            The current object.
-        """
-        self.tfm_ = SingleColumnSquashingScaler(
-            max_absolute_value=self.max_absolute_value,
-            lower_quantile=self.lower_quantile,
-            upper_quantile=self.upper_quantile,
-        )
-        if not sbd.is_column(X):
-            self.tfm_ = OnEachColumn(self.tfm_)
-        self.tfm_.fit(X)
-        return self
-
-    def transform(self, X):
-        """Transform a dataframe.
-
-        Parameters
-        ----------
-        X : Pandas or Polars dataframe or series
+        X : array-like, shape (n_samples, n_features)
             The data to transform.
 
         Returns
         -------
-        result: Pandas or Polars dataframe or series with the same number of rows as X.
+        X_out: array-like, shape (n_samples, n_features)
             The transformed version of the input.
         """
-        check_is_fitted(self)
-        return self.tfm_.transform(X)
+        check_is_fitted(self, ["robust_scaler_", "minmax_scaler_"])
+
+        X_tr = self.robust_scaler_.transform(X)
+
+        if self.minmax_scaler_ is not None:
+            X_tr[:, self.minmax_indices_] = self.minmax_scaler_.transform(
+                X_tr[:, self.minmax_indices_]
+            )
+
+        return _soft_clip(X_tr, self.max_absolute_value)
