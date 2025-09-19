43c0cdb62ce5f0e4a08858dc4e2f3d2d85c28504
Riccardo Cappuzzo <7548232+rcap107@users.noreply.github.com>
2025-09-19 23:33:02 +0200

    Adding a special case to _common so that Data Ops get a more informative message (#1607)

--- skrub/_dataframe/tests/test_common.py
+++ skrub/_dataframe/tests/test_common.py
@@ -15,6 +15,7 @@ import pytest
 from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal as pd_assert_frame_equal
 
+import skrub
 from skrub import selectors as s
 from skrub._dataframe import _common as ns
 
@@ -40,6 +41,12 @@ def test_not_implemented():
         params = [None] * n_params
         with pytest.raises(TypeError):
             func(*params)
+        dop = [skrub.var("a")] * n_params
+        with pytest.raises(
+            TypeError,
+            match=r"Expected a Pandas or Polars DataFrame, but got a skrub DataOp",
+        ):
+            func(*dop)
 
 
 #
