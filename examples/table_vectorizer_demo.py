8c73b7e2fb188fe1a4a72f2b1a648edb3f498528
Hong <hong.lenoir@aurera.fr>
2025-10-22 17:11:48 +0200

    Formatage automatique du code selon pre-commit

--- examples/table_vectorizer_demo.py
+++ examples/table_vectorizer_demo.py
@@ -1,12 +1,11 @@
-from skrub import TableVectorizer
 import pandas as pd
 
-data = pd.DataFrame({
-    "ville": ["Paris", "Lyon", "Marseille"],
-    "population": [2148000, 513000, 861000]
-})
+from skrub import TableVectorizer
+
+data = pd.DataFrame(
+    {"ville": ["Paris", "Lyon", "Marseille"], "population": [2148000, 513000, 861000]}
+)
 
 vectorizer = TableVectorizer()
 X = vectorizer.fit_transform(data)
 print(X)
-
