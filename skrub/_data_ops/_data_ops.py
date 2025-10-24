8b8ae5dd4a7d8cbad01e47c3b1f76fe059e238ac
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-10-24 13:51:06 +0200

    Apply ruff/flake8-bugbear rule B005
    B005 Using `.strip()` with multi-character strings is misleading

--- skrub/_data_ops/_data_ops.py
+++ skrub/_data_ops/_data_ops.py
@@ -1843,9 +1843,8 @@ class BinOp(DataOpImpl):
         return e.op(e.left, e.right)
 
     def __repr__(self):
-        return (
-            f"<{self.__class__.__name__}: {self.op.__name__.lstrip('__').rstrip('__')}>"
-        )
+        op_name = self.op.__name__.removeprefix("__").removesuffix("__")
+        return f"<{self.__class__.__name__}: {op_name}>"
 
 
 class UnaryOp(DataOpImpl):
@@ -1855,9 +1854,8 @@ class UnaryOp(DataOpImpl):
         return e.op(e.operand)
 
     def __repr__(self):
-        return (
-            f"<{self.__class__.__name__}: {self.op.__name__.lstrip('__').rstrip('__')}>"
-        )
+        op_name = self.op.__name__.removeprefix("__").removesuffix("__")
+        return f"<{self.__class__.__name__}: {op_name}>"
 
 
 class EvalMode(DataOpImpl):
