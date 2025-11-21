4756c06885a606d4e3ffec8687a2b960fb6d9a70
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2025-11-21 14:55:55 +0100

    formatting

--- skrub/_data_ops/_inspection.py
+++ skrub/_data_ops/_inspection.py
@@ -311,7 +311,7 @@ def _node_kwargs(data_op, url=None):
         tooltip = f"{tooltip}\n\n{html.escape(description)}"
     # Also escape backslahses inserted in the .dot file
     # otherwise they are interpreted as escape sequences by graphviz
-    tooltip = tooltip.replace('\\', '\\\\')
+    tooltip = tooltip.replace("\\", "\\\\")
     kwargs["tooltip"] = tooltip
     if isinstance(data_op._skrub_impl, (Var, Value)):
         kwargs["peripheries"] = 2
