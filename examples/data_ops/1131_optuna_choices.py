2b45443dd997450ea9cd6668824339b563f2c97a
Guillaume Lemaitre <guillaume@probabl.ai>
2026-01-05 17:02:27 +0100

    add new fetcher

--- examples/data_ops/1131_optuna_choices.py
+++ examples/data_ops/1131_optuna_choices.py
@@ -56,11 +56,12 @@ print(pred.skb.describe_param_grid())
 # Load data for the example
 
 # %%
-from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import KFold
 
 # (We subsample the dataset by half to make the example run faster)
-df = fetch_california_housing(as_frame=True).frame.sample(10_000, random_state=0)
+df = skrub.datasets.fetch_california_housing().california_housing.sample(
+    10_000, random_state=0
+)
 
 # The environment we will use to fit the learners created by our DataOp.
 env = {"data": df}
