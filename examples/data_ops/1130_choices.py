a5bc33d60e5386638c6e99543e9216e5a9ae0dcd
Christine P. Chai <star1327p@gmail.com>
2026-01-05 19:01:12 -0800

    DOC: Reformat a line that is too long

--- examples/data_ops/1130_choices.py
+++ examples/data_ops/1130_choices.py
@@ -238,8 +238,8 @@ encoder = skrub.choose_from(
 X.skb.apply(encoder, cols="text")
 
 # %%
-# In a similar vein, we might want to choose between a HistGradientBoostingClassifier and a Ridge
-# classifier, each with its own set of hyperparameters.
+# In a similar vein, we might want to choose between a HistGradientBoostingClassifier
+# and a Ridge classifier, each with its own set of hyperparameters.
 # We can then define a choice for the classifier and a choice for the
 # hyperparameters of each classifier.
 
