e1727e9cf900f634fe990bca5306cbfa487fd187
Garima Shrivastava <141991095+techy4shri@users.noreply.github.com>
2026-01-16 14:54:27 +0530

    chore: Refactor SingleColumnTransformer so it's in its own file (#1820)
    Co-authored-by: Riccardo Cappuzzo <7548232+rcap107@users.noreply.github.com>

--- skrub/_clean_categories.py
+++ skrub/_clean_categories.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 from . import _dataframe as sbd
-from ._apply_to_cols import RejectColumn, SingleColumnTransformer
 from ._dispatch import dispatch, raise_dispatch_unregistered_type
+from ._single_column_transformer import RejectColumn, SingleColumnTransformer
 
 __all__ = ["CleanCategories"]
 
@@ -150,7 +150,7 @@ class CleanCategories(SingleColumnTransformer):
     >>> cleaner.fit_transform(s)
     Traceback (most recent call last):
         ...
-    skrub._apply_to_cols.RejectColumn: Column 'c' is not categorical.
+    skrub._single_column_transformer.RejectColumn: Column 'c' is not categorical.
 
     However once a column has been accepted, the output of ``transform`` will
     always have a categorical dtype:
