ce09e6ae63e590c09a7ea326599cc4e9eae2eb96
GOMEZ JIMENEZ Gabriela <gabriela.gomez-jimenez@inria.fr>
2026-01-19 15:44:49 +0100

    Fix doctest and move default thousand value

--- skrub/_to_float.py
+++ skrub/_to_float.py
@@ -294,7 +294,7 @@ class ToFloat(SingleColumnTransformer):
     def __init__(self, decimal=".", thousand=None):
         super().__init__()
         self.decimal = decimal
-        self.thousand = thousand
+        self.thousand = "" if thousand is None else thousand
 
     def fit_transform(self, column, y=None):
         """Fit the encoder and transform a column.
@@ -314,8 +314,6 @@ class ToFloat(SingleColumnTransformer):
         """
         del y
         self.all_outputs_ = [sbd.name(column)]
-        if self.thousand is None:
-            self.thousand = ""  # No thousand separator
         if self.decimal is None:
             raise ValueError("The decimal separator cannot be None.")
         if self.thousand == self.decimal:
