b544efd53a9c780a4a517706d3f55647311fe15f
emassoulie <49534401+emassoulie@users.noreply.github.com>
2026-02-24 15:47:17 +0100

    Added documentation for the Selector object, as well as references to it (#1841)
    Co-authored-by: Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
    Co-authored-by: Riccardo Cappuzzo <7548232+rcap107@users.noreply.github.com>

--- doc/api_reference.py
+++ doc/api_reference.py
@@ -160,6 +160,7 @@ API_REFERENCE = {
                     " details."
                 ),
                 "autosummary": [
+                    "selectors.Selector",
                     "selectors.all",
                     "selectors.any_date",
                     "selectors.boolean",
