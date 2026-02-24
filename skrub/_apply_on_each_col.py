933ec5f656efe1ba86ae2f377bac924d4103bc2d
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2026-02-24 11:22:32 +0100

    adding core to init

--- skrub/_apply_on_each_col.py
+++ skrub/_apply_on_each_col.py
@@ -7,7 +7,7 @@ from sklearn.utils.validation import check_is_fitted
 from . import _dataframe as sbd
 from . import _utils, selectors
 from ._join_utils import pick_column_names
-from ._single_column_transformer import RejectColumn, is_single_column_transformer
+from .core._single_column_transformer import RejectColumn, is_single_column_transformer
 
 __all__ = ["ApplyToEachCol"]
 
