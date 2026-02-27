b0553ae088c6e106db0b9737f9cf5c3a0ad86504
Eloi Massoulie <emassoul@PTB-11200877.saclay.inria.fr>
2026-02-27 18:10:37 +0100

    Formatting

--- skrub/core/__init__.py
+++ skrub/core/__init__.py
@@ -1,5 +1,6 @@
 """
-Skrub utilities needed for more advanced usage than those available in the top-level skrub namespace.
+Skrub utilities needed for more advanced usage than those available in the
+top-level skrub namespace.
 """
 
 from ._single_column_transformer import RejectColumn, SingleColumnTransformer
