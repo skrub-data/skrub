e9b2531987bc75bcdd02a1c66b70b3d1d900a3a9
Riccardo Cappuzzo <7548232+rcap107@users.noreply.github.com>
2026-03-03 11:21:51 +0100

    Update skrub/datasets/_fetching.py

--- skrub/datasets/_fetching.py
+++ skrub/datasets/_fetching.py
@@ -308,8 +308,8 @@ def fetch_toxicity(data_home=None):
     This is a balanced binary classification use-case, where the single table
     consists in only two columns:
 
-    - `text`: the text of the comment
-    - `is_toxic`: whether or not the comment is toxic
+   - ``text``: the text of the comment
+   - ``is_toxic``: whether or not the comment is toxic
 
     Size on disk: 220KB.
 
