02b7dc536c9325ff1babeaf11eb737314991cd18
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2026-03-17 14:24:42 +0100

    removing config test from patching

--- skrub/_reporting/tests/test_patch_display.py
+++ skrub/_reporting/tests/test_patch_display.py
@@ -2,7 +2,7 @@ import pickle
 
 import pytest
 
-from skrub import config_context, patch_display, set_config, unpatch_display
+from skrub import patch_display, unpatch_display
 from skrub.conftest import skip_polars_installed_without_pyarrow
 
 
@@ -52,54 +52,6 @@ def test_patch_display(df_module, repeat_patch, repeat_unpatch, capsys):
             assert capsys.readouterr().err != ""
         finally:
             unpatch_display()
-
-
-@pytest.mark.parametrize("repeat_patch", [1, 2])
-@pytest.mark.parametrize("repeat_unpatch", [1, 2])
-@skip_polars_installed_without_pyarrow
-def test_patch_display_config(df_module, repeat_patch, repeat_unpatch, capsys):
-    df = df_module.make_dataframe(
-        dict(
-            a=[1, 2, 3, 4],
-            b=["one", "two", "three", "four"],
-            c=[11.1, 11.2, 11.3, 11.4],
-        )
-    )
-    assert "<table" in df._repr_html_()
-    assert "<skrub-table-report" not in df._repr_html_()
-
-    set_config(use_table_report=False)
-    assert "<table" in df._repr_html_()
-    assert "<skrub-table-report" not in df._repr_html_()
-    for _ in range(repeat_patch):
-        set_config(use_table_report=True)
-        try:
-            assert "<skrub-table-report" in df._repr_html_()
-            pickle.loads(pickle.dumps(df))
-        finally:
-            for _ in range(repeat_unpatch):
-                set_config(use_table_report=False)
-        pickle.loads(pickle.dumps(df))
-        assert "<table" in df._repr_html_()
-        assert "<skrub-table-report" not in df._repr_html_()
-
-        try:
-            capsys.readouterr()
-            with config_context(use_table_report=True, table_report_verbosity=0):
-                df._repr_html_()
-                assert capsys.readouterr().err == ""
-
-            capsys.readouterr()
-            with config_context(use_table_report=True, table_report_verbosity=None):
-                df._repr_html_()
-                assert capsys.readouterr().err != ""
-
-            capsys.readouterr()
-            with config_context(use_table_report=True, table_report_verbosity=1):
-                df._repr_html_()
-                assert capsys.readouterr().err != ""
-        finally:
-            set_config(use_table_report=False)
 
 
 def test_max_plot_max_assoc_columns_parameter(pd_module):
