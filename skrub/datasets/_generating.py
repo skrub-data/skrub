b1b5eab727f7f819f218c6baa01944fb5edf9b22
Jovan Stojanovic <jovanst98@yahoo.fr>
2026-04-01 11:50:02 +0200

    add example to toy_orders

--- skrub/datasets/_generating.py
+++ skrub/datasets/_generating.py
@@ -75,6 +75,31 @@ def toy_orders(split="train"):
     -------
     bunch
         A dictionary-like object with the keys 'X', 'y' and 'orders'.
+
+    Examples
+    --------
+    >>> from skrub.datasets import toy_orders
+    >>> toy_orders(split="train").X
+    ID	product	quantity	date
+    0	1	pen	2	2020-04-03
+    1	2	cup	3	2020-04-04
+    2	3	cup	5	2020-04-04
+    3	4	spoon	1	2020-04-05
+    >>> toy_orders(split="train").y
+    0    False
+    1    False
+    2     True
+    3    False
+    Name: delayed, dtype: bool
+
+    If you want the whole dataframe, use `.orders`:
+
+    >>> toy_orders().orders
+    ID	product	quantity	date	delayed
+    0	1	pen	2	2020-04-03	False
+    1	2	cup	3	2020-04-04	False
+    2	3	cup	5	2020-04-04	True
+    3	4	spoon	1	2020-04-05	False
     """
     X = pd.DataFrame(
         {
