46d948d29b042c0e921fcf9c82bb5201e4d7b525
Jerome Dockes <jerome@dockes.org>
2026-04-14 12:06:35 +0200

    slightly reduce graph size

--- skrub/_data_ops/_inspection.py
+++ skrub/_data_ops/_inspection.py
@@ -356,7 +356,7 @@ def draw_data_op_graph(data_op, url=None, direction="TB"):
     import pydot
 
     g = graph(data_op)
-    dot_graph = pydot.Dot(rankdir=direction)
+    dot_graph = pydot.Dot(rankdir=direction, ranksep=0.4)
     for node_id, e in g["nodes"].items():
         kwargs = _node_kwargs(e, url=url)
         kwargs["id"] = _dot_id(node_id)
@@ -364,7 +364,7 @@ def draw_data_op_graph(data_op, url=None, direction="TB"):
         dot_graph.add_node(node)
     for c, children in g["children"].items():
         for child in children:
-            dot_graph.add_edge(pydot.Edge(_dot_id(child), _dot_id(c)))
+            dot_graph.add_edge(pydot.Edge(_dot_id(child), _dot_id(c), arrowsize=0.7))
 
     return GraphDrawing(dot_graph)
 
