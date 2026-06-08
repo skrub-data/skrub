6c7f356ca8a63d36a16040d7849c22bc098c9eaa
Jerome Dockes <jerome@dockes.org>
2026-06-08 17:52:47 +0200

    improve doc & input validation of concat now it supports arrays

--- skrub/_data_ops/_skrub_namespace.py
+++ skrub/_data_ops/_skrub_namespace.py
@@ -773,12 +773,13 @@ class SkrubNamespace:
 
     @checked_data_op_constructor
     def concat(self, others, axis=0):
-        """Concatenate dataframes vertically or horizontally.
+        """Concatenate arrays or dataframes vertically or horizontally.
 
         Parameters
         ----------
-        others : list of dataframes
-            The dataframes to stack horizontally with ``self``
+        others : list of arrays or dataframes
+            The arrays or dataframes to stack with ``self``. Must be of the
+            same type as ``self``: do not mix arrays with dataframes.
         axis : {0, 1}, default 0
             The axis to concatenate along.
             0: stack vertically (rows)
@@ -786,8 +787,8 @@ class SkrubNamespace:
 
         Returns
         -------
-        dataframe
-            The combined dataframes.
+        array or dataframe
+            The combined arrays or dataframes.
 
         Examples
         --------
@@ -797,6 +798,7 @@ class SkrubNamespace:
         >>> b = skrub.var('b', pd.DataFrame({'b1': [2], 'b2': [3]}))
         >>> c = skrub.var('c', pd.DataFrame({'c1': [4], 'c2': [5]}))
         >>> d = skrub.var('d', pd.DataFrame({'c1': [6], 'c2': [7]}))
+        >>> e = skrub.var('e', pd.DataFrame({'c1': [8], 'c2': [9]}))
         >>> a
         <Var 'a'>
         Result:
@@ -804,26 +806,26 @@ class SkrubNamespace:
            a1  a2
         0   0   1
         >>> a.skb.concat([b, c], axis=1)
-        <Concat: 3 dataframes>
+        <Concat: 3 tables>
         Result:
         ―――――――
            a1  a2  b1  b2  c1  c2
         0   0   1   2   3   4   5
 
-        >>> c.skb.concat([d], axis=0)
-        <Concat: 2 dataframes>
+        >>> c.skb.concat([d, e], axis=0)
+        <Concat: 3 tables>
         Result:
         ―――――――
            c1  c2
         0   4   5
         1   6   7
-
+        2   8   9
 
         Note that even if we want to concatenate a single dataframe we must
         still put it in a list:
 
         >>> a.skb.concat([b], axis=1)
-        <Concat: 2 dataframes>
+        <Concat: 2 tables>
         Result:
         ―――――――
            a1  a2  b1  b2
