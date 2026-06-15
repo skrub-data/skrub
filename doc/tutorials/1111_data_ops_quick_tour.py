85ef979fb5033190dd091452623bf4c395853002
Jerome Dockes <jerome@dockes.org>
2026-06-15 18:56:57 +0200

    iter example

--- doc/tutorials/1111_data_ops_quick_tour.py
+++ doc/tutorials/1111_data_ops_quick_tour.py
@@ -3,31 +3,55 @@ Quick overview of DataOps
 =========================
 
 .. currentmodule:: skrub
+
+Here we give a bird's eye view of the DataOps workflow on a simple regression task
+that we saw in an :ref:`early example <example_encodings>`: predicting the
+salaries of US Government employees.
+
+This dataset is so simple that it can be handled without the DataOps, using a
+scikit-learn :class:`~sklearn.pipeline.Pipeline`, but we will move on to more
+challenging datasets in later sections.
 """
 
 # %%
-import pandas as pd
-from sklearn.ensemble import HistGradientBoostingRegressor
-
-import skrub
+# Here is the dataset we will work with. The column to predict is
+# ``current_annual_salary``.
 
 # sphinx_gallery_start_ignore
+import skrub
+
 skrub.set_config(data_ops_open_graph_dropdown=True)
 # sphinx_gallery_end_ignore
 
-# %%
-# Inputs to our pipeline are declared with :func:`var`
+import skrub
 
+train_dataset = skrub.datasets.fetch_employee_salaries(split="train")
+skrub.TableReport(train_dataset.employee_salaries)
+
+# %%
+# A first simple pipeline
+# -----------------------
+#
+# We start by defining our predictive pipeline. We will need to encode the
+# features with a :class:`TableVectorizer` then predict with a
+# :class:`~sklearn.ensemble.HistGradientBoostingRegressor`.
+#
+# Inputs to our pipeline are declared with :func:`var`:
+
+# %%
 employee_data = skrub.var("employee_data")
 employee_data
 
 # %%
 # Transformation steps are added by calling methods on the intermediate
 # results. An important one is :meth:`DataOp.skb.apply`, which applies a
-# scikit-learn estimator.
+# scikit-learn estimator:
 
 # %%
+from sklearn.ensemble import HistGradientBoostingRegressor
+
 salary = skrub.var("salary")
+
 pred = employee_data.skb.apply(skrub.TableVectorizer()).skb.apply(
     HistGradientBoostingRegressor(), y=salary
 )
@@ -38,10 +62,8 @@ pred
 # for example ``.skb.apply``. We will explain why shortly.
 
 # %%
-# Once we have added all the steps, we create a learner: an object similar to a
+# Once we have added all the steps, we create a *learner*: an object similar to a
 # scikit-learn estimator with ``fit`` and ``predict`` methods.
-
-train_dataset = skrub.datasets.fetch_employee_salaries(split="train")
 
 learner = pred.skb.make_learner()
 learner.fit({"employee_data": train_dataset.X, "salary": train_dataset.y})
@@ -50,7 +72,10 @@ learner.fit({"employee_data": train_dataset.X, "salary": train_dataset.y})
 # Regular scikit-learn estimators always take the same fixed inputs: X and y.
 # Skrub learners can process arbitrary data, so the signature of methods like
 # ``fit`` and ``predict`` is different: we pass a dictionary of inputs. The
-# keys correspond to the names of the variables we used to define our learner.
+# keys correspond to the names of the variables we used to define our learner,
+# here ``"employee_data"`` and ``"salary"``.
+#
+# Finally, we can use our fitted learner to make a prediction:
 
 # %%
 test_dataset = skrub.datasets.fetch_employee_salaries(split="test")
@@ -69,45 +94,90 @@ learner.predict({"employee_data": test_dataset.X, "salary": test_dataset.y})
 train_dataset.path
 
 # %%
-# Therefore we declare a new variable. We also introduce an important feature
-# of DataOps: interactive preview results. If we pass a value to our variable
-# when creating it, it is used as example data on which to run our pipeline as
-# we define it, so we can see what the result looks like every step of the way.
+# Therefore we declare a new variable, to represent the CSV path.
+#
+# We also introduce an important feature of DataOps: interactive preview
+# results. If we pass a value to our variable when creating it, it is used as
+# example data on which skrub runs our pipeline as we define it, so we can see what
+# the result looks like every step of the way.
 
 # %%
 csv_path = skrub.var("csv_path", train_dataset.path)
 csv_path
 
 # %%
+# Note the added "Result" section in the output, which shows what the current
+# pipeline's output looks like.
+#
 # Similarly to ``.skb.apply`` (which applies an estimator), ``.skb.apply_func``
-# applies a function.
+# applies a function:
 
 # %%
+import pandas as pd
+
 full_data = csv_path.skb.apply_func(pd.read_csv)
 full_data
 
 # %%
+# Next, from our full dataframe we extract the predictive features and the
+# regression target.
+#
+# The following snippet of code shows 2 important aspects:
+#
+# - Any methods or operators we access on our DataOp ``full_data``, like
+#   ``drop`` or the ``[]`` operator below, are recorded in the pipeline and
+#   will be applied to the DataOp's result:
+#   ``full_data['current_annual_salary']`` is roughly equivalent to
+#   ``full_data.skb.apply_func(lambda df: df['curent_annual_salary'])``. This
+#   is why all the skrub functionality is behind the ``.skb`` prefix as
+#   mentioned earlier: all other attribute access will be replayed directly on
+#   the result that the DataOp produces.
+# - Once we have defined the features and targets, we mark them with
+#   :meth:`DataOp.skb.mark_as_X()` and :meth:`DataOp.skb.mark_as_y()`
+#   respectively. This tells skrub that when performing cross-validation, those
+#   are the intermediate results that should be divided into training and
+#   testing sets. Therefore, X and y do not need to be constructed and split
+#   *outside* the pipeline. Instead, our pipeline can encompass the full
+#   processing, and we indicate where the train/test split should happen.
+
 employee_data = full_data.drop(
     columns="current_annual_salary", errors="ignore"
 ).skb.mark_as_X()
+# (errors='ignore' because this column could be absent at the inference stage.)
+
+salary = full_data["current_annual_salary"].skb.mark_as_y()
+salary
 
 # %%
-salary = full_data["current_annual_salary"].skb.mark_as_y()
+# Finally, we apply the regressor. Note that the X and y nodes, on which
+# train/test split is performed, are colored differently.
+
 pred = employee_data.skb.apply(skrub.TableVectorizer()).skb.apply(
     HistGradientBoostingRegressor(), y=salary
 )
 pred
 
 # %%
+# Once we have defined our pipeline, we can tell skrub to perform the
+# cross-validation with :meth:`DataOp.skb.cross_validate`.
+
 pred.skb.cross_validate(scoring="neg_mean_absolute_percentage_error")
 
 # %%
+# Note that the variables used in this pipeline are different than the previous
+# one: we just have ``"csv_path"`` and not ``"employee_data"`` and ``"salary"``
+# like before.
+
 learner = pred.skb.make_learner(fitted=True)
 learner.predict({"csv_path": test_dataset.path})
 
 # %%
 # Tuning arbitrary choices
 # ------------------------
+#
+# The last feature we present in this first tutorial is hyperparameter tuning.
+#
+# The start of the pipeline is the same as before:
 
 # %%
 full_data = skrub.var("csv_path", train_dataset.path).skb.apply_func(pd.read_csv)
@@ -117,6 +187,17 @@ employee_data = full_data.drop(
 salary = full_data["current_annual_salary"].skb.mark_as_y()
 
 # %%
+# We use functions like :func:`choose_from` or :func:`choose_float` whenever we
+# have a choice for which we want to try several options and keep the one that
+# performs best on the validation data.
+#
+# We simply replace the value by the special "choice" object produced by skrub
+# in our pipeline, and it becomes a tunable hyperparameter of our skrub
+# learner. Here we want to tune the choice of encoder applied to
+# high-cardinality categorical columns (:class:`StringEncoder` or
+# :class:`MinHashEncoder`), and the learning rate of the
+# :class:`sklearn.ensemble.HistGradientBoostingRegressor`.
+
 encoder = skrub.choose_from(
     {"lse": skrub.StringEncoder(), "minhash": skrub.MinHashEncoder()}, name="encoder"
 )
@@ -128,9 +209,16 @@ pred = employee_data.skb.apply(
     ),
     y=salary,
 )
-pred.skb.describe_param_grid()
+print(pred.skb.describe_param_grid())
 
 # %%
+# To actually run the search for the best hyperparameters, we use
+# :meth:`DataOp.skb.make_randomized_search` or
+# :meth:`DataOp.skb.make_grid_search`. For the randomized search we can use the
+# powerful Optuna library which provides features like state-of-the-art
+# hyperparameter samplers, live interactive visualization of the search with
+# ``optuna-dashboard``, stopping and resuming searches, etc.
+
 search = pred.skb.make_randomized_search(backend="optuna", fitted=True)
 search.results_
 
@@ -138,4 +226,8 @@ search.results_
 search.plot_results()
 
 # %%
+# The search can be used with the same interface as the :class:`SkrubLearner`
+# we saw before. Alternatively, we can access its ``best_learner_`` attribute,
+# which is a SkrubLearner.
+
 search.predict({"csv_path": test_dataset.path})
